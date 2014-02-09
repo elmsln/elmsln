06d012ea403438f8d8a463758f885e119a689690
btopro <bto108@psu.edu>
2014-02-08 23:07:11 -0500

    example defaults, these all need to be generated somehow for the group to be correct

---
+++ scripts/install/elmsln-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This is the execution to do drush si commands for areas that need
+# installed but can't be produced via the jobs directory
+# include configuration settings
+source ../../config/scripts/drush-create-site/config.cfg
