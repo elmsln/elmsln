1c27c11bb5f69d0ee76be79767959b4387a29298
btopro <bto108@psu.edu>
2014-02-10 15:06:16 -0500

    script moved and readies added to domains. build script replaced with install

--- scripts/install/elmsln-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This is the execution to do drush si commands for areas that need
-# installed but can't be produced via the jobs directory
-# include configuration settings
-source ../../config/scripts/drush-create-site/config.cfg
