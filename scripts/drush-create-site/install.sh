1c0148d47f11920277a47eaefc8dca568be1ea2a
btopro <bto108@psu.edu>
2014-02-20 11:53:47 -0500

    legacy file but updating anyway

--- scripts/drush-create-site/install.sh
+++ scripts/drush-create-site/install.sh
@@ -1,37 +1,34 @@
 #install script goes here
 installdir="/usr/local/bin/drush-create-site2"
 wwwuser="apache"
-jobsdir="/var/wwwjobs"
+jobsdir="/var/www/elmsln/config/jobs"
 
-if [ ! -d $installdir ]; then 
-	mkdir $installdir
-	/bin/cp drush-create-site $installdir
-	/bin/cp config.cfg.example $installdir
-	/bin/cp rm-site.sh $installdir
-	/bin/cp init-script /etc/init.d/dcs
-else 
-	echo "Install Directory Exisits. overwrite(y/n)"
-	read overwrite
+if [ ! -d $installdir ]; then
+  mkdir $installdir
+  /bin/cp drush-create-site $installdir
+  /bin/cp config.cfg.example $installdir
+  /bin/cp rm-site.sh $installdir
+  /bin/cp init-script /etc/init.d/dcs
+else
+  echo "Install Directory Exisits. overwrite(y/n)"
+  read overwrite
 
-		if [ $overwrite = y ]; then 
-			/bin/cp -f drush-create-site $installdir
-			/bin/cp -f config.cfg.example $installdir
-			/bin/cp rm-site.sh $installdir
-			/bin/cp init-script /etc/init.d/dcs
-		fi
-			
-		
+  if [ $overwrite = y ]; then
+    /bin/cp -f drush-create-site $installdir
+    /bin/cp -f config.cfg.example $installdir
+    /bin/cp rm-site.sh $installdir
+    /bin/cp init-script /etc/init.d/dcs
+  fi
 fi
 
 #add jobs loc
-
-if [  ! -d $jobsdir ]; then 
-	mkdir $jobsdir
-	/bin/cp hosts.example $jobsdir/hosts
+if [  ! -d $jobsdir ]; then
+  mkdir $jobsdir
+  /bin/cp hosts.example $jobsdir/hosts
 else
-	echo "Jobs Directory Exists. overwrite(y/n)"
-	read jobsoverwrite
-		if [ $jobsoverwrite = y ]; then 
-			/bin/cp -f hosts.example $jobsdir/hosts
-		fi
+  echo "Jobs Directory Exists. overwrite(y/n)"
+  read jobsoverwrite
+  if [ $jobsoverwrite = y ]; then
+    /bin/cp -f hosts.example $jobsdir/hosts
+  fi
 fi
