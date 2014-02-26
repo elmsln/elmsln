68e67a61fb2a7c6ab56400b191845c5e8500ce42
Dann Bohn <djb44@Danns-iMac.local>
2014-02-26 13:50:42 -0500

    hopefully fixed funk with rm-site. had to chage the call to include 'stack' because traversing into the directory, then back up two levels, into config to edit the file diretly, instead of the symlink was the best way to go.

--- scripts/drush-create-site/rm-site.sh
+++ scripts/drush-create-site/rm-site.sh
@@ -6,53 +6,58 @@ cd $DIR
 source ../../config/scripts/drush-create-site/config.cfg
 
 if [ -z "$1" ]; then
-echo "Usage: $0 <webroot> <course name>"
+echo "Usage: $0 <webroot> <course name> <stack>"
 exit 1
 fi
 
 if [ -z "$2" ]; then
-echo "Usage: $0 <webroot> <course name>"
+echo "Usage: $0 <webroot> <course name> <stack>"
+exit 1
+fi
+
+if [ -z "$3" ]; then
+echo "Usage: $0 <webroot> <course name> <stack>"
 exit 1
 fi
 
 #remove sites directory
-for sitedata in `find $1/$2/sites/ -name $2 | grep -v services` ; do
-	echo "found sub-site $sitedata remove(y/n)"
-	read rmsitedata
-	if [ $rmsitedata == "y" ] || [ $rmsitedata == "yes" ]; then
-		dbuser=`grep ^[[:space:]]*\'username $sitedata/settings.php | awk -F\' '{print $4}'`
-		database=`grep ^[[:space:]]*\'database $sitedata/settings.php | awk -F\' '{print $4}'`
-		echo $database
-		echo $dbuser
-		echo "remove database $database?(y/n)"
-		read rmdatabase
-			if [ $rmdatabase == "y" ] || [ $rmdatabase == "yes" ]; then
-				echo "This action can NOT be undone. confirm remove of $database(y/n)"
-				read rmdbconf
-					if [ $rmdbconf == "y" ] || [ $rmdbconf == "yes" ]; then
-				echo "Removing database $database"
-				mysql -u$dbsu -p$dbsupw -e "drop database $database"
-					fi
+for sitedata in `find $1/$2/sites/$3 -name $2 | grep -v services` ; do
+    echo "found sub-site $sitedata remove(y/n)"
+    read rmsitedata
+    if [ $rmsitedata == "y" ] || [ $rmsitedata == "yes" ]; then
+        dbuser=`grep ^[[:space:]]*\'username $sitedata/settings.php | awk -F\' '{print $4}'`
+        database=`grep ^[[:space:]]*\'database $sitedata/settings.php | awk -F\' '{print $4}'`
+        echo $database
+        echo $dbuser
+        echo "remove database $database?(y/n)"
+        read rmdatabase
+            if [ $rmdatabase == "y" ] || [ $rmdatabase == "yes" ]; then
+                echo "This action can NOT be undone. confirm remove of $database(y/n)"
+                read rmdbconf
+                    if [ $rmdbconf == "y" ] || [ $rmdbconf == "yes" ]; then
+                echo "Removing database $database"
+                mysql -u$dbsu -p$dbsupw -e "drop database $database"
+                    fi
 
-			echo "remove database user $dbuser?(y/n)"
-			read rmdbuser
-				if [ $rmdbuser == "y" ] || [ $rmdbuser == "yes" ]; then
-					echo "removing $dbuser"
-					mysql -u$dbsu -p$dbsupw -e "drop user $dbuser@localhost;"
-				fi
-			fi
-		echo "removing site data"
-			servicestest=`find $1/$2/sites/*/services -name $2`
-			echo "services test"
-			echo $servicestest
-				if [[ $servicestest ]]; then
-					rm -rf $servicestest
-				fi
-			rm -rf $sitedata
+            echo "remove database user $dbuser?(y/n)"
+            read rmdbuser
+                if [ $rmdbuser == "y" ] || [ $rmdbuser == "yes" ]; then
+                    echo "removing $dbuser"
+                    mysql -u$dbsu -p$dbsupw -e "drop user $dbuser@localhost;"
+                fi
+            fi
+        echo "removing site data"
+            servicestest=`find $1/$2/sites/$3/*/services -name $2`
+            echo "services test"
+            echo $servicestest
+                if [[ $servicestest ]]; then
+                    rm -rf $servicestest
+                fi
+            rm -rf $sitedata
 
-	else
-		echo "preserving site data in $sitedata"
-	fi
+    else
+        echo "preserving site data in $sitedata"
+    fi
 done
 
 #for database in `mysql -u$dbsu -p$dbsupw -e "show databases" | grep $2` ; do
@@ -74,18 +79,22 @@ done
 # and replaces the symlink w/ the actual file that's been changed.
 # it also creates a .bak file in the core / domains locations which creates a
 # non committed file in the incorrect location.
-sitesphp=`grep -nr $2 $1/$2/sites/sites.php`
+
+#move into config dir for stack
+cd $1/../../config/stacks/$3/sites
+#grep for coursename
+sitesphp=`grep -nr $2 sites.php`
 
 while [[ $sitesphp ]]; do
-        sitesphp=`grep -nr $2 $1/$2/sites/sites.php`
-        grep -nr $2 $1/$2/sites/sites.php
+        sitesphp=`grep -nr $2 sites.php`
+        grep -nr $2 sites.php
         echo "which line do you want to remove?(x to exit)"
         read rmnum
                 validrmnum=`echo $sitesphp | grep $rmnum:`
                 if [[ $validrmnum ]]; then
-                        cp $1/$2/sites/sites.php $1/$2/sites/sites.php.bak
-                        echo "sites.php backed up to $1/$2/sites/sites.php.bak"
-                        sed -i ""$rmnum"d" $1/$2/sites/sites.php
+                        cp sites.php sites.php.bak
+                        echo "sites.php backed up to sites.php.bak"
+                        sed -i ""$rmnum"d" sites.php
                 else
                         if [[ $rmnum == "x" ]]; then
                                 exit 0
@@ -96,20 +105,20 @@ while [[ $sitesphp ]]; do
 done
 
 #if [[ $sitesphp ]]; then
-#	grep -nr $2 $1/sites/sites.php
-#	echo "which line do you want to remove?(k to keep)"
-#	read rmnum
-#	while [[ ! $rmnum =~ [0-999-k] ]]; do
-#	echo "which line do you want to remove?(k to keep)"
-#	read rmnum
-#	done
-#		if [[ $rmnum == "k" ]]; then
-#		echo "preserving sites.php"
-#		else
-#		cp $1/sites/sites.php $1/sites/sites.php.bak
-#		echo "sites.php backed up to $1/sites/sites.php.bak"
-#		sed -i ""$rmnum"d" $1/sites/sites.php
-#		fi
+#   grep -nr $2 $1/sites/sites.php
+#   echo "which line do you want to remove?(k to keep)"
+#   read rmnum
+#   while [[ ! $rmnum =~ [0-999-k] ]]; do
+#   echo "which line do you want to remove?(k to keep)"
+#   read rmnum
+#   done
+#       if [[ $rmnum == "k" ]]; then
+#       echo "preserving sites.php"
+#       else
+#       cp $1/sites/sites.php $1/sites/sites.php.bak
+#       echo "sites.php backed up to $1/sites/sites.php.bak"
+#       sed -i ""$rmnum"d" $1/sites/sites.php
+#       fi
 #fi
 
 if [ -L $1/$2 ]; then
@@ -118,5 +127,3 @@ rm -rf $1/$2
 else
 echo "symlink not present"
 fi
-
-
