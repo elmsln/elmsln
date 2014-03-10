4e05b46042fa69bad611059a4baef7af35d440a3
btopro <bto108@psu.edu>
2014-03-10 12:15:14 -0400

    updated elevator site to better match new style and project direction

--- docs/elevator/index.php
+++ docs/elevator/index.php
@@ -75,21 +75,51 @@ var ADAPT_CONFIG = {
 <!-- content set 460 -->
  <div class='grid_5 logo visibility_460 animate'><p><img src="images/elms_logo_02.png" alt="ELMS Logo"/></p></div>
   <div class='grid_7 visibility_460 text_block animate'>
-    <h2 class='title'>ELMS</h2></div>
-    <div class='grid_6 nav_link animate align_center visibility_460'><p class='highlight'><a class='highlight' href="http://drupal.org/project/cis">Download</a></p></div>
-    <div class='grid_7 visibility_460 text_block animate'><p>The E-Learning Management System (ELMS) is a package built in Drupal that allows you to create and manage an online courses from a simple user interface.</p>
+    <h2 class='title'>ELMSLN</h2></div>
+    <div class='grid_6 nav_link animate align_center visibility_460'><p class='highlight'><a class='highlight' href="http://github.com/btopro/elmsln">Download</a></p></div>
+    <div class='grid_6 nav_link animate align_center visibility_460'><p class='highlight'><a class='highlight' href="http://github.com/btopro/elmsln-vagrant">Vagrant Up!</a></p></div>
+    <div class='grid_6 nav_link animate align_center visibility_460'><p class='highlight'><a class='highlight' href="http://github.com/btopro/elms-styles/">ELMS Style Guide</a></p></div>
+
+    <div class='grid_7 visibility_460 text_block animate'><p>
+      E-Learning Management System Learning Network (ELMSLN) is a package built on top of Drupal that allows you to create and manage online courses.
+    </p>
     </div>
 
     <div class='clear'>&nbsp;</div>
-    <div class='grid_3 nav_link animate align_center visibility_460'><p><a href="#about">About</a></p></div>
+
+    <div class='grid_3 nav_link animate align_center visibility_460'><p><a href="#awards">Awards</a></p></div>
     <div class='grid_3 nav_link animate align_center visibility_460'><p><a href="#features">Features</a></p></div>
-    <div class='grid_3 nav_link animate align_center visibility_460'><p><a href="#community">Community</a></p></div>
+    <div class='grid_3 nav_link animate align_center visibility_460'><p><a href="#connect">Connect</a></p></div>
     <div class='grid_3 nav_link animate align_center visibility_460'><p><a href="/blog">Blog</a></p></div>
 
     <div class='grid_12 space-out visibility_460 animate'>
-    <h2 id='about'>About</h2>
-    <p>Built on top of the Drupal Content Management Framework, ELMS provides carefully crafted functionality that simplifies course creation, management, and distribution.  Unlike other tools, it has been developed by instructional designers, for instructionally focused course content authoring.  Many projects have come out of the ELMS Initiative since 2008 both for education as well as the Drupal community including <a href="http://drupal.org/project/cis">ELMS</a>, <a href="http://drupal.org/project/elmsmedia">ELMS Media</a>, <a href="http://drupal.org/project/ulmus">Ulmus</a>, the Open Studio and over 50 modules and themes.  One of the main goals of ELMS is to allow educators to leverage the power of Drupal. <a href="http://drupal.org/project/cis">Download it today!</a></p>
-    </div>
+      <h2 id='about'>About</h2>
+      <p>
+        Built on top of the Drupal Content Management Framework, ELMSLN provides
+        carefully crafted functionality that simplifies course creation,
+        management, and distribution.  Unlike other tools, it has been developed by
+        instructional designers, for instructionally focused course content authoring.
+        Many projects have come out of the ELMS Initiative since 2008 both for education
+        as well as the Drupal community including over 100 features, install profiles, modules and themes.
+        One of the main goals of ELMS is to allow educators to leverage the power of Drupal.
+      </p>
+      <p><a href="http://github.com/btopro/elmsln">Download it today!</a></p>
+  </div>
+<div class='grid_12 space-out visibility_460 animate'>
+      <h2 id='awards'>Awards</h2>
+      <p>
+        <div class='grid_6 align_center visibility_460 animate'>
+          <a href="http://campustechnology.com/articles/2010/08/01/innovators-awards-2010-penn-state-university.aspx">
+            <img width="300px" height="90px" src="images/awards/campustech2010.jpg" alt="Campus Technology Innovator Award 2010" title="Campus Technology Innovator Award 2010">
+          </a>
+        </div>
+        <div class='grid_6 align_center visibility_460 animate'>
+          <a href="http://campustechnology.com/articles/2011/08/01/innovators-penn-state.aspx">
+            <img width="300px" height="90px" src="images/awards/campustech2011.png" alt="Campus Technology Innovator Award 2011" title="Campus Technology Innovator Award 2011">
+          </a>
+        </div>
+      </p>
+  </div>
     <div class='grid_12 space-out visibility_460 animate'>
 	<h2 id='features'>Features</h2>
     </div>
@@ -147,36 +177,63 @@ var ADAPT_CONFIG = {
 
 
   <div class='grid_10 space-out suffix_2 visibility_460 animate'>
-<h2 id='community'>Community</h2>
+<h2 id='connect'>Connect</h2>
     <p>Join the community today and connect with the project through these sources!</p>
     <p>
         <a href="http://twitter.com/psu_elms"><img class="promo-image" src="images/twitter.png" alt="Follow psu_elms on twitter" title="Follow psu_elms on twitter"></a>
-        <a href="https://www.facebook.com/pages/ELMS/225492544152120"><img class="promo-image" src="images/facebook.png" alt="Like our Facebook page" title="Like our Facebook page"></a>
+        <a href="https://www.facebook.com/psuelms"><img class="promo-image" src="images/facebook.png" alt="Like our Facebook page" title="Like our Facebook page"></a>
         <a href="http://drupal.psu.edu/taxonomy/term/12/0/feed"><img class="promo-image" src="images/rss.png" alt="Subscribe to the ELMS project RSS feed" title="Subscribe to the ELMS project RSS feed"></a>
         <a href="mailto:elms@psu.edu"><img class="promo-image" src="images/email.png" alt="email elms@psu.edu" title="email elms@psu.edu"></a>
-        <a href="http://elearning.psu.edu/"><img class="promo-image" src="images/eli.png" alt="Check out the elearning institute" title="Check out the elearning institute"></a></p>
+        <a href="http://online.aanda.psu.edu/"><img class="promo-image" src="images/eli.png" alt="Check out the elearning institute" title="Check out the elearning institute"></a></p>
   </div>
 
 
 <!-- content set 720 -->
 	<div class='grid_2 prefix_2 visibility_720 animate'><p><img src="images/elms_logo_02.png" alt="ELMS Logo"/></p></div>
 	<div class='grid_8 visibility_720 text_block  animate'>
-    <h2 class='title'>ELMS</h2>
+    <h2 class='title'>ELMSLN</h2>
     </div>
-    <div class='grid_4 nav_link animate visibility_720'><p class='highlight'><a class='highlight' href="http://drupal.org/project/cis">Download</a></p></div>
+    <div class='grid_2 nav_link animate visibility_720'><p class='highlight'><a class='highlight' href="http://github.com/btopro/elmsln">Download</a></p></div>
+    <div class='grid_2 nav_link animate visibility_720'><p class='highlight'><a class='highlight' href="http://github.com/btopro/elmsln-vagrant">Vagrant Up!</a></p></div>
+    <div class='grid_2 nav_link animate visibility_720'><p class='highlight'><a class='highlight' href="http://github.com/btopro/elms-styles/">Style Guide</a></p></div>
 
-    <div class='grid_8 prefix_2 visibility_720 text_block suffix_2 animate'><p>The E-Learning Management System (ELMS) is a Drupal package that allows you to create and manage an online course infrastructure from a simple interface.</p>
+    <div class='grid_8 prefix_2 visibility_720 text_block suffix_2 animate'><p>
+E-Learning Management System Learning Network (ELMSLN) is a package built on top of Drupal that allows you to create and manage online courses.
+
+    </p>
     </div>
 
 
-        <div class='grid_2 prefix_2 nav_link animate visibility_720'><p><a href="#about1">About</a></p></div>
-        <div class='grid_2 nav_link animate visibility_720'><p><a href="#features1">Features</a></p></div>
-		<div class='grid_2 nav_link animate visibility_720'><p><a href="#community1">Community</a></p></div>
+    <div class='grid_2 prefix_2 nav_link animate visibility_720'><p><a href="#awards1">Awards</a></p></div>
+    <div class='grid_2 nav_link animate visibility_720'><p><a href="#features1">Features</a></p></div>
+    <div class='grid_2 nav_link animate visibility_720'><p><a href="#connect1">Connect</a></p></div>
     <div class='grid_2 nav_link animate visibility_720'><p><a href="/blog">Blog</a></p></div>
     <div class='grid_8 space-out prefix_2 suffix_2 visibility_720 animate'>
     <h2 id='about1'>About</h2>
-    <p>Built on top of the Drupal Content Management Framework, ELMS provides carefully crafted functionality that simplifies course creation, management, and distribution.  Unlike other tools, it has been developed by instructional designers, for instructionally focused course content authoring.  Many projects have come out of the ELMS Initiative since 2008 both for education as well as the Drupal community including <a href="http://drupal.org/project/cis">ELMS</a>, <a href="http://drupal.org/project/elmsmedia">ELMS Media</a>, <a href="http://drupal.org/project/ulmus">Ulmus</a>, the Open Studio and over 50 modules and themes.  One of the main goals of ELMS is to allow educators to leverage the power of Drupal/p. <a href="http://drupal.org/project/cis">Download it today!</a></p>
-    </div>
+    <p>Built on top of the Drupal Content Management Framework, ELMSLN provides
+  carefully crafted functionality that simplifies course creation,
+  management, and distribution.  Unlike other tools, it has been developed by
+  instructional designers, for instructionally focused course content authoring.
+  Many projects have come out of the ELMS Initiative since 2008 both for education
+  as well as the Drupal community including over 100 features, install profiles, modules and themes.
+  One of the main goals of ELMS is to allow educators to leverage the power of Drupal.</p>
+  <p><a href="http://github.com/btopro/elmsln">Download it today!</a></p>
+</div>
+<div class='grid_10 space-out prefix_2 visibility_720 animate'>
+      <h2 id='awards1'>Awards</h2>
+      <p>
+        <div class='grid_4 align_center visibility_720 animate'>
+          <a href="http://campustechnology.com/articles/2010/08/01/innovators-awards-2010-penn-state-university.aspx">
+            <img width="200px" height="60px" src="images/awards/campustech2010.jpg" alt="Campus Technology Innovator Award 2010" title="Campus Technology Innovator Award 2010">
+          </a>
+        </div>
+        <div class='grid_4 align_center visibility_720 animate'>
+          <a href="http://campustechnology.com/articles/2011/08/01/innovators-penn-state.aspx">
+            <img width="200px" height="60px" src="images/awards/campustech2011.png" alt="Campus Technology Innovator Award 2011" title="Campus Technology Innovator Award 2011">
+          </a>
+        </div>
+      </p>
+  </div>
     <div class='grid_10 space-out prefix_2 visibility_720 animate'>
 <h2 id='features1'>Features</h2>
 </div>
@@ -257,13 +314,13 @@ var ADAPT_CONFIG = {
         </div>
 
   <div class='grid_10 space-out prefix_2 visibility_720 animate'>
-<h2 id='community1'>Community</h2>
+<h2 id='connect1'>Connect</h2>
 <p>Join the community today and connect with the project through these sources!</p>
 <p><a href="http://twitter.com/psu_elms"><img class="promo-image" src="images/twitter.png" alt="Follow psu_elms on twitter" title="Follow psu_elms on twitter"></a>
-<a href="https://www.facebook.com/pages/ELMS/225492544152120"><img class="promo-image" src="images/facebook.png" alt="Like our Facebook page" title="Like our Facebook page"></a>
+<a href="https://www.facebook.com/psuelms"><img class="promo-image" src="images/facebook.png" alt="Like our Facebook page" title="Like our Facebook page"></a>
 <a href="http://drupal.psu.edu/taxonomy/term/12/0/feed"><img class="promo-image" src="images/rss.png" alt="Subscribe to the ELMS project RSS feed" title="Subscribe to the ELMS project RSS feed"></a>
 <a href="mailto:elms@psu.edu"><img class="promo-image" src="images/email.png" alt="email elms@psu.edu" title="email elms@psu.edu"></a>
-<a href="http://elearning.psu.edu/"><img class="promo-image" src="images/eli.png" alt="Check out the elearning institute" title="Check out the elearning institute"></a></p>
+<a href="http://online.aanda.psu.edu/"><img class="promo-image" src="images/eli.png" alt="Check out the elearning institute" title="Check out the elearning institute"></a></p>
   </div>
 
     <div class='clear'>&nbsp;</div>
