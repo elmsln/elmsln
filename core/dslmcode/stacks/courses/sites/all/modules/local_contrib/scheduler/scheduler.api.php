34601bc56fd58698f3b8ea9f75935bc770b65ac8
btopro <bto108@psu.edu>
2014-03-25 17:08:48 -0400

    scheduler module added to the courses stack

---
+++ core/dslmcode/stacks/courses/sites/all/modules/local_contrib/scheduler/scheduler.api.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file
+ * API documentation for the Scheduler module.
+ */
+
+/**
+ * Allows to prevent publication of a scheduled node.
+ *
+ * @param object $node
+ *   The scheduled node that is about to be published.
+ *
+ * @return bool
+ *   FALSE if the node should not be published. TRUE otherwise.
+ */
+function hook_scheduler_allow_publishing($node) {
+  $allowed = TRUE;
+
+  // Prevent publication of nodes that do not have the 'Approved for publication
+  // by the CEO' checkbox ticked.
+  if ($items = field_get_items('node', $node, 'field_approved')) {
+    $allowed = !empty($items[0]['value']);
+
+    // If publication is denied then inform the user why.
+    if (!$allowed) {
+      drupal_set_message(t('The content will only be published after approval by the CEO.'));
+    }
+  }
+
+  return $allowed;
+}
+
+/**
+ * Allows to prevent unpublication of a scheduled node.
+ *
+ * @param object $node
+ *   The scheduled node that is about to be unpublished.
+ *
+ * @return bool
+ *   FALSE if the node should not be unpublished. TRUE otherwise.
+ */
+function hook_scheduler_allow_unpublishing($node) {
+  $allowed = TRUE;
+
+  // Prevent unpublication of competition entries if not all prizes have been
+  // claimed.
+  if ($node->type == 'competition' && $items = field_get_items('node', $node, 'field_competition_prizes')) {
+    $allowed = (bool) count($items);
+
+    // If unpublication is denied then inform the user why.
+    if (!$allowed) {
+      drupal_set_message(t('The competition will only be unpublished after all prizes have been claimed by the winners.'));
+    }
+  }
+
+  return $allowed;
+}
---
+++ core/dslmcode/stacks/courses/sites/all/modules/local_contrib/scheduler/scheduler_vertical_tabs.js
@@ -0,0 +1,50 @@
+/**
+ * @file
+ * jQuery to provide summary information inside vertical tabs.
+ */
+
+(function ($) {
+
+/**
+ * Provide summary information for vertical tabs.
+ */
+Drupal.behaviors.scheduler_settings = {
+  attach: function (context) {
+
+    // Add the theme name as an additional class to the vertical-tabs div. This
+    // can then be used in scheduler.css to rectify the style for collapsible
+    // fieldsets where different themes need slightly different fixes. The theme
+    // is available in ajaxPageState.
+    var theme = Drupal.settings.ajaxPageState['theme'];
+    $('div.vertical-tabs').addClass(theme);
+
+    // Provide summary when editing a node.
+    $('fieldset#edit-scheduler-settings', context).drupalSetSummary(function(context) {
+      var vals = [];
+      if ($('#edit-publish-on').val() || $('#edit-publish-on-datepicker-popup-0').val()) {
+        vals.push(Drupal.t('Scheduled for publishing'));
+      }
+      if ($('#edit-unpublish-on').val() || $('#edit-unpublish-on-datepicker-popup-0').val()) {
+        vals.push(Drupal.t('Scheduled for unpublishing'));
+      }
+      if (!vals.length) {
+        vals.push(Drupal.t('Not scheduled'));
+      }
+      return vals.join('<br/>');
+    });
+
+    // Provide summary during content type configuration.
+    $('fieldset#edit-scheduler', context).drupalSetSummary(function(context) {
+      var vals = [];
+      if ($('#edit-scheduler-publish-enable', context).is(':checked')) {
+        vals.push(Drupal.t('Publishing enabled'));
+      }
+      if ($('#edit-scheduler-unpublish-enable', context).is(':checked')) {
+        vals.push(Drupal.t('Unpublishing enabled'));
+      }
+      return vals.join('<br/>');
+    });
+  }
+};
+
+})(jQuery);
