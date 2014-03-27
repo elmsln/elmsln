c5a037a63fbe138e0c63c18d1d899324308cbcd2
btopro <bto108@psu.edu>
2014-03-27 11:59:51 -0400

    lmsless branding bar cleaned up more tho still very very alpha in look and function

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/cis_lmsless/templates/cis-lmsless-bar.tpl.php
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/cis_lmsless/templates/cis-lmsless-bar.tpl.php
@@ -5,13 +5,13 @@
    * @todo dropdowns for available sections and system to switch to.
    */
 ?>
-<div class="cis-lmsless-bar <?php print $classes; ?>" <?php print $attributes; ?>>
-  <div class="cis-lmsless-home"><?php print l('HOME', $front_page); ?></div>
-  <div class="cis-lmsless-site-name"><?php print $site_name; ?></div>
- <div class="cis-lmsless-active-section"> <?php print $section; ?></div>
-  <select class="cis-lmsless-bar-services">
-    <?php foreach ($services as $service) : ?>
-      <option class="cis-lmsless-bar-service" <?php print $title_attributes; ?>><?php print l($service['title'], $service['url'] . base_path());?></option>
-    <?php endforeach ?>
+<div class="cis-lmsless-container cis-lmsless-bar <?php print $classes; ?>" <?php print $attributes; ?>>
+  <div class="cis-lmsless-item cis-lmsless-home"><?php print l('HOME', $front_page); ?></div>
+  <div class="cis-lmsless-item cis-lmsless-site-name"><?php print $site_name; ?></div>
+  <div class="cis-lmsless-item cis-lmsless-active-section"> <?php print $section; ?></div>
+  <select class="cis-lmsless-item cis-lmsless-bar-services">
+  <?php foreach ($services as $service) : ?>
+    <option class="cis-lmsless-bar-service" value="<?php print $service['url']; ?>"><?php print $service['title']; ?></option>
+  <?php endforeach ?>
   </select>
 </div>
