ec3983b48d8af277f048c981bd27fff4ea629e50
btopro <bto108@psu.edu>
2014-03-31 13:51:25 -0400

    cis-lmsless was missing previously from this package

---
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/cis_lmsless/templates/cis-lmsless-bar.tpl.php
@@ -0,0 +1,18 @@
+<?php
+  /**
+   * CIS LMS-less template file
+   *
+   * @todo dropdowns for available sections to switch.
+   * @todo replace home text with icon.
+   */
+?>
+<div class="cis-lmsless-container cis-lmsless-bar <?php print $classes; ?>" <?php print $attributes; ?>>
+  <div class="cis-lmsless-item cis-lmsless-home"><?php print l('HOME', $front_page); ?></div>
+  <div class="cis-lmsless-item cis-lmsless-site-name"><?php print $site_name; ?></div>
+  <div class="cis-lmsless-item cis-lmsless-active-section"> <?php print $section; ?></div>
+  <select class="cis-lmsless-item cis-lmsless-bar-services">
+  <?php foreach ($services as $service) : ?>
+    <option class="cis-lmsless-bar-service" value="<?php print $service['url']; ?>"><?php print $service['title']; ?></option>
+  <?php endforeach ?>
+  </select>
+</div>
