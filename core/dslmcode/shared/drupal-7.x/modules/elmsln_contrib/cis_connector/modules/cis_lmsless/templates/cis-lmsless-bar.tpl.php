2affb6373d798bf8e27de84bc48980272d9bd4f6
btopro <bto108@psu.edu>
2014-05-13 11:16:16 -0400

    theme flexibility and documentation for lmsless

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/cis_lmsless/templates/cis-lmsless-bar.tpl.php
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/cis_lmsless/templates/cis-lmsless-bar.tpl.php
@@ -4,6 +4,14 @@
    *
    * @todo dropdowns for available sections to switch.
    * @todo replace home text with icon.
+   * Available variables
+   * $services - an array containing the url and title of all other services
+   * the user currently has access to.
+   * $front_page - link to the front page of the current site
+   * $site_name - the name of the site
+   * $section - the active section of the current user
+   * $active - the part of the services array relating to the current system
+   * being worked with.
    */
 ?>
 <div class="cis-lmsless-container cis-lmsless-bar <?php print $classes; ?>" <?php print $attributes; ?>>
