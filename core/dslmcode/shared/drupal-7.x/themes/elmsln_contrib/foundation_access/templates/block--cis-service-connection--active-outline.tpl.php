94ac85336cdee2f1086072d169197eac701cb181
Michael Collins <mscoll85@gmail.com>
2014-06-16 09:55:39 -0400

    Left active outline sticky navigation

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/block--cis-service-connection--active-outline.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/block--cis-service-connection--active-outline.tpl.php
@@ -1,3 +1,3 @@
-
-			<div class="book-outline main-a"><?php print $content; ?></div>
+<div class="sticky-nav book-outline main-a"><?php print $content; ?></div>			
+<div class="book-outline sticky-book-outline"><?php print $content; ?></div>
 
