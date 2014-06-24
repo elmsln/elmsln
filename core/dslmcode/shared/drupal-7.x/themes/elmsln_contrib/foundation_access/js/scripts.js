7bb51f021518abbd8436352f945971b81afb547c
Michael Collins <mscoll85@gmail.com>
2014-06-24 14:14:01 -0400

    Sticky Menu jquery

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/scripts.js
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/scripts.js
@@ -1,29 +1,26 @@
 (function ($, Drupal) {
 
-  Drupal.behaviors.foundation_access = {
-    attach: function(context, settings) {
-      // Get your Yeti started.
-      $( ".accessibility-content-toggle a" ).appendTo( ".action-links-bar ul" ).wrap("<li></li>");
-      $( ".accessibility-content-toggle").hide();
+	Drupal.behaviors.foundation_access = {
+		attach: function(context, settings) {
+			// Get your Yeti started.
+			$( ".accessibility-content-toggle a" ).appendTo( ".action-links-bar ul" ).wrap("<li></li>");
+			$( ".accessibility-content-toggle").hide();
 
-      $(window).scroll(function(){ // scroll event  
+			$(window).scroll(function(){ // scroll event  
 		//sticky stuff from http://andrewhenderson.me/tutorial/jquery-sticky-sidebar/
-	    var topOffset = $('.book-outline.main-a').offset().top; // returns number
-	    var bottomOffset = $('.book-outline.main-a').outerHeight(true) + topOffset;
+      var topOffset = $('.book-outline.main-a').offset().top; // returns number
+			var bottomOffset = $('.book-outline.main-a').outerHeight(true) + topOffset;
 	    var windowTop = $(window).scrollTop(); // returns number
 	 
-	    if (bottomOffset < windowTop) { // If the bottom of the menu element is off screen
-	      $('.book-outline.sticky-book-outline').css({ position: 'fixed', top: 0});
-		  $('.book-outline.main-a');
+	    if (bottomOffset < windowTop) { // Go into sticky mode
+				$('.book-outline.sticky-book-outline').css({ position: 'fixed', top: 0, display:'block'});
+				$('.book-outline.main-a').css({display:'none'});
+			}
+	    else { // Do not go into sticky mode
+	      $('.book-outline.sticky-book-outline').css({ position: 'static', display:'none'});
+				$('.book-outline.main-a').css({display:'block'});
 	    }
-	    else { // If the bottom of the menu element is still on the screen
-	      $('.book-outline.sticky-book-outline').css({ position: 'static'});
-		  $('.book-outline.main-a');
-	    }
-	 
 	  });
-
-    }
-  };
-
+		}
+	};
 })(jQuery, Drupal);
