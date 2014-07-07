dc44266a59ff62a8fe3f5b9a36eb442c3a7c47f4
btopro <bto108@psu.edu>
2014-07-07 13:37:45 -0400

    new files added to piwik

---
+++ core/_nondrupal/piwik/js/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+define("PIWIK_KEEP_JS_TRACKER_COMMENT", 1);
+
+require_once __DIR__ . '/tracker.php';
