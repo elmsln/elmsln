7b8b0dc2ddc616b492ddc5144302ff88e95a8443
Michael Collins <mscoll85@gmail.com>
2014-07-08 10:30:31 -0400

    removed admin toolkit template

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/block--menu--menu-cis-toolkit.tpl.php
+++
@@ -1,9 +0,0 @@
-<div class="contain-to-grid sticky">
-	<nav class="top-bar" data-topbar data-options="sticky_on: large">
-		<ul class="title-area">
-		    <li class="name"><h1><a href="#">Link</a></h1></li>
-		     <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-		    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-		</ul>
-	</nav>
-</div>
