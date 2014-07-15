fbd21d419a05ff3ef52802b8a90f04e91d980e1d
Michael Collins <mscoll85@gmail.com>
2014-07-15 17:18:38 -0400

    Added active outline menu accordion functionality

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/scripts.js
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/scripts.js
@@ -3,24 +3,46 @@
 	Drupal.behaviors.foundation_access = {
 		attach: function(context, settings) {
 			// Get your Yeti started.
+			
+			// Active Outline Accordion Menu
+			// block--cis-service-connection--active-outline.tpl.php
+			$('.accordion').children().hide();
+			$('.accordion-btn').click(function () {
+				//toggle the dl
+				var $this = $(this);
+				$('.accordion').children().hide();
+				$this.next().children().toggle();
+			});
+
+			// Move accessibility link to action-links menu
 			$( ".accessibility-content-toggle a" ).appendTo( ".action-links-bar ul" ).wrap("<li></li>");
 			$( ".accessibility-content-toggle").hide();
 
 			$(window).scroll(function(){ // scroll event  
-		//sticky stuff from http://andrewhenderson.me/tutorial/jquery-sticky-sidebar/
-      var topOffset = $('.book-outline.main-a').offset().top; // returns number
-			var bottomOffset = $('.book-outline.main-a').outerHeight(true) + topOffset;
-	    var windowTop = $(window).scrollTop(); // returns number
-	 
-	    if (bottomOffset < windowTop) { // Go into sticky mode
-				$('.book-outline.sticky-book-outline').css({ position: 'fixed', top: 0, display:'block'});
-				$('.book-outline.main-a');
-			}
-	    else { // Do not go into sticky mode
-	      $('.book-outline.sticky-book-outline').css({ position: 'static', display:'none'});
-				$('.book-outline.main-a');
-	    }
-	  });
+			//sticky stuff from http://andrewhenderson.me/tutorial/jquery-sticky-sidebar/
+		      	var topOffset = $('.book-outline.main-a').offset().top; // returns number
+				var bottomOffset = $('.book-outline.main-a').outerHeight(true) + topOffset;
+			    var windowTop = $(window).scrollTop(); // returns number
+			 
+			    if (bottomOffset < windowTop) { // Go into sticky mode
+						$('.book-outline.sticky-book-outline').css({ position: 'fixed', top: 0, display:'block'});
+						$('.book-outline.main-a');
+					}
+			    else { // Do not go into sticky mode
+			      $('.book-outline.sticky-book-outline').css({ position: 'static', display:'none'});
+						$('.book-outline.main-a');
+			    }
+			});
+			// Accordion Menu
+			// Make a variable the hidden panels state
+			
+			// When
+			// $(‘.accordion-btn’).click(function() {
+			// 	var allPanels = $(‘.accordion’).hide();
+			// 	allPanels.slideUp();
+			// 	$(this).next().slideDown();
+			// 	return false;
+			// });
 		}
 	};
 })(jQuery, Drupal);
