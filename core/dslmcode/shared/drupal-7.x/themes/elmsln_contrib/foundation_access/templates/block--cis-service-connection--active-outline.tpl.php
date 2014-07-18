9e144575f1b8d357884f5ab1dd2249278ed16727
Michael Collins <mscoll85@gmail.com>
2014-07-18 13:11:36 -0400

    Menu Styling

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/block--cis-service-connection--active-outline.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/block--cis-service-connection--active-outline.tpl.php
@@ -1,34 +1,34 @@
-<ul>
+<ul class="menu book-oultine slide-panels">
   <li><a class="accordion-btn">Unit 1</a>
     <dl class="accordion" data-accordion="myAccordionGroup">
       <dd class="accordion-navigation">
         <a href="#panel1c">Lesson 1</a>
         <div id="panel1c" class="content">
-          <li><a href="#">Introduction<a></li>
-          <li><a href="#">Page 1<a></li>
-          <li><a href="#">Page 2<a></li>
-          <li><a href="#">Page 3<a></li>
-          <li><a href="#">Summary<a></li>
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
         </div>
       </dd>
       <dd class="accordion-navigation">
         <a href="#panel2c">Lesson 2</a>
         <div id="panel2c" class="content">
-          <li><a href="#">Introduction<a></li>
-          <li><a href="#">Page 1<a></li>
-          <li><a href="#">Page 2<a></li>
-          <li><a href="#">Page 3<a></li>
-          <li><a href="#">Summary<a></li>
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
         </div>
       </dd>
       <dd class="accordion-navigation">
         <a href="#panel3c">Lesson 3</a>
         <div id="panel3c" class="content">
-          <li><a href="#">Introduction<a></li>
-          <li><a href="#">Page 1<a></li>
-          <li><a href="#">Page 2<a></li>
-          <li><a href="#">Page 3<a></li>
-          <li><a href="#">Summary<a></li>
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
         </div>
       </dd>
     </dl>
@@ -39,31 +39,31 @@
       <dd class="accordion-navigation">
         <a href="#panel4c">Lesson 4</a>
         <div id="panel4c" class="content">
-          <li><a href="#">Introduction<a></li>
-          <li><a href="#">Page 1<a></li>
-          <li><a href="#">Page 2<a></li>
-          <li><a href="#">Page 3<a></li>
-          <li><a href="#">Summary<a></li>
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
         </div>
       </dd>
       <dd class="accordion-navigation">
         <a href="#panel5c">Lesson 5</a>
         <div id="panel5c" class="content">
-          <li><a href="#">Introduction<a></li>
-          <li><a href="#">Page 1<a></li>
-          <li><a href="#">Page 2<a></li>
-          <li><a href="#">Page 3<a></li>
-          <li><a href="#">Summary<a></li>
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
         </div>
       </dd>
       <dd class="accordion-navigation">
         <a href="#panel6c">Lesson 6</a>
         <div id="panel6c" class="content">
-          <li><a href="#">Introduction<a></li>
-          <li><a href="#">Page 1<a></li>
-          <li><a href="#">Page 2<a></li>
-          <li><a href="#">Page 3<a></li>
-          <li><a href="#">Summary<a></li>
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
         </div>
       </dd>
     </dl>
@@ -74,31 +74,31 @@
       <dd class="accordion-navigation">
         <a href="#panel7c">Lesson 7</a>
         <div id="panel7c" class="content">
-          <li><a href="#">Introduction<a></li>
-          <li><a href="#">Page 1<a></li>
-          <li><a href="#">Page 2<a></li>
-          <li><a href="#">Page 3<a></li>
-          <li><a href="#">Summary<a></li>
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
         </div>
       </dd>
       <dd class="accordion-navigation">
         <a href="#panel8c">Lesson 8</a>
         <div id="panel8c" class="content">
-          <li><a href="#">Introduction<a></li>
-          <li><a href="#">Page 1<a></li>
-          <li><a href="#">Page 2<a></li>
-          <li><a href="#">Page 3<a></li>
-          <li><a href="#">Summary<a></li>
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
         </div>
       </dd>
       <dd class="accordion-navigation">
         <a href="#panel9c">Lesson 9</a>
         <div id="panel9c" class="content">
-          <li><a href="#">Introduction<a></li>
-          <li><a href="#">Page 1<a></li>
-          <li><a href="#">Page 2<a></li>
-          <li><a href="#">Page 3<a></li>
-          <li><a href="#">Summary<a></li>
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
         </div>
       </dd>
     </dl>
