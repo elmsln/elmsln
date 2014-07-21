5aa3373700f3e43fad5372331febf9d2f071569e
Michael Collins <mscoll85@gmail.com>
2014-07-21 15:46:22 -0400

    Active Outline Menu Restructuring

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/block--cis-service-connection--active-outline.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/block--cis-service-connection--active-outline.tpl.php
@@ -1,7 +1,86 @@
+<!-- Default Menu -->
 <div class="sticky-nav book-outline main-a"><?php print $content; ?></div>
-<div class="book-outline sticky-book-outline">
-  <a href="#" data-dropdown="book-sticky-nav" class="button dropdown">
-    <?php print (($active = _cis_service_connection_active_outline()) ? $active->title : t('Lessons')); ?>
-  </a>
-  <? print '<ul id="book-sticky-nav" data-downdown-content class="f-dropdown nav nav-list">' . $content . '</ul>'; ?>
-</div>
+
+<!-- Active Outline -->
+<ul class="menu book-oultine slide-panels">
+  <li><a href='#unit-1' class="accordion-btn">Unit 1</a>
+    <dl class="accordion" data-accordion="myAccordionGroup">
+      <dd class="accordion-navigation">
+        <h3>Lesson 1</h3>
+            <a href="#" class='outline-sub-link small button'>Lesson Intro</a>
+            <a href="#panel1c" class='outline-sub-link expand small button'>Topics</a>
+        <div id="panel1c" class="content">
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
+        </div>
+      </dd>
+      <dd class="accordion-navigation">
+        <h3>Lesson 2</h3>
+        <a href="#" class='outline-sub-link small button'>Lesson Intro</a>
+        <a href="#panel2c" class='outline-sub-link expand small button'>Topics</a>
+        <div id="panel2c" class="content">
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
+        </div>
+      </dd>
+      <dd class="accordion-navigation">
+        <h3>Lesson 3</h3>
+        <a href="#" class='outline-sub-link small button'>Lesson Intro</a>
+        <a href="#panel3c" class='outline-sub-link expand small button'>Topics</a>
+        <div id="panel3c" class="content">
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
+        </div>
+      </dd>
+    </dl>
+  </li>
+  <li><a href='#unit-1' class="accordion-btn">Unit 2</a>
+    <dl class="accordion" data-accordion="myAccordionGroup">
+      <dd class="accordion-navigation">
+        <h3>Lesson 4</h3>
+        <a href="#" class='outline-sub-link small button'>Lesson Intro</a>
+        <a href="#panel4c" class='outline-sub-link expand small button'>Topics</a>
+        <div id="panel4c" class="content">
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
+        </div>
+      </dd>
+      <dd class="accordion-navigation">
+        <h3>Lesson 5</h3>
+        <a href="#" class='outline-sub-link small button'>Lesson Intro</a>
+        <a href="#panel5c" class='outline-sub-link expand small button'>Topics</a>
+        <div id="panel5c" class="content">
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
+        </div>
+      </dd>
+      <dd class="accordion-navigation">
+        <h3>Lesson 6</h3>
+        <a href="#" class='outline-sub-link small button'>Lesson Intro</a>
+        <a href="#panel6c" class='outline-sub-link expand small button'>Topics</a>
+        <div id="panel6c" class="content">
+          <li><a href="#">Introduction</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <li><a href="#">Summary</a></li>
+        </div>
+      </dd>
+    </dl>
+  </li>
+</ul>
