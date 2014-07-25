f64db206771ecc72322be19996bbd144a464925f
Michael Collins <mscoll85@gmail.com>
2014-07-25 11:04:53 -0400

    Active button query logic
    Selectors not quite working yet, but very close.

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/scripts.js
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/scripts.js
@@ -6,22 +6,33 @@
 			
 			// Active Outline Accordion Menu
 			// block--cis-service-connection--active-outline.tpl.php
-			$('.accordion').children().hide();
-			$('.accordion-btn').click(function () {
-				//toggle the dl
+			$('.accordion').hide(); //hide accordion items on page load
+			$('.accordion-btn').click(function() {//toggle the clicked dl item
 				var $this = $(this);
-				$('.accordion').children().hide();
-				$this.next().children().toggle();
+				//$('.accordion-btn').addClass('active');
+				$('.accordion').not($this.next()).hide(); // close all items except for current clicked item
+				//$('.accordion-btn').not($this.next()).removeClass('active');
+				$this.next().toggle(); // open or close cliked item
 			});
+
+
+
+
 
 			// Move accessibility link to action-links menu
 			$( ".accessibility-content-toggle a" ).appendTo( ".action-links-bar ul" ).wrap("<li></li>");
 			$( ".accessibility-content-toggle").hide();
 
 			$(window).scroll(function(){ // scroll event  
-			//sticky stuff from http://andrewhenderson.me/tutorial/jquery-sticky-sidebar/
-		      	var topOffset = $('.book-outline.main-a').offset().top; // returns number
-				var bottomOffset = $('.book-outline.main-a').outerHeight(true) + topOffset;
+
+
+				// find width and position of .main and apply to .sticky-content-nav
+				//var mainWidth = $('.main').outerWidth(true);
+
+
+				//sticky stuff from http://andrewhenderson.me/tutorial/jquery-sticky-sidebar/
+				var topOffset = $('.book-outline.main-a').offset().top; // returns number
+				var bottomOffset = $('.book-outline.main-a').outerHeight(true) + topOffset; // returns number
 			    var windowTop = $(window).scrollTop(); // returns number
 			 
 			    if (bottomOffset < windowTop) { // Go into sticky mode
@@ -32,17 +43,8 @@
 			      $('.book-outline.sticky-book-outline').css({ position: 'static', display:'none'});
 						$('.book-outline.main-a');
 			    }
-			});
-			// Accordion Menu
-			// Make a variable the hidden panels state
 			
-			// When
-			// $(‘.accordion-btn’).click(function() {
-			// 	var allPanels = $(‘.accordion’).hide();
-			// 	allPanels.slideUp();
-			// 	$(this).next().slideDown();
-			// 	return false;
-			// });
+			});
 		}
 	};
 })(jQuery, Drupal);
