35870061c01b0e6be1741a55a093c11acc1d85a6
Michael Collins <mscoll85@gmail.com>
2014-07-25 11:07:13 -0400

    Adjustments for breadcrumbs and active outline

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/page.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/page.tpl.php
@@ -200,12 +200,13 @@
               <div class="large-12 action-links-ui"><span class="point-a"></span><span class="point-b"></span></div>
             </div>
             <?php endif; ?> 
-          <?php if ($breadcrumb): print $breadcrumb; endif; ?>
+          
           <?php if ($title && !$is_front): ?>
             <?php print render($title_prefix); ?>
             <h1 id="page-title" class="title"><?php print $title; ?></h1>
             <?php print render($title_suffix); ?>
           <?php endif; ?>
+          <?php if ($breadcrumb): print $breadcrumb; endif; ?>
           <?php print render($page['content']); ?>
         </div>
         <!--/.main region -->
