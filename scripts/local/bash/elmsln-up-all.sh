56770a09aaf7e49cf6624cdd189851ce4d3343cd
btopro <bto108@psu.edu>
2014-08-20 11:49:08 -0400

    automating automation of automated stuff

--- scripts/local/bash/elmsln-up-all.sh
+++ scripts/local/bash/elmsln-up-all.sh
@@ -5,8 +5,40 @@
 # command against them. I use this every morning to keep all my galaxies
 # in sync across environments.
 
+#provide messaging colors for output to console
+txtbld=$(tput bold)             # Bold
+bldgrn=${txtbld}$(tput setaf 2) #  green
+bldred=${txtbld}$(tput setaf 1) #  green
+txtreset=$(tput sgr0)
+elmslnecho(){
+  echo "${bldgrn}$1${txtreset}"
+  return 1
+}
+elmslnwarn(){
+  echo "${bldred}$1${txtreset}"
+  return 1
+}
+# Define seconds timestamp
+timestamp(){
+  date +"%s"
+}
+
+start="$(timestamp)"
+elmslnecho "$(date +%T) job started"
 # read in file and execute commands against ssh'ed hosts
 while read line
 do
-    $line "bash ~/elmsln/scripts/upgrade/elmsln-upgrade-sites.sh master yes && drush @online status;" < /dev/null
+if [[ $line == *\#* ]]
+then
+  elmslnwarn "skipping this line as it is commented out: $line";
+else
+  elmslnecho "going to start working against $line"
+  # setup line, not required but can help w/ upgrades that hang
+  $line "cd ~/elmsln && git pull origin master && rm /tmp/elmsln-upgrade-lock" < /dev/null
+  # things are in place, run the actual command
+  $line "bash ~/elmsln/scripts/upgrade/elmsln-upgrade-sites.sh master yes && drush @online status;" < /dev/null
+fi
 done < "$1"
+
+end="$(timestamp)"
+elmslnecho "job took $(expr $end - $start) seconds to run"
