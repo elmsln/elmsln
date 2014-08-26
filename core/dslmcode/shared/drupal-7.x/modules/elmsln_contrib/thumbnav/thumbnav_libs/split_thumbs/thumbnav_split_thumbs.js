999ddf20c9f3e788b6558883344c53b04f8a7b33
btopro <bto108@psu.edu>
2014-08-26 14:53:24 -0400

    thumbnav check for properties before usage

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/thumbnav/thumbnav_libs/split_thumbs/thumbnav_split_thumbs.js
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/thumbnav/thumbnav_libs/split_thumbs/thumbnav_split_thumbs.js
@@ -1,11 +1,11 @@
 (function ($) {
   $(document).ready(function(){
     // display left controller if told to do so
-    if (Drupal.settings.thumbnav.left) {
+    if (typeof Drupal.settings.thumbnav.left != 'undefined' && Drupal.settings.thumbnav.left) {
       Drupal.thumbnav.leftControllerShow();
     }
     // display right controller if told to do so
-    if (Drupal.settings.thumbnav.right) {
+    if (typeof Drupal.settings.thumbnav.right != 'undefined' && Drupal.settings.thumbnav.right) {
       Drupal.thumbnav.rightControllerShow();
     }
   });
