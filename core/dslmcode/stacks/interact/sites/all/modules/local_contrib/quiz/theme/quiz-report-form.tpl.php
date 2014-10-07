ae2c2c1461cf9807e3f86a40db2ce0da23c2570b
btopro <bto108@psu.edu>
2014-10-07 12:57:03 -0400

    dropping ajax poll and 2 deprecated quiz question types; quiz to dev

--- core/dslmcode/stacks/interact/sites/all/modules/local_contrib/quiz/theme/quiz-report-form.tpl.php
+++ core/dslmcode/stacks/interact/sites/all/modules/local_contrib/quiz/theme/quiz-report-form.tpl.php
@@ -14,8 +14,6 @@
  * $form[x]['score'] - the users score on the current question.(FAPI array usually of type "markup" or "textfield")
  * $form[x]['max_score'] - the max score for the current question.(FAPI array of type "value")
  * $form[x]['response'] - the users response, usually a FAPI array of type markup.
- * $form[x]['#is_correct'] - If the users response is correct(boolean)
- * $form[x]['#is_evaluated'] - If the users response has been evaluated(boolean)
  */
 ?>
 <h2><?php print t('Question Results'); ?></h2>
