60f9c55aef2c17aabaa0d574ea36954b4934623b
btopro <bto108@psu.edu>
2014-10-16 15:38:13 -0400

    elmsln-up-all updated to support reporting systems

--- scripts/local/bash/elmsln-up-all.sh
+++ scripts/local/bash/elmsln-up-all.sh
@@ -37,23 +37,48 @@ if [ -z $3 ]; then
 fi
 
 start="$(timestamp)"
-elmslnecho "$(date +%T) job started"
+msg="Skynet online, initializing ELMSLN global upgrade at $(date +%T)"
+elmslnecho "${msg}"
+# optional property for channel reporting script
+if [ $4 ]; then
+  attachments=",\"attachments\": [{\"fallback\": \"Skynet upgrade status\",\"pretext\": \"Skynet upgrade status: start\",\"color\": \"warning\",\"fields\": [{\"title\": \"start\",\"value\": \"$start\",\"short\": false}]}]"
+  bash $4 "${msg}" "${attachments}"
+fi
 # read in file and execute commands against ssh'ed hosts
 while read line
 do
 if [[ $line == *\#* ]]
 then
-  elmslnwarn "skipping this line as it is commented out: $line";
+  #elmslnwarn "skipping this line as it is commented out: $line";
+  prevline=$line
 else
-  elmslnecho "going to start working against $line"
+  msg="$(timestamp) going to start working against '${prevline}'"
+  if [ $4 ]; then
+    attachments=",\"attachments\": [{\"fallback\": \"ELMSLN instance found, beginning upgrade\",\"pretext\": \"ELMSLN instance found, beginning upgrade\",\"color\": \"warning\",\"fields\": [{\"title\": \"start\",\"value\": \"$tmpstart\",\"short\": false}]}]"
+    bash $4 "${msg}" "${attachments}"
+  fi
+  elmslnecho "${msg}"
+  tmpstart="$(timestamp)"
   # setup line, not required but can help w/ upgrades that hang
   $line "cd ~/elmsln && git pull origin $2 && rm /tmp/elmsln-upgrade-lock" < /dev/null
   # reup the config directory
   $line "cd ~/elmsln/config && git pull origin $3" < /dev/null
   # things are in place, run the actual command
   $line "bash ~/elmsln/scripts/upgrade/elmsln-upgrade-sites.sh $2 yes && drush @online status;" < /dev/null
+  tmpend="$(timestamp)"
+  msg="Finished working on '${prevline}' in $(expr $tmpend - $tmpstart) seconds"
+  elmslnecho "${msg}"
+  if [ $4 ]; then
+    attachments=",\"attachments\": [{\"fallback\": \"Skynet: ELMSLN node upgrade status: complete\",\"pretext\": \"Skynet: ELMSLN node upgrade status: complete for ${prevline}\",\"color\": \"good\",\"fields\": [{\"title\": \"end\",\"value\": \"$tmpend\",\"short\": false},{\"title\": \"duration\",\"value\": \"$(expr $tmpend - $tmpstart) seconds\",\"short\": false}]}]"
+    bash $4 "${msg}" "${attachments}"
+  fi
 fi
 done < "$1"
 
 end="$(timestamp)"
-elmslnecho "job took $(expr $end - $start) seconds to run"
+msg="Skynet powering down, ELMSLN global upgrade complete. Upgrade took $(expr $end - $start) seconds to complete. Welcome to the future."
+elmslnecho "${msg}"
+if [ $4 ]; then
+  attachments=",\"attachments\": [{\"fallback\": \"Skynet upgrade status: complete\",\"pretext\": \"Skynet upgrade status: complete\",\"color\": \"good\",\"fields\": [{\"title\": \"start\",\"value\": \"$start\",\"short\": false},{\"title\": \"end\",\"value\": \"$end\",\"short\": false},{\"title\": \"duration\",\"value\": \"$(expr $end - $start) seconds\",\"short\": false}]}]"
+  bash $4 "${msg}" "${attachments}"
+fi
