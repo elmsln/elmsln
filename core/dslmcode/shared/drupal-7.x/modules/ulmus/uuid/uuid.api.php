45cef14a5961fc50b2bbaa8946cb74cb774118fb
btopro <bto108@psu.edu>
2014-10-21 10:41:01 -0400

    UUID updated to latest dev

--- core/dslmcode/shared/drupal-7.x/modules/ulmus/uuid/uuid.api.php
+++ core/dslmcode/shared/drupal-7.x/modules/ulmus/uuid/uuid.api.php
@@ -6,26 +6,6 @@
  */
 
 /**
- * Defines one or more UUID generators exposed by a module.
- *
- * @return
- *   An associative array with the key being the machine name for the
- *   implementation and the values being an array with the following keys:
- *     - title: The human readable name for the generator.
- *     - callback: The function to be called for generating the UUID.
- *
- * @see uuid_get_info()
- */
-function hook_uuid_info() {
-  $generators = array();
-  $generators['my_module'] = array(
-    'title' => t('My module UUID generator'),
-    'callback' => 'my_module_generate_uuid',
-  );
-  return $generators;
-}
-
-/**
  * Ensures all records have a UUID assigned to them.
  *
  * When called this hook should ensure all records it is responsible for
