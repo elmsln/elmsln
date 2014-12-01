a80f6a1b58a8b04e8840ef84b191df0f32e2565e
btopro <bto108@psu.edu>
2014-12-01 15:30:55 -0500

    dropping sticky for now

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/block--cis-service-connection--active-outline.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/block--cis-service-connection--active-outline.tpl.php
@@ -1,2 +1,2 @@
 <ul id="activeoutline" class="menu book-oultine slide-panels"><?php print $content; ?></ul>
-<ul id="activeoutline-sticky" class="menu book-oultine slide-panels sticky-bar"><?php print $content; ?></ul>
+<!-- <ul id="activeoutline-sticky" class="menu book-oultine slide-panels sticky-bar"><?php print $content; ?></ul> -->
