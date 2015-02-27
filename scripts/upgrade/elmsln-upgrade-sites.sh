e91b2ce64e3abf01c001ddafa796a04e3f22a08f
btopro <bto108@psu.edu>
2015-02-27 11:27:13 -0500

    shift to fix #34 awaiting travis testing

--- scripts/upgrade/elmsln-upgrade-sites.sh
+++ scripts/upgrade/elmsln-upgrade-sites.sh
@@ -27,6 +27,22 @@ if [ -z $elmsln ]; then
   exit 1
 fi
 
+# to decrease risk of a WSOD when there are significant shifts
+# under the hood, we should run RRs prior to the rest of the routine
+elmslnecho "Rebuilding registies and caches for all systems"
+drush @elmsln rr --y
+# run the safe upgrade of projects by taking the site offline then back on
+elmslnecho "Running update hooks"
+drush @elmsln cook dr_run_updates --y
+# run global upgrades from drup recipes
+drush @elmsln drup d7_elmsln_global ${elmsln}/scripts/upgrade/drush_recipes/d7/global --y
+# trigger crons to run now that these sites are all back and happy
+elmslnecho "Run crons as clean up"
+drush @elmsln cron --y
+# now loop through and prime the caches on everything
+elmslnecho "Seeding caches of all entities"
+drush @elmsln ecl --y
+
 # stacks we currently are supporting for these type of upgrades
 # @todo look into pushing this info off onto the cfg file as well
 # this way we can read from this source after they've already set the
@@ -35,17 +51,10 @@ fi
 # and in the future allow for clustered elmsln instances where parts of
 # the network are upgraded and managed locally per system install without
 # the possibility of having other stacks written into this one.
-standalone=('online' 'media' 'courses' 'studio' 'interact' 'blog')
-for stack in "${standalone[@]}"
+stacklist=('online' 'media' 'courses' 'studio' 'interact' 'blog' 'discuss' 'comply')
+for stack in "${stacklist[@]}"
 do
-  elmslnecho "working against $stack"
-  # run the safe upgrade of projects by taking the site offline then back on
-  drush @${stack}-all cook dr_safe_upgrade --y
-  # run global upgrades
-  drush @${stack}-all drup d7_elmsln_global ${elmsln}/scripts/upgrade/drush_recipes/d7/global --y
-  # run stack specific upgrades
+  elmslnecho "Applying specific upgrades against $stack"
+  # run stack specific upgrades if they exist
   drush @${stack}-all drup d7_elmsln_${stack}-all ${elmsln}/scripts/upgrade/drush_recipes/d7/${stack}-all --y
 done
-# this only makes sense for online
-elmslnecho "online: seed entity / front facing caches"
-drush @online-all hss --y
