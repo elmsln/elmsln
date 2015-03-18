98026a5912fd9f1cd50cf2fbd2e704e5664db6c6
btopro <bto108@psu.edu>
2015-03-18 14:00:16 -0400

    handsfree for later usage

--- scripts/install/handsfree/handsfree-install.sh
+++ scripts/install/handsfree/handsfree-install.sh
@@ -17,7 +17,26 @@ elmslnwarn(){
   echo "${bldred}$1${txtreset}"
 }
 
-# where am i? move to where I am. This ensures source is properly sourced
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
+# @todo, assume this system has 0 configuration ahead of time
+# we are going to setup the server in 1 script and then do the
+# rest of getting elmsln in place here
+#
+# probably can clone nittany-vagrant and then use those scripts
+# to establish the baseline for the server components. From there
+# we can generate a random root password, then use that to request
+# a new elmsdbo user/password
+# that should be about all we need to set things up minus obviously
+# needing some kind of arguments piped in for the few things
+# that are actually specific at that point.
+#cd ~
+# @todo pipe in values
+#bash /var/www/elmsln/scripts/install/root/elmsln-preinstall.sh
+# copy and paste this
+#cd ~
+#rm -rf ~/.drush
+# replace YOU with your username and root with whatever group you want
+# to own the system. If not sure leave the second value as root though
+# admin is common as well.
+#sudo chown -R root:root /var/www/elmsln
 
+#bash /var/www/elmsln/scripts/install/elmsln-install.sh
