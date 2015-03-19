ae801c5c38c19a291f57d2af3a272f240b122ce3
btopro <bto108@psu.edu>
2015-03-18 22:59:07 -0400

    initial bulk of the centos-install from blank

---
+++ scripts/install/handsfree/centos/centos-install.sh
@@ -0,0 +1,26 @@
+# a script to install server dependencies
+
+# using yum to install the main packages
+yes | yum -y install patch git nano gcc make mysql mysql-server httpd php php-gd php-xml php-pdo php-mbstring php-mysql php-pear php-devel php-pecl-ssh2 php-pecl-apc
+
+yes | yum groupinstall 'Development Tools'
+
+# using pecl to install uploadprogress
+pecl channel-update pecl.php.net
+# uploadprogress to get rid of that warning
+pecl install uploadprogress
+
+# adding uploadprogresss to php conf files
+touch /etc/php.d/uploadprogress.ini
+echo extension=uploadprogress.so > /etc/php.d/uploadprogress.ini
+
+# set httpd_can_sendmail so drupal mails go out
+setsebool -P httpd_can_sendmail on
+# run the handsfree installer that's the same for all deployments
+# todo pipe in values making this specific to the type of server its on
+mkdir -p /var/www/elmsln
+cd /var/www
+git clone https://github.com/btopro/elmsln.git elmsln
+sudo mysql_install_db
+sudo mysql_secure_installation
+bash /var/www/elmsln/scripts/install/handsfree-install.sh
