c37595775bdd5e518e51916b094803a8f785b9be
btopro <bto108@psu.edu>
2015-03-18 22:58:55 -0400

    simplifying handsfree installer

--- scripts/install/handsfree/handsfree-install.sh
+++ scripts/install/handsfree/handsfree-install.sh
@@ -5,38 +5,19 @@
 # and the entire box is built to exactly what it needs to be from the
 # ground up
 
-# provide messaging colors for output to console
-txtbld=$(tput bold)             # Bold
-bldgrn=$(tput setaf 2) #  green
-bldred=${txtbld}$(tput setaf 1) #  red
-txtreset=$(tput sgr0)
-elmslnecho(){
-  echo "${bldgrn}$1${txtreset}"
-}
-elmslnwarn(){
-  echo "${bldred}$1${txtreset}"
-}
-
-# @todo, assume this system has 0 configuration ahead of time
-# we are going to setup the server in 1 script and then do the
-# rest of getting elmsln in place here
-#
-# probably can clone nittany-vagrant and then use those scripts
-# to establish the baseline for the server components. From there
-# we can generate a random root password, then use that to request
-# a new elmsdbo user/password
-# that should be about all we need to set things up minus obviously
-# needing some kind of arguments piped in for the few things
-# that are actually specific at that point.
-#cd ~
+# this script assumes ELMSLN code base is on the server but that's
+# about it. See one of the server installs in the hands free directory
+cd $HOME
 # @todo pipe in values
-#bash /var/www/elmsln/scripts/install/root/elmsln-preinstall.sh
+bash /var/www/elmsln/scripts/install/root/elmsln-preinstall.sh
 # copy and paste this
-#cd ~
-#rm -rf ~/.drush
+cd $HOME
+rm -rf $HOME/.drush
 # replace YOU with your username and root with whatever group you want
 # to own the system. If not sure leave the second value as root though
 # admin is common as well.
-#sudo chown -R root:root /var/www/elmsln
-
-#bash /var/www/elmsln/scripts/install/elmsln-install.sh
+chown -R root:root /var/www/elmsln
+# setup user as admin
+bash /var/www/elmsln/scripts/install/users/elmsln-admin-user.sh
+# install system and off we go
+bash /var/www/elmsln/scripts/install/elmsln-install.sh
