7d4c7ab8ec7da86308b5d54901c93ad251081527
btopro <bto108@psu.edu>
2015-04-01 12:28:17 -0400

    working on grade book module

--- core/dslmcode/profiles/cle-7.x-1.x/modules/cle_custom/cle_gradebook/js/cle_gradebook.js
+++ core/dslmcode/profiles/cle-7.x-1.x/modules/cle_custom/cle_gradebook/js/cle_gradebook.js
@@ -7,7 +7,7 @@ Drupal.theme.prototype.cle_gradebook_modal = function () {
   html += '<div id="ctools-modal" class="popups-box">';
   html += '  <div class="ctools-modal-content ctools-modal-cle-gradebook-modal-content">';
   html += '    <span class="popups-close"><a class="close" href="#">' + Drupal.CTools.Modal.currentSettings.closeImage + '</a></span>';
-  html += '    <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
+  html += '    <div><div id="modal-content" class="expand reveal-modal-open modal-content popups-body"></div></div>';
   html += '  </div>';
   html += '</div>';
   return html;
