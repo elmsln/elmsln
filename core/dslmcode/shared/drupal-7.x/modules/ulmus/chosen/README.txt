f5acfdcbf058c1a0e04ed23bb9912119696e5d4d
btopro <bto108@psu.edu>
2015-04-14 12:58:05 -0400

    chosen

---
+++ core/dslmcode/shared/drupal-7.x/modules/ulmus/chosen/README.txt
@@ -0,0 +1,32 @@
+-- SUMMARY --
+
+Chosen uses the Chosen jQuery plugin to make your <select> elements more user-friendly.
+
+
+-- INSTALLATION --
+
+  1. Download the Chosen jQuery plugin (http://harvesthq.github.io/chosen/ version 1.1.0 is recommended) and extract the file under sites/all/libraries.
+  2. Download and enable the module.
+  3. Configure at Administer > Configuration > User interface > Chosen (requires administer site configuration permission)
+
+-- INSTALLATION VIA DRUSH --
+
+ A Drush command is provided for easy installation of the Chosen plugin.
+
+ drush chosenplugin
+
+ The command will download the plugin and unpack it in "sites/all/libraries".
+ It is possible to add another path as an option to the command, but not
+ recommended unless you know what you are doing.
+
+-- ACCESSIBILITY CONCERN --
+
+There are accessibility problems with the main library as identified here:
+	https://github.com/harvesthq/chosen/issues/264
+
+-- TROUBLE SHOOTING --
+
+  How to exlude a select field from becoming a chosen select.
+    - go to the configuration page and add your field using the jquery "not"
+      operator to the textarea with the comma seperated values.
+      For date fields this could look like: select:not([name*='day'],[name*='year'],[name*='month'])
---
+++ core/dslmcode/shared/drupal-7.x/modules/ulmus/chosen/chosen.js
@@ -0,0 +1,64 @@
+(function($) {
+  Drupal.behaviors.chosen = {
+    attach: function(context, settings) {
+      settings.chosen = settings.chosen || Drupal.settings.chosen;
+
+      // Prepare selector and add unwantend selectors.
+      var selector = settings.chosen.selector;
+
+      // Function to prepare all the options together for the chosen() call.
+      var getElementOptions = function (element) {
+        var options = $.extend({}, settings.chosen.options);
+
+        // The width default option is considered the minimum width, so this
+        // must be evaluated for every option.
+        if ($(element).width() < settings.chosen.minimum_width) {
+          options.width = settings.chosen.minimum_width + 'px';
+        }
+        else {
+          options.width = $(element).width() + 'px';
+        }
+
+        // Some field widgets have cardinality, so we must respect that.
+        // @see chosen_pre_render_select()
+        if ($(element).attr('multiple') && $(element).data('cardinality')) {
+          options.max_selected_options = $(element).data('cardinality');
+        }
+
+        return options;
+      };
+
+      // Process elements that have opted-in for Chosen.
+      // @todo Remove support for the deprecated chosen-widget class.
+      $('select.chosen-enable, select.chosen-widget', context).once('chosen', function() {
+        options = getElementOptions(this);
+        $(this).chosen(options);
+      });
+
+      $(selector, context)
+        // Disabled on:
+        // - Field UI
+        // - WYSIWYG elements
+        // - Tabledrag weights
+        // - Elements that have opted-out of Chosen
+        // - Elements already processed by Chosen
+        .not('#field-ui-field-overview-form select, #field-ui-display-overview-form select, .wysiwyg, .draggable select[name$="[weight]"], .draggable select[name$="[position]"], .chosen-disable, .chosen-processed')
+        .filter(function() {
+          // Filter out select widgets that do not meet the minimum number of
+          // options.
+          var minOptions = $(this).attr('multiple') ? settings.chosen.minimum_multiple : settings.chosen.minimum_single;
+          if (!minOptions) {
+            // Zero value means no minimum.
+            return true;
+          }
+          else {
+            return $(this).find('option').length >= minOptions;
+          }
+        })
+        .once('chosen', function() {
+          options = getElementOptions(this);
+          $(this).chosen(options);
+        });
+    }
+  };
+})(jQuery);
