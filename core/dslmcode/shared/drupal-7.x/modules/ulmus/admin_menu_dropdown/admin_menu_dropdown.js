27564334e926f4a6e80f7595e2b74e7fe466e0cf
btopro <bto108@psu.edu>
2015-04-17 17:58:02 -0400

    dropdown aggregation fix

--- core/dslmcode/shared/drupal-7.x/modules/ulmus/admin_menu_dropdown/admin_menu_dropdown.js
+++ core/dslmcode/shared/drupal-7.x/modules/ulmus/admin_menu_dropdown/admin_menu_dropdown.js
@@ -1,15 +1,33 @@
 // $Id$
 
+/**
+ * Admin menu dropdown behavior.
+ */
 (function($){
-if (Drupal.settings.admin_menu_dropdown.default) {
-  $(document).ready(function(){$('#admin-menu').hide();$('body').addClass('adm_menu_hidden');});
-}
-$(document).keypress(function(e) {
-  var unicode=e.keyCode? e.keyCode : e.charCode;
-  if (String.fromCharCode(unicode)==Drupal.settings.admin_menu_dropdown.key){
-    $('#admin-menu').slideToggle('fast');
-    // TODO: Maybe animate the margin change so its not so jumpy?
-    $('body').toggleClass('adm_menu_hidden');
+  Drupal.behaviors.adminMenuDropDown = {
+    attach: function (context, settings) {
+      // Initialize settings.
+      settings.admin_menu_dropdown = $.extend({
+        onload: false,
+        key: '`'
+      }, settings.admin_menu_dropdown || {});
+
+      $('#admin-menu').once(function () {
+        // Attach to keypress event.
+        $(document).keypress(function(e) {
+          var unicode = e.keyCode? e.keyCode : e.charCode;
+          if (String.fromCharCode(unicode) == settings.admin_menu_dropdown.key) {
+            $('#admin-menu').slideToggle('fast');
+            // TODO: Maybe animate the margin change so its not so jumpy?
+            $('body').toggleClass('adm_menu_hidden');
+          }
+        });
+        if (settings.admin_menu_dropdown.onload) {
+          // Initially hide admin menu.
+          $(this).hide();
+          $('body').addClass('adm_menu_hidden');
+        }
+      });
+    }
   }
-});
 })(jQuery);
