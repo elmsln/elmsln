003a24365eae1320aae49ad9e393769a17ec3009
btopro <bto108@psu.edu>
2015-04-30 06:58:06 -0400

    studio features re-exported

--- core/dslmcode/profiles/cle-7.x-1.x/modules/features/cle_submission/js/assignment_listing.js
+++ core/dslmcode/profiles/cle-7.x-1.x/modules/features/cle_submission/js/assignment_listing.js
@@ -6,14 +6,14 @@
   Drupal.behaviors.cleTileWrapper = {
     attach: function(context, settings) {
       $('.cle-tile-wrapper', context).hover(function(){
-      // on hovering a tile
-      $(this).addClass('cle-visible-tile');
-      $(this).find('.views-field-title, .views-field-comment-count, .views-field-value').slideDown(200, 'linear');
-    }, function(){
-      // off a tile
-      $(this).removeClass('cle-visible-tile');
-      $(this).find('.views-field-title, .views-field-comment-count, .views-field-value').delay(800).slideUp(400, 'linear');
-    });
+			// on hovering a tile
+			$(this).addClass('cle-visible-tile');
+			$(this).find('.views-field-title, .views-field-comment-count, .views-field-value').slideDown(200, 'linear');
+		}, function(){
+		  // off a tile
+			$(this).removeClass('cle-visible-tile');
+			$(this).find('.views-field-title, .views-field-comment-count, .views-field-value').delay(800).slideUp(400, 'linear');
+		});
     }
   };
 })(jQuery);
