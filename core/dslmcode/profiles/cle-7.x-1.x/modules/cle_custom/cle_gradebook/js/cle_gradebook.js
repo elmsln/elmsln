e6d13b0819ae2016bca4775514ef6c62d315d5f9
btopro <bto108@psu.edu>
2015-04-30 06:38:58 -0400

    grade book updates

--- core/dslmcode/profiles/cle-7.x-1.x/modules/cle_custom/cle_gradebook/js/cle_gradebook.js
+++ core/dslmcode/profiles/cle-7.x-1.x/modules/cle_custom/cle_gradebook/js/cle_gradebook.js
@@ -2,14 +2,34 @@
 * Provide the HTML to create the modal dialog.
 */
 (function ($) {
-Drupal.theme.prototype.cle_gradebook_modal = function () {
-  var html = '';
-  html += '<div id="ctools-modal" class="popups-box">';
-  html += '  <div class="ctools-modal-content ctools-modal-cle-gradebook-modal-content">';
-  html += '    <span class="popups-close"><a class="close" href="#">' + Drupal.CTools.Modal.currentSettings.closeImage + '</a></span>';
-  html += '    <div><div id="modal-content" class="expand reveal-modal-open modal-content popups-body"></div></div>';
-  html += '  </div>';
-  html += '</div>';
-  return html;
-}
+  Drupal.theme.prototype.cle_gradebook_modal = function () {
+    var html = '<div id="ctools-modal" class="popups-box">';
+    html += '  <div class="ctools-modal-content ctools-modal-cle-gradebook-modal-content">';
+    html += '    <span class="popups-close"><a class="close ctools-modal-cle-gradebook-close" href="#">' + Drupal.CTools.Modal.currentSettings.closeImage + '</a></span>';
+    html += '    <div><div id="modal-content" class="expand reveal-modal-open modal-content popups-body"></div></div>';
+    html += '  </div>';
+    html += '</div>';
+    return html;
+  }
+  // ability to disable background scrolling on modal open
+  Drupal.behaviors.gradebookBodyScrollDisable = {
+  attach: function (context, settings) {
+    $('.ctools-modal-cle-gradebook-close').on("click", function () {
+      $("body").removeClass("scroll-disabled")
+    });
+  }
+  };
+  // bind these events only once
+  $(document).ready(function(){
+    $('.ctools-modal-cle-gradebook-modal.disable-scroll').on("click", function () {
+      $("body").addClass("scroll-disabled");
+    });
+        // Bind a keypress on escape for closing the modalContent
+    $(document).bind('keydown', function(event) {
+      if (event.keyCode == 27) {
+        $("body").removeClass("scroll-disabled")
+        return false;
+      }
+    });
+  });
 })(jQuery);
