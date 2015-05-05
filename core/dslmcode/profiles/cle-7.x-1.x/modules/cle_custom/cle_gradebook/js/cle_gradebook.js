9b3787bc7668ab5389887793a4ba977f36098bf5
btopro <bto108@psu.edu>
2015-05-04 21:45:43 -0400

    gradebook ajax js side

--- core/dslmcode/profiles/cle-7.x-1.x/modules/cle_custom/cle_gradebook/js/cle_gradebook.js
+++ core/dslmcode/profiles/cle-7.x-1.x/modules/cle_custom/cle_gradebook/js/cle_gradebook.js
@@ -19,6 +19,48 @@
     });
   }
   };
+  // ajax submit cause ctools apparently doesn't support this w/ DS
+  Drupal.behaviors.gradebookAssessmentSubmit = {
+    'attach': function(context) {
+      // checkboxes need prop set to be displayed dynamically
+      if ($('#modal-content input[name=flag_submission_value]').val() == 1) {
+        $('#modal-content input[name=flag_submission]').prop('checked', true);
+      }
+      $('#modal-content input[name=flag_submission]').val($('#modal-content input[name=flag_submission_value]').val());
+      // select state
+      $('#modal-content select[name=assessment_state]').val($('#modal-content input[name=assessment_state_value]').val());
+      // set point value
+      $('#modal-content input[name=assessment]').val($('#modal-content input[name=assessment_value]').val());
+      // bind click event to fake the submission via ajax and update what we have to
+      $('#modal-content .form-submit', context).bind('click', function() {
+        // structure the data
+        var data = {
+          flag: $('#modal-content input[name=flag_submission]').val(),
+          state: $('#modal-content select[name=assessment_state]').val(),
+          points: $('#modal-content input[name=assessment]').val()
+        };
+        // kick off the ajax request to update the values with those selected
+        $.ajax({
+          url: Drupal.settings.basePath + 'cle/gradebook/ajax/' + Drupal.settings.cleGradebookToken + '/' + $('#modal-content input[name=aid]').attr('value') + '/' + $('#modal-content input[name=submission]').attr('value'),
+          type: 'POST',
+          data: data,
+        })
+        .done(function(data) {
+          var returned = jQuery.parseJSON(data);
+          // update the grid where we have a match from the data returned
+          sublight = '.cle-submission-' + $('#modal-content input[name=submission]').attr('value');
+          $(sublight).attr('src', $(sublight).attr('src').replace($('#modal-content input[name=assessment_state_value]').val(), returned.state));
+          // close window
+          $('.ctools-modal-cle-gradebook-close').click();
+        })
+        .fail(function(data) {
+          console.log(data);
+          alert(Drupal.t('Save failed'));
+        });
+        return false;
+      });
+    }
+  };
   // bind these events only once
   $(document).ready(function(){
     $('.ctools-modal-cle-gradebook-modal.disable-scroll').on("click", function () {
