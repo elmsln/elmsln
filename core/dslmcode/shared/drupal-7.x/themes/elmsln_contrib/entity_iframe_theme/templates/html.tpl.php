b62369e51fabdcc900617003df4d599f1e776339
btopro <bto108@psu.edu>
2015-05-13 16:29:11 -0400

    über minimalist on this theme; that's the point

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/entity_iframe_theme/templates/html.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/entity_iframe_theme/templates/html.tpl.php
@@ -16,9 +16,6 @@
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-  </div>
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
