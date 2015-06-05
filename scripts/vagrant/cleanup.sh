b14049b51c440362c4465dab885cb6b88948da7f
btopro <bto108@psu.edu>
2015-06-05 11:18:46 -0400

    need root to write this part during install since its a clean up function

--- scripts/vagrant/cleanup.sh
+++ scripts/vagrant/cleanup.sh
@@ -11,23 +11,3 @@ sudo bash /var/www/elmsln/scripts/utilities/harden-security.sh vagrant
 # restart apache / mysql just to be safe
 sudo /etc/init.d/httpd restart
 sudo /etc/init.d/mysqld restart
-
-# add in checks to ensure apache/mysql haven't stopped
-# when we SSH into the box. For the purposes of drupal
-# and vagrant, these services should NEVER be stopped
-# though there appears to be a possible glitch with
-# vagrant and core services on the VM starting correctly
-# when ever networking is screwed up
-echo '#!/bin/bash' >> /etc/profile.d/chkon.sh
-echo 'mysql=$(sudo /etc/init.d/mysqld status)' >> /etc/profile.d/chkon.sh
-echo 'httpd=$(sudo /etc/init.d/httpd status)' >> /etc/profile.d/chkon.sh
-# test for mysql
-echo 'if [[ $mysql == *"mysqld is stopped"* ]]' >> /etc/profile.d/chkon.sh
-echo 'then' >> /etc/profile.d/chkon.sh
-echo '  sudo /etc/init.d/mysqld start' >> /etc/profile.d/chkon.sh
-echo 'fi' >> /etc/profile.d/chkon.sh
-# test for apache
-echo 'if [[ $httpd == *"httpd is stopped"* ]]' >> /etc/profile.d/chkon.sh
-echo 'then' >> /etc/profile.d/chkon.sh
-echo '  sudo /etc/init.d/httpd start' >> /etc/profile.d/chkon.sh
-echo 'fi' >> /etc/profile.d/chkon.sh
--- scripts/vagrant/handsfree-vagrant.sh
+++ scripts/vagrant/handsfree-vagrant.sh
@@ -13,8 +13,29 @@ cp -R /var/www/elmsln-config-vagrant/shared/drupal-7.x/modules/vagrant /var/www/
 rm /var/www/elmsln/config/shared/drupal-7.x/settings/shared_settings.php
 cp /var/www/elmsln-config-vagrant/shared/drupal-7.x/settings/shared_settings.php /var/www/elmsln/config/shared/drupal-7.x/settings/shared_settings.php
 
+# some minor clean up that we need to do via sudo
 # setup host file so httprl works for local cache rebuilding
 cat /var/www/elmsln/scripts/vagrant/hosts >> /etc/hosts
+# add in checks to ensure apache/mysql haven't stopped
+# when we SSH into the box. For the purposes of drupal
+# and vagrant, these services should NEVER be stopped
+# though there appears to be a possible glitch with
+# vagrant and core services on the VM starting correctly
+# when ever networking is screwed up
+echo '#!/bin/bash' >> /etc/profile.d/chkon.sh
+echo 'mysql=$(sudo /etc/init.d/mysqld status)' >> /etc/profile.d/chkon.sh
+echo 'httpd=$(sudo /etc/init.d/httpd status)' >> /etc/profile.d/chkon.sh
+# test for mysql
+echo 'if [[ $mysql == *"mysqld is stopped"* ]]' >> /etc/profile.d/chkon.sh
+echo 'then' >> /etc/profile.d/chkon.sh
+echo '  sudo /etc/init.d/mysqld start' >> /etc/profile.d/chkon.sh
+echo 'fi' >> /etc/profile.d/chkon.sh
+# test for apache
+echo 'if [[ $httpd == *"httpd is stopped"* ]]' >> /etc/profile.d/chkon.sh
+echo 'then' >> /etc/profile.d/chkon.sh
+echo '  sudo /etc/init.d/httpd start' >> /etc/profile.d/chkon.sh
+echo 'fi' >> /etc/profile.d/chkon.sh
+
 
 # vagrant specific stuff
 drush @online dis seckit --y
