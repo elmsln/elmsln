0102db9d49409a17d1747821ed54d53e7d6e094c
btopro <bto108@psu.edu>
2015-06-08 16:55:53 -0400

    FA needs to run out of CIS 2.x instances

--- core/dslmcode/profiles/cis-7.x-2.x/themes/cis_foundation_access/template.php
+++ core/dslmcode/profiles/cis-7.x-2.x/themes/cis_foundation_access/template.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Implements menu_link__main_menu.
+ * Implements menu_link__menu_cis_toolkit.
  */
 function cis_foundation_access_menu_link__menu_cis_toolkit(&$variables) {
   return _foundation_access_menu_outline($variables);
 }
 
 /**
- * Implements menu_tree__main_menu.
+ * Implements menu_tree__menu_cis_toolkit.
  */
 function cis_foundation_access_menu_tree__menu_cis_toolkit($variables) {
   return '<ul class="off-canvas-list has-submenu content-outline-navigation">' . $variables['tree'] . '</ul>';
