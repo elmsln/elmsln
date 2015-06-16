0c7aa3654f8be97e0c682fb3fb13a375b4f1bf17
btopro <bto108@psu.edu>
2015-06-16 15:31:57 -0400

    need to move to active directory first

--- scripts/elmsln.sh
+++ scripts/elmsln.sh
@@ -2,6 +2,10 @@
 # elmsln.sh is intended to be an interactive prompt for administering elmsln
 # this provides shortcuts for running commands you could have otherwise
 # but like the developers of the project, are far too lazy to search for.
+
+# where am i? move to where I am. This ensures source is properly sourced
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 source ../config/scripts/drush-create-site/config.cfg
 
 #provide messaging colors for output to console
