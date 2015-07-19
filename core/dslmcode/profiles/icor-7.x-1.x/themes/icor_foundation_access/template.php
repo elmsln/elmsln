c4fe6f5c93a2362274a0b1c6b4521ea3bd6fbfdb
btopro <bto108@psu.edu>
2015-07-19 11:07:38 -0400

    icor ux consolidated and imlpemented

--- core/dslmcode/profiles/icor-7.x-1.x/themes/icor_foundation_access/template.php
+++ core/dslmcode/profiles/icor-7.x-1.x/themes/icor_foundation_access/template.php
@@ -2,13 +2,13 @@
 /**
  * Implements menu_link__main_menu.
  */
-function icor_foundation_access_menu_link__menu_icor_toolkit(&$variables) {
+function icor_foundation_access_menu_link__menu_icor_navigation(&$variables) {
   return _foundation_access_menu_outline($variables);
 }
 
 /**
  * Implements menu_tree__main_menu.
  */
-function icor_foundation_access_menu_tree__menu_icor_toolkit($variables) {
+function icor_foundation_access_menu_tree__menu_icor_navigation($variables) {
   return '<ul class="off-canvas-list has-submenu content-outline-navigation">' . $variables['tree'] . '</ul>';
 }
