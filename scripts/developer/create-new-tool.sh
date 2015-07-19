125b11859e9e973ee43b984b9a245b9941da476c
btopro <bto108@psu.edu>
2015-07-19 15:07:21 -0400

    its working!

--- scripts/developer/create-new-tool.sh
+++ scripts/developer/create-new-tool.sh
@@ -45,18 +45,14 @@ if [ -z $3 ]; then
 fi
 # test for an argument as to what user to write as
 if [ -z $4 ]; then
-  elmslnwarn "You must supply the type of tool this is, authority or instance?"
+  elmslnwarn "You must supply the type of tool this is, authority or service?"
   exit 1
 fi
 domain=$1
 distro=$2
 version=$3
 tooltype=$4
-# check that this domain exists as a stack, otherwise error out
-if [ ! -d "$elmsln/core/dslmcode/stacks/$domain" ]; then
-  elmslnwarn "This domain must already exist if we are to correctly discover it."
-  exit 1
-fi
+
 # check that the config doesn't already exist in the example directory
 if [ ! -d "$configsdir/stacks/$domain" ]; then
   # get structure in place for stack config
@@ -78,35 +74,42 @@ if [ ! -d "$elmsln/core/dslmcode/profiles/${distro}-${version}" ]; then
   cd "$elmsln/core/dslmcode/profiles"
   cp -R ulmus-7.x-1.x "${distro}-${version}"
   cd "${distro}-${version}"
-  renames=('local.make.example' 'drecipes/elmsln_ulmus.drecipe' 'ulmus.info' 'ulmus.install' 'ulmus.profile' 'themes/SUB_foundation_access/SUB_foundation_access.info' 'themes/SUB_foundation_access/css/SUB_styles.css' 'themes/SUB_foundation_access/template.php')
+  renames=('.travis.yml' 'drupal-org.make' 'local.make.example' 'drecipes/elmsln_ulmus.drecipe' 'ulmus.info' 'ulmus.install' 'ulmus.profile' 'themes/SUB_foundation_access/SUB_foundation_access.info' 'themes/SUB_foundation_access/css/SUB_styles.css' 'themes/SUB_foundation_access/template.php')
   for rename in "${renames[@]}"
     do
-    sed -i "/\ulmus/a \ \t $distro" $rename
-    sed -i "/\SUB/a \ \t $distro" $rename
+    sed -i '' "s/ulmus/$distro/g" $rename
+    sed -i '' "s/SUB/$distro/g" $rename
   done
-  find . -name 'ulmus*' -type f -exec bash -c 'mv "$1" "${1/\/ulmus/$distro/}"' -- {} \;
-  find . -name 'SUB*' -type f -exec bash -c 'mv "$1" "${1/\/SUB/$distro/}"' -- {} \;
+  mv ulmus.info $distro.info
+  mv ulmus.profile $distro.profile
+  mv ulmus.install $distro.install
+  mv drecipes/elmsln_ulmus.drecipe "drecipes/elmsln_${distro}.drecipe"
+  mv themes/SUB_foundation_access/css/SUB_styles.css "themes/SUB_foundation_access/css/${distro}_styles.css"
+  mv themes/SUB_foundation_access/SUB_foundation_access.info "themes/SUB_foundation_access/${distro}_foundation_access.info"
+  mv themes/SUB_foundation_access "themes/${distro}"
   # add into the file the correct depdendencies for these
   if [ $tooltype == 'authority' ];
     then
-    sed -i "/\;dependencies[] = cis_course_authority/a \ \t \dependencies[] = cis_course_authority" "${distro}.info"
+    sed -i '' "s/\;dependencies\[\] = cis_course_authority/dependencies\[\] = cis_course_authority/g" ${distro}.info
   fi
-  # instances don't change anything abnormal
-  if [ $tooltype == 'instance' ];
+  # service don't change anything abnormal
+  if [ $tooltype == 'service' ];
     then
-    sed -i "/\;dependencies[] = og/a \ \t \dependencies[] = og" "${distro}.info"
-    sed -i "/\;dependencies[] = og_ui/a \ \t \dependencies[] = og_ui" "${distro}.info"
-    sed -i "/\;dependencies[] = cis_section/a \ \t \dependencies[] = cis_section" "${distro}.info"
+    sed -i '' "s/\;dependencies\[\] = og/dependencies\[\] = og/g" ${distro}.info
+    sed -i '' "s/\;dependencies\[\] = og_ui/dependencies\[\] = og_ui/g" ${distro}.info
+    sed -i '' "s/\;dependencies\[\] = cis_section/dependencies\[\] = cis_section/g" ${distro}.info
   fi
 fi
 
 # remove git in this new place and create a new repo w/ the correct name conventions
-# rm -rf .git
-# git init
-#git checkout -b $version
-# git add -A
-# git commit -m "initial commit of $distro"
-elmslnecho "issue this for d.o work: git remote add origin YOU@git.drupal.org:project/$distro.git"
+rm -rf .git
+git init
+git checkout -b $version
+git add -A
+git commit -m "initial commit of $distro"
+elmslnecho "issue this for d.o work:"
+elmslnecho "git remote add origin YOU@git.drupal.org:project/$distro.git"
+elmslnecho "git push origin $version"
 
 # check that it doesn't already exist
 if [ ! -d "$elmsln/domains/$domain" ]; then
@@ -114,12 +117,12 @@ if [ ! -d "$elmsln/domains/$domain" ]; then
   # authorities are just a symlink to the folder in question, much easier!
   if [ $tooltype == 'authority' ];
     then
-    drush eas $domain $distro --service-instance=TRUE
+    drush eas $domain "${distro}-${version}"
   fi
-  # instances have a bit more symlinks
-  if [ $tooltype == 'instance' ];
+  # services have a bit more symlinks
+  if [ $tooltype == 'service' ];
     then
-    drush eas $domain $distro --service-instance=TRUE
+    drush eas $domain "${distro}-${version}" --service-instance=TRUE
     cp "$elmsln/core/dslmcode/cores/drupal-7/entity-iframe-consumer.html" "$domain/entity-iframe-consumer.html"
     cp "$elmsln/core/dslmcode/cores/drupal-7/apple-touch-icon-precomposed.png" "$domain/apple-touch-icon-precomposed.png"
     cp "courses/.gitignore" "$domain/.gitignore"
@@ -128,36 +131,16 @@ if [ ! -d "$elmsln/domains/$domain" ]; then
 fi
 
 # update the documentation file on apache config
-echo "<VirtualHost *:80>" >  $elmsln/docs/domains.txt
-echo "    DocumentRoot $elmsln/domains/$domain" >  $elmsln/docs/domains.txt
-echo "    ServerName $domain.${address}" >  $elmsln/docs/domains.txt
-echo "    ServerAlias ${serviceprefix}${domain}.${serviceaddress}" >  $elmsln/docs/domains.txt
-echo "</VirtualHost>" >  $elmsln/docs/domains.txt
-echo "<Directory $elmsln/domains/$domain>" >  $elmsln/docs/domains.txt
-echo "    AllowOverride All" >  $elmsln/docs/domains.txt
-echo "    Order allow,deny" >  $elmsln/docs/domains.txt
-echo "    allow from all" >  $elmsln/docs/domains.txt
-echo "    Include $elmsln/domains/$domain/.htaccess" >  $elmsln/docs/domains.txt
-echo "</Directory>" >  $elmsln/docs/domains.txt
+echo "<VirtualHost *:80>" >>  $elmsln/docs/domains.txt
+echo "    DocumentRoot /var/www/elmsln/domains/$domain" >> $elmsln/docs/domains.txt
+echo "    ServerName $domain.${address}.edu" >> $elmsln/docs/domains.txt
+echo "    ServerAlias ${serviceprefix}${domain}.${serviceaddress}.edu" >> $elmsln/docs/domains.txt
+echo "</VirtualHost>" >> $elmsln/docs/domains.txt
+echo "<Directory /var/www/elmsln/domains/$domain>" >> $elmsln/docs/domains.txt
+echo "    AllowOverride All" >> $elmsln/docs/domains.txt
+echo "    Order allow,deny" >> $elmsln/docs/domains.txt
+echo "    allow from all" >> $elmsln/docs/domains.txt
+echo "    Include /var/www/elmsln/domains/$domain/.htaccess" >> $elmsln/docs/domains.txt
+echo "</Directory>" >> $elmsln/docs/domains.txt
 
-# todo, still need to re-up the _elmsln_scripted key that's been generated
-# need to grep into the following file, look to drush-create-site for example
-# "$configsdir/shared/drupal-7.x/modules/_elmsln_scripted/${university}/${university}_${host}_settings/${university}_${host}_settings.module
-# todo, need to issue a registry resync against CIS
-# this will activate the new service once we've populated the info above
-elmslnecho "The tool named $domain has now been added to the ELMSLN structure, but the URLs associated to it are not active."
-elmslnecho "You should add this to $elmsln/docs/domains.txt for how to hook it up to apache."
-elmslnecho "You'll want to add something like the following to /etc/httpd/conf.d/elmsln.conf"
-elmslnecho "<VirtualHost *:80>"
-elmslnecho "    DocumentRoot $elmsln/domains/$domain"
-elmslnecho "    ServerName $domain.${address}"
-elmslnecho "    ServerAlias ${serviceprefix}${domain}.${serviceaddress}"
-elmslnecho "</VirtualHost>"
-elmslnecho "<Directory $elmsln/domains/$domain>"
-elmslnecho "    AllowOverride All"
-elmslnecho "    Order allow,deny"
-elmslnecho "    allow from all"
-elmslnecho "    Include $elmsln/domains/$domain/.htaccess"
-elmslnecho "</Directory>"
-elmslnecho ""
-elmslnecho "After that is in place, restart apache and then you should be able to access ${protocol}://${domain}.${address}/README.txt"
+elmslnecho "The tool named $domain has now been added to the ELMSLN structure."
