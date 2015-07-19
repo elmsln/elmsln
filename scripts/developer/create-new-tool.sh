26462e6fd01a535128079450cb8058132f5f81c6
btopro <bto108@psu.edu>
2015-07-19 13:09:03 -0400

    working to automate... me

--- scripts/developer/create-new-tool.sh
+++ scripts/developer/create-new-tool.sh
@@ -12,7 +12,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 elmsln="${DIR}/../../"
-configdir="${DIR}/../../../instances/_development/config-example"
+configsdir="${DIR}/../../../instances/_development/config-example"
 address="YOURUNIT"
 serviceaddress="SERVICEYOURUNIT"
 serviceprefix="DATA."
@@ -35,11 +35,23 @@ if [ -z $1 ]; then
 fi
 # test for an argument as to what user to write as
 if [ -z $2 ]; then
+  elmslnwarn "You must supply a distro to create like inbox"
+  exit 1
+fi
+# test for an argument as to what user to write as
+if [ -z $3 ]; then
+  elmslnwarn "You must supply the version such as 7.x-1.x."
+  exit 1
+fi
+# test for an argument as to what user to write as
+if [ -z $4 ]; then
   elmslnwarn "You must supply the type of tool this is, authority or instance?"
   exit 1
 fi
 domain=$1
-tooltype=$2
+distro=$2
+version=$3
+tooltype=$4
 # check that this domain exists as a stack, otherwise error out
 if [ ! -d "$elmsln/core/dslmcode/stacks/$domain" ]; then
   elmslnwarn "This domain must already exist if we are to correctly discover it."
@@ -61,25 +73,72 @@ if [ ! -d "$configsdir/stacks/$domain" ]; then
   echo ");" >> "$configsdir/stacks/$domain/sites/sites.php"
 fi
 
+# check for the distro, if we don't have it then create it
+if [ ! -d "$elmsln/core/dslmcode/profiles/${distro}-${version}" ]; then
+  cd "$elmsln/core/dslmcode/profiles"
+  cp -R ulmus-7.x-1.x "${distro}-${version}"
+  cd "${distro}-${version}"
+  renames=('local.make.example' 'drecipes/elmsln_ulmus.drecipe' 'ulmus.info' 'ulmus.install' 'ulmus.profile' 'themes/SUB_foundation_access/SUB_foundation_access.info' 'themes/SUB_foundation_access/css/SUB_styles.css' 'themes/SUB_foundation_access/template.php')
+  for rename in "${renames[@]}"
+    do
+    sed -i "/\ulmus/a \ \t $distro" $rename
+    sed -i "/\SUB/a \ \t $distro" $rename
+  done
+  find . -name 'ulmus*' -type f -exec bash -c 'mv "$1" "${1/\/ulmus/$distro/}"' -- {} \;
+  find . -name 'SUB*' -type f -exec bash -c 'mv "$1" "${1/\/SUB/$distro/}"' -- {} \;
+  # add into the file the correct depdendencies for these
+  if [ $tooltype == 'authority' ];
+    then
+    sed -i "/\;dependencies[] = cis_course_authority/a \ \t \dependencies[] = cis_course_authority" "${distro}.info"
+  fi
+  # instances don't change anything abnormal
+  if [ $tooltype == 'instance' ];
+    then
+    sed -i "/\;dependencies[] = og/a \ \t \dependencies[] = og" "${distro}.info"
+    sed -i "/\;dependencies[] = og_ui/a \ \t \dependencies[] = og_ui" "${distro}.info"
+    sed -i "/\;dependencies[] = cis_section/a \ \t \dependencies[] = cis_section" "${distro}.info"
+  fi
+fi
+
+# remove git in this new place and create a new repo w/ the correct name conventions
+# rm -rf .git
+# git init
+#git checkout -b $version
+# git add -A
+# git commit -m "initial commit of $distro"
+elmslnecho "issue this for d.o work: git remote add origin YOU@git.drupal.org:project/$distro.git"
+
 # check that it doesn't already exist
 if [ ! -d "$elmsln/domains/$domain" ]; then
   cd "$elmsln/domains"
   # authorities are just a symlink to the folder in question, much easier!
   if [ $tooltype == 'authority' ];
     then
-    ln -s "../core/dslmcode/stacks/$domain" "$domain"
+    drush eas $domain $distro --service-instance=TRUE
   fi
   # instances have a bit more symlinks
   if [ $tooltype == 'instance' ];
     then
-    ln -s "../../config/stacks/$domain/favicon.ico" "favicon.ico"
-    ln -s "../core/dslmcode/stacks/$domain/.htaccess" ".htaccess"
+    drush eas $domain $distro --service-instance=TRUE
     cp "$elmsln/core/dslmcode/cores/drupal-7/entity-iframe-consumer.html" "$domain/entity-iframe-consumer.html"
     cp "$elmsln/core/dslmcode/cores/drupal-7/apple-touch-icon-precomposed.png" "$domain/apple-touch-icon-precomposed.png"
     cp "courses/.gitignore" "$domain/.gitignore"
     cp "courses/README.txt" "$domain/README.txt"
   fi
 fi
+
+# update the documentation file on apache config
+echo "<VirtualHost *:80>" >  $elmsln/docs/domains.txt
+echo "    DocumentRoot $elmsln/domains/$domain" >  $elmsln/docs/domains.txt
+echo "    ServerName $domain.${address}" >  $elmsln/docs/domains.txt
+echo "    ServerAlias ${serviceprefix}${domain}.${serviceaddress}" >  $elmsln/docs/domains.txt
+echo "</VirtualHost>" >  $elmsln/docs/domains.txt
+echo "<Directory $elmsln/domains/$domain>" >  $elmsln/docs/domains.txt
+echo "    AllowOverride All" >  $elmsln/docs/domains.txt
+echo "    Order allow,deny" >  $elmsln/docs/domains.txt
+echo "    allow from all" >  $elmsln/docs/domains.txt
+echo "    Include $elmsln/domains/$domain/.htaccess" >  $elmsln/docs/domains.txt
+echo "</Directory>" >  $elmsln/docs/domains.txt
 
 # todo, still need to re-up the _elmsln_scripted key that's been generated
 # need to grep into the following file, look to drush-create-site for example
