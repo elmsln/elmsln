82f4f5cf8e5198be62289c4d94500c25cc7452e6
btopro <bto108@psu.edu>
2015-08-04 15:05:41 -0400

    cis filter modal updates

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/cis_filter/js/cis_filter.js
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/cis_filter/js/cis_filter.js
@@ -3,9 +3,9 @@
 */
 (function ($) {
   Drupal.theme.prototype.cis_filter_modal = function () {
-    var html = '<div id="ctools-modal" class="popups-box">';
+    var html = '<div id="ctools-modal" class="popups-box close-reveal-modal">';
     html += '  <div class="ctools-modal-content ctools-modal-cis-filter-modal-content">';
-    html += '    <span class="popups-close"><a class="close ctools-modal-cis-filter-close" href="#">' + Drupal.CTools.Modal.currentSettings.closeImage + '</a></span>';
+    html += '    <span class="popups-close"><a class="close close-reveal-modal" aria-label="Close" href="#">' + Drupal.CTools.Modal.currentSettings.closeImage + '</a></span>';
     html += '    <div><div id="modal-content" class="expand reveal-modal-open modal-content popups-body"></div></div>';
     html += '  </div>';
     html += '</div>';
@@ -14,48 +14,10 @@
   // ability to disable background scrolling on modal open
   Drupal.behaviors.gradebookBodyScrollDisable = {
   attach: function (context, settings) {
-    $('.ctools-modal-cis-filter-close').on("click", function () {
+    $('.close-reveal-modal').on("click", function () {
       $("body").removeClass("scroll-disabled")
     });
   }
-  };
-  // ajax submit cause ctools apparently doesn't support this w/ DS
-  Drupal.behaviors.gradebookAssessmentSubmit = {
-    'attach': function(context) {
-      // checkboxes need prop set to be displayed dynamically
-      if ($('#modal-content input[name=flag_submission_value]').val() == 1) {
-        $('#modal-content input[name=flag_submission]').prop('checked', true);
-      }
-      $('#modal-content input[name=flag_submission]').val($('#modal-content input[name=flag_submission_value]').val());
-      // select state
-      $('#modal-content select[name=assessment_state]').val($('#modal-content input[name=assessment_state_value]').val());
-      // set point value
-      $('#modal-content input[name=assessment]').val($('#modal-content input[name=assessment_value]').val());
-      // bind click event to fake the submission via ajax and update what we have to
-      $('#modal-content .form-submit', context).bind('click', function() {
-        // structure the data
-        var data = {
-          flag: $('#modal-content input[name=flag_submission]').val(),
-          state: $('#modal-content select[name=assessment_state]').val(),
-          points: $('#modal-content input[name=assessment]').val()
-        };
-        // kick off the ajax request to update the values with those selected
-        $.ajax({
-          url: Drupal.settings.basePath + 'cis/service/ajax/' + Drupal.settings.cisFilterToken + '/' + $('#modal-content input[name=aid]').attr('value') + '/' + $('#modal-content input[name=submission]').attr('value'),
-          type: 'POST',
-          data: data,
-        })
-        .done(function(data) {
-          var returned = jQuery.parseJSON(data);
-          // close window
-          $('.ctools-modal-cis-filter-close').click();
-        })
-        .fail(function(data) {
-          alert(Drupal.t('Save failed'));
-        });
-        return false;
-      });
-    }
   };
   // bind these events only once
   $(document).ready(function(){
