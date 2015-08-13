39ea45a7d40cb08d2935ae41e8f82cafae63822f
btopro <bto108@psu.edu>
2015-08-12 22:12:23 -0400

    Book navigation Map added into MOOC as next progression of this style nav #190

---
+++ core/dslmcode/profiles/mooc-7.x-1.x/themes/mooc_foundation_access/templates/blocks/block--mooc-helper--mooc-helper-toc.tpl.php
@@ -0,0 +1,5 @@
+<div id="<?php print $block_html_id ?>-nav-modal" class="reveal-modal etb-nav-modal disable-scroll" data-reveal>
+<h3><?php print t('Table of Contents'); ?></h3>
+<?php print $content; ?>
+<a class="close-reveal-modal" aria-label="Close">&#215;</a>
+</div>
