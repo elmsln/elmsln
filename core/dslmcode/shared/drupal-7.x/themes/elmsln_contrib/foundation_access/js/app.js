e4da654c2ec085ccd2d0deddd17d7abd2a62e120
btopro <bto108@psu.edu>
2015-08-21 09:48:01 -0400

    loading these instead of in different file that doesn't run

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/app.js
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/app.js
@@ -156,5 +156,20 @@
     // forcibly call these to fire the first time
     Drupal.offcanvasHeight.attach();
     Drupal.offcanvasSubmenuHeight.attach();
+    /* Implement customer javascript here */
+    $(".disable-scroll").on("show", function () {
+      $("body").addClass("scroll-disabled");
+    }).on("hidden", function () {
+      $("body").removeClass("scroll-disabled")
+    });
+
+    $('*[data-reveal-id]').click(function () {
+      var revealID = $(this).attr("data-reveal-id");
+      var wrapper = $("#" + revealID);
+      // If the wrapper element is open then give it focus
+      if (wrapper.hasClass('open')) {
+        wrapper.focus();
+      }
+    });
   });
 })(jQuery);
