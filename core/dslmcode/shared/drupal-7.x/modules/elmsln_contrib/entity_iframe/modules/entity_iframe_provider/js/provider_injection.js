a36675585431111bc3afca48bde327287a718f5f
btopro <bto108@psu.edu>
2015-08-28 15:20:49 -0400

    capture URL of the current page and ship it back upstream :)

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/entity_iframe/modules/entity_iframe_provider/js/provider_injection.js
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/entity_iframe/modules/entity_iframe_provider/js/provider_injection.js
@@ -2,7 +2,8 @@
 (function($) {
   $(document).ready(function(){
     var h = $(document).height();
+    var url = window.location.href;
     $('body').append('<iframe id="entity_iframe_consumer_backdoor" src="' + Drupal.settings.entity_iframe.consumer[0] + '/entity-iframe-consumer.html?iframeid=' + Drupal.settings.entity_iframe.consumer[1] + '" width="1" height="1"/>');
-    $('#entity_iframe_consumer_backdoor').attr('src', ($('#entity_iframe_consumer_backdoor').attr('src') + '#h=' + h));
+    $('#entity_iframe_consumer_backdoor').attr('src', ($('#entity_iframe_consumer_backdoor').attr('src') + '#h=' + h + '&url=' + url));
   });
 })(jQuery);
