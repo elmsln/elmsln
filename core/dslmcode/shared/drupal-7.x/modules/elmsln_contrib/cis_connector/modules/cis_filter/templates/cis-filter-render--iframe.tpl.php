b1b079417b3b86f7d8648f2d528638fd8dcbbe3c
btopro <bto108@psu.edu>
2015-10-06 15:25:19 -0400

    template files for new remote rendering method to short codes

---
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/cis_filter/templates/cis-filter-render--iframe.tpl.php
@@ -0,0 +1,10 @@
+<?php
+  // render an iframe based on passed in values
+  // Variables
+  //  $id - content id
+  //  $class - classes to apply
+  //  $link - link to what the frame is pointing to
+  //  $width - width of the frame
+  //  $height - height of the frame
+?>
+<iframe id="<?php print $id; ?>" class="<?php print $class; ?>" src="<?php print $link; ?>" width="<?php print $width; ?>" height="<?php print $height; ?>"></iframe>
