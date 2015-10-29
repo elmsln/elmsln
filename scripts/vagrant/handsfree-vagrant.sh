a37dac9bec42ac953332747e2e89a530fa013957
btopro <bto108@psu.edu>
2015-10-29 12:15:43 -0400

    user more standard command for the reset in vagrant for doing auto restarting if development environment frozen

--- scripts/vagrant/handsfree-vagrant.sh
+++ scripts/vagrant/handsfree-vagrant.sh
@@ -23,17 +23,17 @@ cat /var/www/elmsln/scripts/vagrant/hosts >> /etc/hosts
 # vagrant and core services on the VM starting correctly
 # when ever networking is screwed up
 echo '#!/bin/bash' >> /etc/profile.d/chkon.sh
-echo 'mysql=$(sudo /etc/init.d/mysqld status)' >> /etc/profile.d/chkon.sh
-echo 'httpd=$(sudo /etc/init.d/httpd status)' >> /etc/profile.d/chkon.sh
+echo 'mysql=$(sudo /sbin/service mysqld status)' >> /etc/profile.d/chkon.sh
+echo 'httpd=$(sudo /sbin/service httpd status)' >> /etc/profile.d/chkon.sh
 # test for mysql
 echo 'if [[ $mysql == *"mysqld is stopped"* ]]' >> /etc/profile.d/chkon.sh
 echo 'then' >> /etc/profile.d/chkon.sh
-echo '  sudo /etc/init.d/mysqld start' >> /etc/profile.d/chkon.sh
+echo '  sudo /sbin/service mysqld restart' >> /etc/profile.d/chkon.sh
 echo 'fi' >> /etc/profile.d/chkon.sh
 # test for apache
 echo 'if [[ $httpd == *"httpd is stopped"* ]]' >> /etc/profile.d/chkon.sh
 echo 'then' >> /etc/profile.d/chkon.sh
-echo '  sudo /etc/init.d/httpd start' >> /etc/profile.d/chkon.sh
+echo '  sudo /sbin/service httpd restart' >> /etc/profile.d/chkon.sh
 echo 'fi' >> /etc/profile.d/chkon.sh
 
 
