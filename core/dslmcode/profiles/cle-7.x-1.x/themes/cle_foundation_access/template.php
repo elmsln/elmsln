67bb2a0f09dcfe7d2be17a8a067b7d67cb8a75c3
btopro <bto108@psu.edu>
2015-10-29 01:27:29 -0400

    breadcrumb doesn't make sense in CLE

--- core/dslmcode/profiles/cle-7.x-1.x/themes/cle_foundation_access/template.php
+++ core/dslmcode/profiles/cle-7.x-1.x/themes/cle_foundation_access/template.php
@@ -12,3 +12,10 @@ function cle_foundation_access_menu_link__menu_cle_navigation(&$variables) {
 function cle_foundation_access_menu_tree__menu_cle_navigation($variables) {
   return '<ul class="off-canvas-list has-submenu content-outline-navigation">' . $variables['tree'] . '</ul>';
 }
+
+/**
+ * Implements theme_breadrumb().
+ */
+function cle_foundation_access_breadcrumb($variables) {
+  // hide breadcrumbs
+}
