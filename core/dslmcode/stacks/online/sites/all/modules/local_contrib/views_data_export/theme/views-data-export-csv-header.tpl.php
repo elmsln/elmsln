e1e060e41a19d13ec8b7a1c50f1837ba00659637
btopro <bto108@psu.edu>
2015-11-11 12:25:06 -0500

    views_data_export not needed in courses stack as its global now

--- core/dslmcode/stacks/online/sites/all/modules/local_contrib/views_data_export/theme/views-data-export-csv-header.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Print out header row, if option was selected.
-if ($options['header']) {
-  print implode($separator, $header) . "\r\n";
-}
--- core/dslmcode/stacks/online/sites/all/modules/local_contrib/views_data_export/theme/views-data-export-txt-header.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-// TXT files don't really have a footer.
