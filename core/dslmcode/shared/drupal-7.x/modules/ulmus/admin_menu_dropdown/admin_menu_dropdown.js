3ab8c5957a432038d507fdf459af4a1900bc69e6
mmilutinovic1313 <mmilutinovic1313@gmail.com>
2015-11-17 17:07:16 -0500

    Removed console.log

--- core/dslmcode/shared/drupal-7.x/modules/ulmus/admin_menu_dropdown/admin_menu_dropdown.js
+++ core/dslmcode/shared/drupal-7.x/modules/ulmus/admin_menu_dropdown/admin_menu_dropdown.js
@@ -17,7 +17,6 @@
         $(document).keypress(function(e) {
           var unicode = e.keyCode? e.keyCode : e.charCode;
           if (String.fromCharCode(unicode) == settings.admin_menu_dropdown.key && !($(document.activeElement).attr("type") == "text" || $(document.activeElement)[0].tagName == "TEXTAREA")) {
-            console.log($(document.activeElement));
             $('#admin-menu').slideToggle('fast');
             // TODO: Maybe animate the margin change so its not so jumpy?
             $('body').toggleClass('adm_menu_hidden');
