4ea9e884ffa6b35e3ef528016be8aa55992d825a
btopro <bto108@psu.edu>
2015-11-17 22:50:51 -0500

    random password seeding needed

--- scripts/utilities/add-domain-support.sh
+++ scripts/utilities/add-domain-support.sh
@@ -97,7 +97,8 @@ if [ ! -d "$elmsln/domains/$domain" ]; then
     cp "courses/README.txt" "$domain/README.txt"
   fi
 fi
-
+char=(0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V X W Y Z)
+max=${#char[*]}
 # now build the tool in question and user account
 dbpw=''
 for j in `seq 1 30`
@@ -105,6 +106,7 @@ do
   let "rand=$RANDOM % 62"
   dbpw="${dbpw}${char[$rand]}"
 done
+
 # if authority we need to build the distro
 if [ $tooltype == 'authority' ];
   then
