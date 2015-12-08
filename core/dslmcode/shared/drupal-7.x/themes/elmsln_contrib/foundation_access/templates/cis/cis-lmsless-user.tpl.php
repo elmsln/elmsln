42fb863b23fbb37fb09f285f2883c897d6e83807
btopro <bto108@psu.edu>
2015-12-08 16:21:50 -0500

    profile link when logged in and ReOrg a bit

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/cis/cis-lmsless-user.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/cis/cis-lmsless-user.tpl.php
@@ -10,8 +10,14 @@
   <h1><?php print t('Account'); ?></h1>
   <hr class="pad-1" />
   <?php if (!empty($username)) : ?>
-    <h2><?php print "$username ($userlink)"; ?></h2>
+    <h2><?php print "$username ($userprofile) "; ?><span style="float:right;"><?php print $userlink; ?></span></h2>
     <?php if (isset($userpicturebig)) { print $userpicturebig; } ?>
+  <?php endif; ?>
+  <?php if (isset($user_roles)) : ?>
+  <h2><?php print t('Roles'); ?></h2>
+  <div class="user-nav-user-roles">
+    <?php print $user_roles; ?>
+  </div>
   <?php endif; ?>
   <hr class="pad-1" />
   <?php if (isset($masquerade)) : ?>
@@ -21,12 +27,6 @@
   </div>
   <?php endif; ?>
   <hr class="pad-1" />
-  <?php if (isset($user_roles)) : ?>
-  <h2><?php print t('Roles'); ?></h2>
-  <div class="cis-admin-area user-nav-user-roles">
-    <?php print $user_roles; ?>
-  </div>
-  <?php endif; ?>
   <?php if (isset($section_title)) : ?>
   <h2><?php print t('Section'); ?></h2>
   <div class="cis-admin-area user-nav-section">
