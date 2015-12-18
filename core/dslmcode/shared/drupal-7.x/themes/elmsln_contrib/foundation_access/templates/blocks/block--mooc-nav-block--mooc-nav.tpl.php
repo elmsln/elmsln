715a7e5a38ee977b20579dd07127caac48118db4
btopro <bto108@psu.edu>
2015-12-18 01:18:39 -0500

    functional but needs work #408
    this is gettings really powerful

---
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/blocks/block--mooc-nav-block--mooc-nav.tpl.php
@@ -0,0 +1,4 @@
+<div id="<?php print $block_html_id; ?>-nav" class="mooc-nav-<?php print $block->mooc_nav['style']; ?> <?php print $classes; ?>">
+<h3><?php print check_plain($block->subject); ?></h3>
+<?php print $content; ?>
+</div>
