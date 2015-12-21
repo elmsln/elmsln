94b422251377656001dd675776dc8cfcafe69278
btopro <bto108@psu.edu>
2015-12-21 11:54:48 -0500

    CIS upgrade needs caches forcibly rebuilt via cc, rr, and apache / myql restarts

--- scripts/upgrade/optional/cis1x-2x.sh
+++ scripts/upgrade/optional/cis1x-2x.sh
@@ -33,6 +33,11 @@ ln -s ../../../profiles/cis-7.x-2.x cis
 elmslnecho "Applying Upgrades"
 cd ${elmsln}
 drush @online cook cis1x_2x --dr-locations=${elmsln}/scripts/upgrade/drush_recipes/d7/optional/cis/ --y
+# restart apache
+sudo /etc/init.d/httpd restart
+sudo /etc/init.d/mysqld restart
+drush @online cc all --y
+drush @online rr --y
 # revert core features to ensure they match what things are now
 drush @online fr cis_displays cis_default_permissions cis_types cis_users cis_ux --force --y
 # update db
