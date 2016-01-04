75e8142c83787f40b04b3c0bdbd53f9a6c750aed
btopro <bto108@psu.edu>
2016-01-04 11:24:46 -0500

    #428

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/blocks/block--cis-shortcodes--cis-shortcodes-block.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/blocks/block--cis-shortcodes--cis-shortcodes-block.tpl.php
@@ -1,4 +1,4 @@
-<div id="<?php print $block_html_id ?>-nav-modal" class="reveal-modal disable-scroll" data-reveal>
+<div id="<?php print $block_html_id ?>-nav-modal" class="reveal-modal" data-reveal>
 <?php print $content; ?>
 <a class="close-reveal-modal" aria-label="<?php print t('Close'); ?>">&#215;</a>
 </div>
