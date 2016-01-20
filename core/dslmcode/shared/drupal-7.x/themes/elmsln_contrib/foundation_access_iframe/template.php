19778370ab277c32caf8b4d37fd38ceb2104c28f
btopro <bto108@psu.edu>
2016-01-20 12:38:53 -0500

    now trap for entity iframe not having this set #490

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access_iframe/template.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access_iframe/template.php
@@ -3,8 +3,10 @@
  * Implements template_process_page.
  */
 function foundation_access_iframe_process_page(&$variables) {
-  // remove the "Home" button when in iframe mode
-  $keys = array_keys($variables['page']['header']);
-  $keyname = array_shift($keys);
-  unset($variables['page']['header'][$keyname]['#prefix']);
+  // remove the "Home" button when in iframe mode if its there
+  if (isset($variables['page']['header'])) {
+    $keys = array_keys($variables['page']['header']);
+    $keyname = array_shift($keys);
+    unset($variables['page']['header'][$keyname]['#prefix']);
+  }
 }
