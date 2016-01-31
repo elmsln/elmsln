ee886ebbdc00a0999347a7c11f6c63627bd3f646
btopro <bto108@psu.edu>
2016-01-31 15:24:52 -0500

    start of #536

--- core/dslmcode/profiles/cle-7.x-1.x/themes/cle_foundation_access/template.php
+++ core/dslmcode/profiles/cle-7.x-1.x/themes/cle_foundation_access/template.php
@@ -1,13 +1,6 @@
 <?php
 
 /**
- * Implements menu_tree__main_menu.
- */
-function cle_foundation_access_menu_tree__menu_cle_navigation($variables) {
-  return '<ul class="header-menu-options">' . $variables['tree'] . '</ul>';
-}
-
-/**
  * Implements theme_breadrumb().
  */
 function cle_foundation_access_breadcrumb($variables) {
