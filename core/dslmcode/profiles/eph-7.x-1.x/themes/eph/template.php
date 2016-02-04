cabe3c4d89f6577d7ed9a0dfbe727185f1ce798b
btopro <bto108@psu.edu>
2016-02-04 16:06:54 -0500

    start of install profile for #241

---
+++ core/dslmcode/profiles/eph-7.x-1.x/themes/eph/template.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Implements menu_tree__menu_discuss_toolkit.
+ */
+function eph_foundation_access_menu_tree__menu_eph_navigation($variables) {
+  return '<ul class="header-menu-options">' . $variables['tree'] . '</ul>';
+}
