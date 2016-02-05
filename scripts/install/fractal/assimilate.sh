16a86acc6e0e781aeb2ff25cbbb263d0c4e4403e
btopro <bto108@psu.edu>
2016-02-05 11:08:10 -0500

    progress on #202 still aways to go

--- scripts/install/fractal/assimilate.sh
+++ scripts/install/fractal/assimilate.sh
@@ -35,18 +35,33 @@ read -rp "$prompt" port
 ssh-copy-id -i ~/.ssh/id_rsa.pub "-p $port $name@$address"
 
 # ALL REMOTE STUFF
-# dump to db export via remote execution
+# TODO test if this already has elmsln installed
+
 elmslnecho "installing elmsln on remote based on this deployment's settings; might take awhile"
 # TODO, ask what handsfree to run
 ssh -p $port $name@$address "yes | yum -y install wget git && git clone https://github.com/elmsln/elmsln.git /var/www/elmsln && bash /var/www/elmsln/scripts/install/handsfree/centos/centos-install.sh $university $host $address $protocol $admin $elmsln_stats_program"
 # kill shared directory and then rsync the local one over
 ssh -p $port $name@$address "rm -rf /var/www/elmsln/config/shared/"
-dugecho 'rsyncing local to remote, will take a few minutes..'
+elmslnecho 'rsyncing local to remote, will take a few minutes..'
 # rsync the shared modules directory over to this box now that it's established
 rsync -az -e "ssh -p $port" $name@$address:/var/www/elmsln/config/shared/ /var/www/elmsln/config/shared/
 
-# need to select which tools to push to this box
+# need to select which tool to push to this box
 # rsync full directory from that server for these tools
 # leave local in-tact
+# print stacks to the screen to pick
+find $elmsln/core/dslmcode/stacks/ -maxdepth 1 -type d | sed 's/\///' | sed 's/\.//'
+prompt="What stack do you want to move? "
+read -rp "$prompt" stacktomove
+
+# TODO ensure this was a valid selection
+rsync -az -e "ssh -p $port" $name@$address:/var/www/elmsln/config/stacks/$stacktomove /var/www/elmsln/config/stacks/$stacktomove
+# ALL REMOTE STUFF
+# dump to db export via remote execution
+elmslnecho 'making a database dump of all current systems and migrate to remote'
+# TODO this needs to spider each it finds and db-dump one at a time
+#drush @${stacktomove}-all sql-dump --result-file="ssh -p $port $name@$address:/var/elmsln_assimilate" --y
+# todo: now do the sql-sync stuff from that system down to this one
+#drush @${stacktomove}-all sql-query --file=/var/elmsln_assimilate/${stacktomove}.sing100.sql --y
 
 # TODO, need to get cis correctly redirecting over to the other box
