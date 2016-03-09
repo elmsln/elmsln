364e91cdd4cb323c45315dde010836788f09bddd
btopro <bto108@psu.edu>
2016-03-09 14:08:56 -0500

    #727 related clean up

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/features/elmsln_core/field-focus.js
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/features/elmsln_core/field-focus.js
@@ -2,7 +2,7 @@
  * Allow for auto selection of iframe content area on focus
  */
 (function ($) {
-  Drupal.behaviors.mooc_helper_node_form = {
+  Drupal.behaviors.elmsln_core_node_form = {
     attach: function(context) {
       // set title as focus field when we're creating a new item
       $('#edit-title').focus();
