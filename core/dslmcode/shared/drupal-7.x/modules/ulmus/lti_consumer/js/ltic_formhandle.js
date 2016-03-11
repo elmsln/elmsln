9af7445fdc64e9a035bcff79f870891ae2cf04f5
btopro <bto108@psu.edu>
2016-03-10 20:52:16 -0500

    #683 lti_provider module for testing

---
+++ core/dslmcode/shared/drupal-7.x/modules/ulmus/lti_consumer/js/ltic_formhandle.js
@@ -0,0 +1,18 @@
+// Using the closure to map jQuery to $. 
+//(function ($) {
+//// Store our function as a property of Drupal.behaviors.
+//    Drupal.behaviors.lticFormActions = {
+//        attach: function (context, settings) {
+//            $('[name^="ltiLaunchForm_"]').live('submit', function(e){
+//                e.preventDefault();
+//               var form = this;
+//               $('#basicltiLaunchFrame').empty();
+//                console.log('clicked:'+this.name);           
+//                form.submit();              
+//            });
+//        }
+//    };
+//
+//
+//}(jQuery));
+
