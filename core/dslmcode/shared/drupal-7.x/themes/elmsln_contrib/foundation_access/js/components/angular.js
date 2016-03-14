4037ffa6a9d475b1389a469ee39c9ee893ee05fe
heyMP <mgp140@gmail.com>
2016-03-14 09:41:45 -0400

    Added CIS dashboard module.

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/components/angular.js
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/components/angular.js
@@ -1,11 +1,39 @@
 module.exports = function() {
-  angular
-    .module('Fa',['ngMaterial', 'ngMessages'])
-    .controller('FaHeaderOptionsCtrl', function DemoCtrl($mdDialog) {
-      var originatorEv;
-      this.openMenu = function($mdOpenMenu, ev) {
-        originatorEv = ev;
-        $mdOpenMenu(ev);
-      };
-    });
+	angular.module('Fa', [
+	  'ngResource'
+	])
+
+	/**
+	 * Constants
+	 */
+	.constant('endpoint_url', '')
+
+	/**
+	 * Config
+	 */
+	.config(['$resourceProvider', function($resourceProvider) {
+	  // Don't strip trailing slashes from calculated URLs
+	  $resourceProvider.defaults.stripTrailingSlashes = false;
+	}])
+
+	/**
+	 * Service
+	 */
+	.service('CoursesService', ['$resource', 'endpoint_url', function($resource, endpoint_url) {
+	  return $resource(endpoint_url + '/node.json?type=course&deep-load-refs=field_collection_item,node', null, {
+	  	query: {
+	  		method: 'GET',
+	  		isArray: false
+	  	}
+	  });
+	}])
+
+	/**
+	 * Controllers
+	 */
+	.controller('cisDashboard', ['$scope', 'CoursesService', function($scope, CoursesService) {
+	  $scope.courses = CoursesService.query();
+	  console.log($scope.courses);
+	}])
+	;
 };
