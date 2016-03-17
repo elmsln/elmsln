aa96ab4c881b86b9f7cd23015d5f7ee0bcf551e3
btopro <bto108@psu.edu>
2016-03-17 13:14:35 -0400

    #757 #758 upgrade script

---
+++ scripts/upgrade/system/0.2.1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# these need to be turned on... everywhere by force
+# 523 also being worked on currently to help work through this
+~/.composer/vendor/bin/drush @elmsln en elmsln_core elmsln_api --y
