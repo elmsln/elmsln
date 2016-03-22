03f0419a4efd13e6de2bc94fb05565692a780356
btopro <bto108@psu.edu>
2016-03-22 17:03:31 -0400

    #431 fix so we don't get undefined at end / beginning of books

--- core/dslmcode/profiles/mooc-7.x-1.x/modules/mooc_custom/mooc_helper_book_nav/js/mooc_helper_book_nav.js
+++ core/dslmcode/profiles/mooc-7.x-1.x/modules/mooc_custom/mooc_helper_book_nav/js/mooc_helper_book_nav.js
@@ -5,12 +5,16 @@ $(document).ready(function() {
       e = event;
     }
     if(e.keyCode==37){  // left key
-      window.location = $('.page-previous').attr('href');
-      return false;
+      if ($('.page-previous').attr('href')) {
+        window.location = $('.page-previous').attr('href');
+        return false;
+      }
     }
     if(e.keyCode==39){  // right key
-      window.location = $('.page-next').attr('href');
-      return false;
+      if ($('.page-next').attr('href')) {
+        window.location = $('.page-next').attr('href');
+        return false;
+      }
     }
     if(e.keyCode==79){  // outline key
       $('.mooc-helper-toc').click();
