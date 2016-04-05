1327c2741a7c3ada78ccb6438f3188e60d88ce3c
btopro <bto108@psu.edu>
2016-04-05 07:57:16 -0400

    #800 support for disabling animations

---
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/a11y/plugins/animation/animation.js
@@ -0,0 +1,25 @@
+(function ($) {
+  $(document).ready(function(){
+    $('#a11y_animation_checkbox').click(function(){
+      Drupal.a11y.animation(this.checked);
+    });
+    // test for cookie being set
+    if ($.cookie('a11y_animation') == 'true') {
+      $('#a11y_animation_checkbox').click();
+    }
+  });
+  // animation functionality
+  Drupal.a11y.animation = function(animation){
+    if (animation == true) {
+      $.fx.off = true;
+      $("body").addClass('a11y-animation');
+      $("body").append($("<link id='a11y_animation_styles' rel='stylesheet' href='" + Drupal.settings.a11y.path + "plugins/animation/animation.css' type='text/css' media='screen' />"));
+    }
+    else {
+      $.fx.off = false;
+      $("body").removeClass('a11y-animation');
+      $("#a11y_animation_styles").remove();
+    }
+    $.cookie('a11y_animation', animation, { path: '/', domain: Drupal.settings.a11y.domain });
+  };
+})(jQuery);
