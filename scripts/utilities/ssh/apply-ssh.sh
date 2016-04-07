82f8ec587e8218255c65aa8c7e7f97eaf2152b19
btopro <bto108@psu.edu>
2016-04-07 13:21:58 -0400

    forcibly apply ssh settings for the ulmusdrush user

---
+++ scripts/utilities/ssh/apply-ssh.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# where am i? move to where I am. This ensures source is properly sourced
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+# load config
+source ../../../config/scripts/drush-create-site/config.cfg
+
+#provide messaging colors for output to console
+txtbld=$(tput bold)             # Bold
+bldgrn=${txtbld}$(tput setaf 2) #  green
+bldred=${txtbld}$(tput setaf 1) #  red
+txtreset=$(tput sgr0)
+elmslnecho(){
+  echo "${bldgrn}$1${txtreset}"
+}
+elmslnwarn(){
+  echo "${bldred}$1${txtreset}"
+}
+# Define seconds timestamp
+timestamp(){
+  date +"%s"
+}
+
+#test for empty vars. if empty required var -- exit
+if [ -z $elmsln ]; then
+  elmslnwarn "please update your config.cfg file"
+  exit 1
+fi
+
+# check that we are the root user
+if [[ $EUID -ne 0 ]]; then
+  elmslnwarn "Please run as root"
+  exit 1
+fi
+
+# apply ulmusdrush .ssh creds for use with the command-job script
+if [ -d /home/ulmusdrush/.ssh/ ]; then
+  cp -R ulmusdrush/.ssh/* /home/ulmusdrush/.ssh/
+else
+  cp -R ulmusdrush/.ssh /home/ulmusdrush/
+fi
+# ensure everything is all set here
+chown -R ulmusdrush:elmsln /home/ulmusdrush/.ssh/
