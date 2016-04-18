f9bd348605fa49465a37844937295ad4914646c8
btopro <bto108@psu.edu>
2016-04-18 16:08:45 -0400

    hooked some more parts up #405
    now loads offerings and semester as well as starting to put some links
    in place

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/tweaks.js
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/tweaks.js
@@ -1,8 +1,9 @@
 /**
- * This file should be used instead of foundation_access/js/tweaks.js 
+ * This file should be used instead of foundation_access/js/tweaks.js
  * if you are not using the grunt task runner.
  */
 
 (function ($) {
 
 })(jQuery);
+
