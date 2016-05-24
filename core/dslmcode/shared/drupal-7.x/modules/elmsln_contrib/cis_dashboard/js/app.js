19ddf8f5061dcbf106ff8f30562d29aded979246
heyMP <mgp140@gmail.com>
2016-05-24 17:29:04 -0400

    Moved angular code to cis dashboard module.  moved cis dashboard out of cis profile temporarily.

---
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_dashboard/js/app.js
@@ -0,0 +1,46 @@
+angular.module('Fa', [
+  'ngResource'
+])
+
+/**
+ * Constants
+ */
+.constant('endpoint_url', '')
+
+/**
+ * Config
+ */
+.config(['$resourceProvider', function($resourceProvider) {
+  // Don't strip trailing slashes from calculated URLs
+  $resourceProvider.defaults.stripTrailingSlashes = false;
+}])
+
+/**
+ * Service
+ */
+.service('CoursesService', ['$resource', 'endpoint_url', function($resource, endpoint_url) {
+  return $resource(endpoint_url + '/node.json?type=course&deep-load-refs=field_collection_item,node', null, {
+  	query: {
+  		method: 'GET',
+  		isArray: false
+  	}
+  });
+}])
+
+/**
+ * Controllers
+ */
+.controller('cisDashboard', ['$scope', 'CoursesService', function($scope, CoursesService) {
+  $scope.courses = CoursesService.query();
+  // simple selector to make this item be 'active'
+  $scope.select = function(item) {
+    $scope.selected = item;
+  };
+  // test to see if something is 'active'
+  $scope.isActive = function(item) {
+    return $scope.selected === item;
+  };
+}])
+;
+
+console.log('cis angular working');
