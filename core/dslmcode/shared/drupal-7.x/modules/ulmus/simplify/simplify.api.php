5cabe3cabeeb9d75716406972f874104d38afb2d
btopro <bto108@psu.edu>
2016-06-02 10:17:58 -0400

    we had D8 simply instead of D7, doh

--- core/dslmcode/shared/drupal-7.x/modules/ulmus/simplify/simplify.api.php
+++ core/dslmcode/shared/drupal-7.x/modules/ulmus/simplify/simplify.api.php
@@ -7,17 +7,17 @@
 /**
  * Alter the list of fields that can be hidden.
  *
- * @param array $fields
+ * @param $fields
  *   An associative array of fields that can be hidden, where the key is the
  *   machine name of the field and the value is the human-readable name of the
  *   field.
- * @param string $type
+ * @param $type
  *   The type of fields passed to the $fields parameter. Can be one of:
  *   nodes, users, comments, taxonomy, blocks.
  *   See simplify_get_fields() for examples.
  */
 function hook_simplify_get_fields_alter(&$fields, $type) {
-  // Allow our module's custom 'foo' node field to be hidden.
+  // Allow our module's custom 'foo' node field to be hidden
   if ($type == 'nodes') {
     $fields['foo'] = t('Foo field');
   }
@@ -27,16 +27,17 @@ function hook_simplify_get_fields_alter(&$fields, $type) {
  * Alter the way fields are hidden, or hide fields defined in
  * hook_simplify_get_fields_alter().
  *
- * @param array $form
+ * @param $form
  *   The form array in which the field to be hidden resides. Hiding a field is
  *   generally done by setting its '#access' form value to FALSE.
- * @param array $field
+ * @param $field
  *   The machine name of the field to be hidden as defined in
  *   simplify_get_fields() or hook_simplify_get_fields_alter().
  */
 function hook_simplify_hide_field_alter(&$form, $field) {
-  // Hide our module's custom 'foo' node field.
+  // Hide our module's custom 'foo' node field
   if ($field == 'foo') {
     $form['foo']['#access'] = FALSE;
   }
 }
+
