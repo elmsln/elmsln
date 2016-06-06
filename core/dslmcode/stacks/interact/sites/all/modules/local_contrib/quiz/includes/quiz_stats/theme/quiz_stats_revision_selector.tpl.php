6ee16e9f37868de73cb9bf9fedbb985b6e5c6aa5
btopro <bto108@psu.edu>
2016-06-06 12:16:34 -0400

    more quiz clean up after moving it to the grades system

--- core/dslmcode/stacks/interact/sites/all/modules/local_contrib/quiz/includes/quiz_stats/theme/quiz_stats_revision_selector.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-print '<p>' . $content['explanation'] . '</p>' . "\n";
-print '<p>';
-$counter = 1;
-foreach ($content['links'] as $key => $value) {
-  print ' | '. l(t('revision !num', array('!num' => $counter++)), $value);
-}
-print ' |</p>';
-?>
