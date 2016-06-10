d77421f2e8229660a4463158d3bd974d7ea871d6
btopro <bto108@psu.edu>
2016-06-10 09:50:32 -0400

    wrappers to ensure that these don't fire before jwerty loads

--- core/dslmcode/profiles/mooc-7.x-1.x/modules/mooc_custom/mooc_helper_book_nav/js/mooc_helper_book_nav.js
+++ core/dslmcode/profiles/mooc-7.x-1.x/modules/mooc_custom/mooc_helper_book_nav/js/mooc_helper_book_nav.js
@@ -1,18 +1,20 @@
 (function ($) {
-  // go back
-  jwerty.key('←', function () {
-    if ($('.page-previous').attr('href')) {
-      window.location = $('.page-previous').attr('href');
-    }
-  });
-  // go forward
-  jwerty.key('→', function () {
-    if ($('.page-next').attr('href')) {
-      window.location = $('.page-next').attr('href');
-    }
-  });
-  // shortcuts for outline button
-  jwerty.key('o', function () {
-    $('.mooc-helper-toc').click();
+  $(document).ready(function(){
+    // go back
+    jwerty.key('←', function () {
+      if ($('.page-previous').attr('href')) {
+        window.location = $('.page-previous').attr('href');
+      }
+    });
+    // go forward
+    jwerty.key('→', function () {
+      if ($('.page-next').attr('href')) {
+        window.location = $('.page-next').attr('href');
+      }
+    });
+    // shortcuts for outline button
+    jwerty.key('o', function () {
+      $('.mooc-helper-toc').click();
+    });
   });
 })(jQuery);
--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/features/elmsln_core/js/elmsln-core-jwerty.js
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/features/elmsln_core/js/elmsln-core-jwerty.js
@@ -1,22 +1,24 @@
 (function ($) {
-  jwerty.key('↓,↓', function () {
-    var height = $(window).height();
-    $('html, body').animate({
-        scrollTop: $(window).scrollTop()+(height*0.75)
-    }, 1000);
-  });
-  jwerty.key('↑,↑', function () {
-    var height = $(window).height();
-    $('html, body').animate({
-      scrollTop: $(window).scrollTop()-(height*0.75)
-    }, 1000);
-  });
-  // network
-  jwerty.key('n', function () {
-    $('.apps-icon').click();
-  });
-  // user
-  jwerty.key('u', function () {
-    $('.etb-nav_item_service_btn.etb-icon.user-icon').click();
+  $(document).ready(function(){
+    jwerty.key('↓,↓', function () {
+      var height = $(window).height();
+      $('html, body').animate({
+          scrollTop: $(window).scrollTop()+(height*0.75)
+      }, 1000);
+    });
+    jwerty.key('↑,↑', function () {
+      var height = $(window).height();
+      $('html, body').animate({
+        scrollTop: $(window).scrollTop()-(height*0.75)
+      }, 1000);
+    });
+    // network
+    jwerty.key('n', function () {
+      $('.apps-icon').click();
+    });
+    // user
+    jwerty.key('u', function () {
+      $('.etb-nav_item_service_btn.etb-icon.user-icon').click();
+    });
   });
 })(jQuery);
