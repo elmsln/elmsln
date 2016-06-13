9f681d0237729fab96edd64505880dee63eeb346
btopro <bto108@psu.edu>
2016-06-13 13:22:05 -0400

    a step closer to #567 as we now have continuous mode
    this is getting way too cool… blog post to follow

--- core/dslmcode/shared/drupal-7.x/modules/ulmus/voicecommander/js/voicecommander.js
+++ core/dslmcode/shared/drupal-7.x/modules/ulmus/voicecommander/js/voicecommander.js
@@ -9,10 +9,10 @@
   Drupal.voicecommander = Drupal.voicecommander || { functions: {} }
   // go to a link, this is most common
   Drupal.voicecommander.goTo = function(phrase) {
-    window.location.href = Drupal.settings.basePath + Drupal.settings.voiceCommanderVoiceCommands[phrase].data;
+    window.location.href = Drupal.settings.basePath + Drupal.settings.voiceCommander.commands[phrase].data;
   };
   Drupal.voicecommander.goToELMSLN = function(phrase) {
-    window.location.href = Drupal.settings.voiceCommanderVoiceCommands[phrase].data;
+    window.location.href = Drupal.settings.voiceCommander.commands[phrase].data;
   };
   // go backward in history
   Drupal.voicecommander.back = function(phrase) {
@@ -48,21 +48,6 @@
     var config = this.config;
     var commands = {};
     if (undefined !== annyang && annyang !== null) {
-
-      // Define callbacks.
-      annyang.addCallback('error', function () {
-        showMessage(Drupal.settings.errorCallback);
-      });
-      annyang.addCallback('errorNetwork', function () {
-        showMessage(Drupal.settings.errorNetworkCallback);
-      });
-      annyang.addCallback('errorPermissionBlocked', function () {
-        showMessage(Drupal.settings.permissionBlockedCallback);
-      });
-      annyang.addCallback('errorPermissionDenied', function () {
-        showMessage(Drupal.settings.permissionDeniedCallback);
-      });
-
       function showMessage(message) {
         if (isMobile) {
           $('.voicecommander-mobile .message-area > div.vc-message').text(message);
@@ -77,10 +62,10 @@
         }
       }
       // loop through and add the callbacks into annyang
-      for (var i in Drupal.settings.voiceCommanderVoiceCommands) {
-        var currentKey = Drupal.settings.voiceCommanderVoiceCommands[i].phrase.toLowerCase();
+      for (var i in Drupal.settings.voiceCommander.commands) {
+        var currentKey = Drupal.settings.voiceCommander.commands[i].phrase.toLowerCase();
         // support for wildcards which annyang needs to process directly
-        commands[currentKey] = Drupal.settings.voiceCommanderVoiceCommands[i];
+        commands[currentKey] = Drupal.settings.voiceCommander.commands[i];
       }
       var commandsWithCallbacks = {};
       // now convert to a method that they'd be able to fire correctly when
@@ -100,123 +85,144 @@
       var useItOnce = true;
       var voiceCommanderStr = $('body').append('<div id="voice-commander-rec"></div><div class="voice-commander-rec-window"><div class="icon"></div><p></p></div>');
       var out;
-
-      voiceCommanderStr.keydown(function () {
-        if (event.ctrlKey && event.altKey && useItOnce) {
-
-          $('.voice-commander-rec-window > p').removeClass('speak');
-
-          annyang.abort();
-          clearTimeout(out);
-
-          setTimeout(function () {
-            $('body').addClass('voicecommander');
-            $('.voice-commander-rec-window > p').text(Drupal.settings.waitCallback);
-
-            annyang.start({
-              autoRestart: false,
-              continuous: true
-            });
-
-            annyang.addCallback('start', function () {
-              $('.voice-commander-rec-window > p').addClass('speak').text(Drupal.settings.startCallback);
-            });
-
-          }, 300);
-
-          useItOnce = false;
-        }
-      });
-
-      voiceCommanderStr.keyup(function () {
-        if (event.ctrlKey || event.altKey) {
-          $('body').removeClass('voicecommander');
-
-          out = setTimeout(function () {
-            annyang.abort();
-          }, 1000);
-
-          useItOnce = true;
-        }
-      });
-
-      var detectMobilePattern = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-      var isMobile = (Drupal.settings.voiceCommanderSettingsMobile == 1) ? detectMobilePattern : false;
-
-      if (isMobile) {
-        $('body').addClass('voicecommander-mobile').append('<div class="vc-mobile-button"></div>').append('<div class="message-area"><div class="icon"></div><div class="vc-message"></div><div class="vc-abort"></div></div>');
-        $('.vc-abort').click(function () {
-          annyang.abort();
+      // well now, the future is getting pretty bright; continuous mode overrides all other capabilities
+      if (Drupal.settings.voiceCommander.continuous == true) {
+        annyang.start({
+          autoRestart: true,
+          continuous: true
         });
+      }
+      else {
+        // Define callbacks.
+        annyang.addCallback('error', function () {
+          showMessage(Drupal.settings.voiceCommander.errorCallback);
+        });
+        annyang.addCallback('errorNetwork', function () {
+          showMessage(Drupal.settings.voiceCommander.errorNetworkCallback);
+        });
+        annyang.addCallback('errorPermissionBlocked', function () {
+          showMessage(Drupal.settings.voiceCommander.permissionBlockedCallback);
+        });
+        annyang.addCallback('errorPermissionDenied', function () {
+          showMessage(Drupal.settings.voiceCommander.permissionDeniedCallback);
+        });
+        voiceCommanderStr.keydown(function () {
+          if (event.ctrlKey && event.altKey && useItOnce) {
 
-        var button = $('.voicecommander-mobile > .vc-mobile-button');
+            $('.voice-commander-rec-window > p').removeClass('speak');
 
-        function message_start() {
-          var message = $('body.voicecommander-mobile .message-area').css('display', 'block');
-          // Message area animation.
-          $('body.voicecommander-mobile').animate({
-            margin: "20px 0 0 0"
-          }, 400);
-
-          function messageAnimation_start() {
-            message.animate({
-              top: "20px"
-            }, 400);
-          }
-          messageAnimation_start();
-        }
-
-        function message_stop() {
-          var message = $('body.voicecommander-mobile .message-area').css('display', 'block');
-          var icon = $('.voicecommander-mobile .message-area .icon');
-          // Message area animation.
-          $('body.voicecommander-mobile').css('margin', '0 0 0 0');
-
-          function messageAnimation_stop() {
-            message.animate({
-              top: "0px"
-            }, 400);
-          }
-          messageAnimation_stop();
-        }
-
-        button.hover(function () {
-          setTimeout(function () {
-            clearTimeout(out);
             annyang.abort();
-            showMessage(Drupal.settings.waitCallback);
+            clearTimeout(out);
 
             setTimeout(function () {
-              annyang.start();
-              message_start();
+              $('body').addClass('voicecommander');
+              $('.voice-commander-rec-window > p').text(Drupal.settings.voiceCommander.waitCallback);
+
+              annyang.start({
+                autoRestart: false,
+                continuous: true
+              });
 
               annyang.addCallback('start', function () {
-                showMessage(Drupal.settings.startCallback);
-                // Icon animation.
-                var icon = $('.voicecommander-mobile .message-area .icon');
-
-                function iconAnimation_start() {
-                  icon.animate({
-                    opacity: 0
-                  }, 400).animate({
-                    opacity: 1
-                  }, 400);
-                }
-
-                iconAnimation_start();
-
-                setInterval(function () {
-                  iconAnimation_start();
-                }, 800);
+                $('.voice-commander-rec-window > p').addClass('speak').text(Drupal.settings.voiceCommander.startCallback);
               });
+
             }, 300);
-          }, 600);
-        }, function () {
-          message_stop();
-          out = setTimeout(function () {
-            annyang.abort();
-          }, 2000);
+
+            useItOnce = false;
+          }
         });
+
+        voiceCommanderStr.keyup(function () {
+          if (event.ctrlKey || event.altKey) {
+            $('body').removeClass('voicecommander');
+
+            out = setTimeout(function () {
+              annyang.abort();
+            }, 1000);
+
+            useItOnce = true;
+          }
+        });
+
+        var detectMobilePattern = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        var isMobile = (Drupal.settings.voiceCommander.mobile == 1) ? detectMobilePattern : false;
+
+        if (isMobile) {
+          $('body').addClass('voicecommander-mobile').append('<div class="vc-mobile-button"></div>').append('<div class="message-area"><div class="icon"></div><div class="vc-message"></div><div class="vc-abort"></div></div>');
+          $('.vc-abort').click(function () {
+            annyang.abort();
+          });
+
+          var button = $('.voicecommander-mobile > .vc-mobile-button');
+
+          function message_start() {
+            var message = $('body.voicecommander-mobile .message-area').css('display', 'block');
+            // Message area animation.
+            $('body.voicecommander-mobile').animate({
+              margin: "20px 0 0 0"
+            }, 400);
+
+            function messageAnimation_start() {
+              message.animate({
+                top: "20px"
+              }, 400);
+            }
+            messageAnimation_start();
+          }
+
+          function message_stop() {
+            var message = $('body.voicecommander-mobile .message-area').css('display', 'block');
+            var icon = $('.voicecommander-mobile .message-area .icon');
+            // Message area animation.
+            $('body.voicecommander-mobile').css('margin', '0 0 0 0');
+
+            function messageAnimation_stop() {
+              message.animate({
+                top: "0px"
+              }, 400);
+            }
+            messageAnimation_stop();
+          }
+
+          button.hover(function () {
+            setTimeout(function () {
+              clearTimeout(out);
+              annyang.abort();
+              showMessage(Drupal.settings.voiceCommander.waitCallback);
+
+              setTimeout(function () {
+                annyang.start();
+                message_start();
+
+                annyang.addCallback('start', function () {
+                  showMessage(Drupal.settings.voiceCommander.startCallback);
+                  // Icon animation.
+                  var icon = $('.voicecommander-mobile .message-area .icon');
+
+                  function iconAnimation_start() {
+                    icon.animate({
+                      opacity: 0
+                    }, 400).animate({
+                      opacity: 1
+                    }, 400);
+                  }
+
+                  iconAnimation_start();
+
+                  setInterval(function () {
+                    iconAnimation_start();
+                  }, 800);
+                });
+              }, 300);
+            }, 600);
+          }, function () {
+            message_stop();
+            out = setTimeout(function () {
+              annyang.abort();
+            }, 2000);
+          });
+        }
       }
     }
   });
