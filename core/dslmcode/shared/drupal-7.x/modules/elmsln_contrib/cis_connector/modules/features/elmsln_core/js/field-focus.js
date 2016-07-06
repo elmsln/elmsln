589b2313d3f1d8a6381e26c8c3d776e39851a605
btopro <bto108@psu.edu>
2016-07-06 14:58:03 -0400

    #1005

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/features/elmsln_core/js/field-focus.js
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/features/elmsln_core/js/field-focus.js
@@ -1,11 +1,9 @@
 /**
- * Allow for auto selection of iframe content area on focus
+ * Focus on the title field when the frame loads
  */
 (function ($) {
-  Drupal.behaviors.elmsln_core_node_form = {
-    attach: function(context) {
-      // set title as focus field when we're creating a new item
-      $('#edit-title').focus();
-    }
-  };
+  $(document).ready(function(){
+    // set title as focus field when we're creating a new item
+    $('#edit-title').focus();
+  });
 })(jQuery);
