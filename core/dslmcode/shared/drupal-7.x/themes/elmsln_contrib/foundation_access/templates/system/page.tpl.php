8cf7a4577135f667d8a60996a73a5655acbda585
Bryan btopro Ollendyke <bto108@psu.edu>
2016-07-12 11:10:10 -0400

    use original branch copy of page.tpl.php

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/system/page.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/system/page.tpl.php
@@ -11,7 +11,6 @@
     $cis_lmsless['active']['title'] = '';
   }
 ?>
-
     <div id="etb-tool-nav" class="off-canvas-wrap" data-offcanvas>
       <div class="inner-wrap">
         <!-- progress bar -->
@@ -29,52 +28,30 @@
                 <?php if (isset($edit_path)): ?>
                 <li class="r-header__edit-icons__list-item">
                   <?php if (arg(2) == 'edit'): ?>
-                  <a href="#" onclick="document.getElementById('edit-submit').click();" title="<?php print t('Save')?>" class="r-header__icon">
+                  <a href="#" onclick="document.getElementById('edit-submit').click();" title="<?php print t('Save')?>" class="r-header__icon elmsln-edit-button">
                     <i class="zmdi zmdi-save"></i>
                   </a>
                 <?php else: ?>
-                  <a href="<?php print $edit_path; ?>" title="<?php print t('Edit content')?>" class="r-header__icon">
+                  <a href="<?php print $edit_path; ?>" title="<?php print t('Edit content')?>" class="r-header__icon  elmsln-edit-button" data-jwerty-key="e" data-voicecommand="edit">
                     <i class="zmdi zmdi-edit"></i>
                   </a>
                 <?php endif; ?>
                 </li>
                 <?php endif; ?>
-<!--
-                <li class="r-header__edit-icons__list-item">
-                  <md-menu md-position-mode="target-right target">
-                    <md-button aria-label="Course outline options menu" class="md-icon-button r-header__icon" ng-click="ctrl.openMenu($mdOpenMenu, $event)">
-                     <i class="zmdi zmdi-collection-text"></i>
-                    </md-button>
-                   <md-menu-content width="4">
-                     <md-menu-item>
-                       <md-button href="#">Add child page</md-button>
-                     </md-menu-item>
-                     <md-menu-item>
-                       <md-button href="#">Edit child outline</md-button>
-                     </md-menu-item>
-                     <md-menu-item>
-                       <md-button href="#">Edit course outline</md-button>
-                     </md-menu-item>
-                     <md-menu-item>
-                       <md-button href="#">Duplicate outline</md-button>
-                     </md-menu-item>
-                    </md-menu-content>
-                   </md-menu>
-                </li> -->
                 <?php if (!empty($cis_shortcodes)) : ?>
-                  <li class="r-header__edit-icons__list-item"><a href="#" title="<?php print t('Share')?>" class="r-header__icon" data-reveal-id="block-cis-shortcodes-cis-shortcodes-block-nav-modal" aria-controls="cis-shortcodes-drop" aria-expanded="false">
+                  <li class="r-header__edit-icons__list-item"><a href="#" title="<?php print t('Share')?>" class="r-header__icon elmsln-share-button" data-reveal-id="block-cis-shortcodes-cis-shortcodes-block-nav-modal" aria-controls="cis-shortcodes-drop" aria-expanded="false" data-jwerty-key="s" data-voicecommand="open share (menu)">
                     <i class="zmdi zmdi-arrow-split"></i>
                   </a></li>
                 <?php endif; ?>
-                <?php if (isset($speedreader) || isset($mespeak)) : ?>
-                  <li class="r-header__edit-icons__list-item"><a href="#" title="Speed Reader" class="r-header__icon" data-reveal-id="page-accessibility-menu" aria-controls="accessibility-drop" aria-expanded="false">
+                <?php if (!empty($a11y)) : ?>
+                  <li class="r-header__edit-icons__list-item"><a href="#" title="<?php print t('Accessibility options')?>" class="r-header__icon elmsln-accessibility-button" data-reveal-id="page-accessibility-menu" aria-controls="accessibility-drop" aria-expanded="false" data-jwerty-key="a" data-voicecommand="open accessibility (menu)">
                     <i class="zmdi zmdi-tune"></i>
                   </a></li>
                 <?php endif; ?>
                 <!-- end Edit Icon -->
                 <li class="r-header__edit-icons__list-item">
                 <?php if (!empty($tabs['#primary']) || !empty($tabs['#secondary']) || !empty($tabs_extras)): ?>
-                    <a href="#" title="<?php print t('More')?>" class="r-header__icon" data-dropdown="r-header__icon--advanced" aria-controls="r-header__icon--advanced" aria-expanded="false">
+                    <a href="#" title="<?php print t('More')?>" class="r-header__icon elmsln-more-button" data-dropdown="r-header__icon--advanced" aria-controls="r-header__icon--advanced" aria-expanded="false" data-jwerty-key="m" data-voicecommand="open more (menu)">
                       <i class="zmdi zmdi-more-vert"></i>
                     </a>
                     <ul id="r-header__icon--advanced" data-dropdown-content class="f-dropdown f-dropdown--classic content" aria-hidden="true" tabindex="-1">
@@ -94,7 +71,7 @@
                     <?php endif; ?>
                     </ul>
                 <?php else: ?>
-                  <a href="#" title="<?php print t('No additional options')?>" class="r-header__icon fa-action-disabled">
+                  <a href="#" title="<?php print t('No additional options')?>" class="r-header__icon fa-action-disabled  elmsln-more-button">
                       <i class="zmdi zmdi-more-vert"></i>
                     </a>
                 <?php endif; ?>
@@ -186,7 +163,7 @@
 <div id="page-accessibility-menu" class="reveal-modal" data-reveal aria-labelledby="<?php print t('Accessibility'); ?>" aria-hidden="true" role="dialog">
   <h2 id="Accessibility"><?php print t('Accessibility') ?></h2>
   <?php print $a11y; ?>
-  <a class="close-reveal-modal">&#215;</a>
+  <a class="close-reveal-modal" data-voicecommand="close (menu)" data-jwerty-key="Esc">&#215;</a>
 </div>
 <?php endif; ?>
 <!-- /Accessibility Modal -->
