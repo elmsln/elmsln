6836ac355540e7e939c3dcccd9ebb2b44db9b6a6
Bryan btopro Ollendyke <bto108@psu.edu>
2016-07-18 14:54:05 -0400

    make sure api statements populate based on match

--- core/dslmcode/profiles/elmsmedia-7.x-1.x/modules/features/elmsmedia_h5p/js/elmsmedia-xapi-listener.js
+++ core/dslmcode/profiles/elmsmedia-7.x-1.x/modules/features/elmsmedia_h5p/js/elmsmedia-xapi-listener.js
@@ -2,14 +2,15 @@
 // of this content item being framed to display elsewhere
 (function ($) {
   Drupal.elmslnMessage = function(e) {
-    // look at the message that came over and assemble our 4 parts of the message
-    // @todo only do this from trusted sources which we'll have knowledge of
-    var data = e.data;
-    Drupal.settings.h5pTincanBridge.course = data.course;
-    Drupal.settings.h5pTincanBridge.section = data.section;
-    Drupal.settings.h5pTincanBridge.title = data.title;
-    Drupal.settings.h5pTincanBridge.url = data.url;
-    Drupal.settings.h5pTincanBridge.role = data.role;
+    // only read in data if this is a trusted domain via browser policy
+    if (e.isTrusted && e.data.subject == 'elmsln.xapiContext') {
+      var data = e.data;
+      Drupal.settings.h5pTincanBridge.course = data.course;
+      Drupal.settings.h5pTincanBridge.section = data.section;
+      Drupal.settings.h5pTincanBridge.title = data.title;
+      Drupal.settings.h5pTincanBridge.url = data.url;
+      Drupal.settings.h5pTincanBridge.role = data.role;
+    }
   };
   window.addEventListener("message", Drupal.elmslnMessage, false);
 
