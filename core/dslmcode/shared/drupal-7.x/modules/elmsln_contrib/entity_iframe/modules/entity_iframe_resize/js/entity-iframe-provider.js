c7c06d10c50307c0fa2264fd265484a2e4cf42e3
Bryan btopro Ollendyke <bto108@psu.edu>
2016-07-18 21:28:42 -0400

    #1040

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/entity_iframe/modules/entity_iframe_resize/js/entity-iframe-provider.js
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/entity_iframe/modules/entity_iframe_resize/js/entity-iframe-provider.js
@@ -2,23 +2,6 @@
 // set event listener for our post back message from parents
 // of this content item being framed to display elsewhere
 (function ($) {
-  // iframe resize function that finds who triggered it
-  Drupal.entityIframeResize = function(e) {
-    // look at the message that came over and assemble our 4 parts of the message
-    // only read in data if this is a trusted domain via browser policy
-    if (e.isTrusted && e.data.subject == 'entityIframe.resize') {
-      // Find out who sent the message
-      var iframes = document.getElementsByTagName('iframe');
-      for (var i = 0; i < iframes.length; i++) {
-        if (iframes[i].contentWindow === event.source) {
-          $(iframes[i]).attr('height', e.data.height);
-          break;
-        }
-      }
-    }
-  };
-  // listen for receiving the message and then resizing to that height
-  window.addEventListener("message", Drupal.entityIframeResize, false);
   // wire up the mutator observer so we can detect a change in the frame to post
   $(document).ready(function(){
     // account for prefixed mutators in older browsers
