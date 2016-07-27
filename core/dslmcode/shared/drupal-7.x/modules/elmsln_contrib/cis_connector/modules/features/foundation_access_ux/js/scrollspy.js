f6ae5f2f1a9446bb3cab9653de5e0fa009298ce3
Bryan btopro Ollendyke <bto108@psu.edu>
2016-07-27 00:27:13 -0400

    nicer effect when jogging back to the placement in the document

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/features/foundation_access_ux/js/scrollspy.js
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/features/foundation_access_ux/js/scrollspy.js
@@ -4,5 +4,13 @@
     $('h2[data-scrollspy="scrollspy"],h3[data-scrollspy="scrollspy"],h4[data-scrollspy="scrollspy"]').addClass('scrollspy');
     // activate class
     $('.scrollspy').scrollSpy();
+    $('.scrollspy-toc').pushpin({top: $('#scrollspy-nav').offset().top });
+    $('.scrollspy-link').click(function(){
+      $($(this).attr('href')).css('background-color', '#FF8866').animate({
+        backgroundColor: 'transparent',
+        },
+        1000, function() {
+      });
+    });
   });
 })(jQuery);
