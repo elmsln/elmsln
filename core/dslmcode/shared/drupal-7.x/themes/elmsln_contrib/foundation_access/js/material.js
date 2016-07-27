2064ec07b67e61de1a5b39f80edbf99ad926c3c1
Bryan btopro Ollendyke <bto108@psu.edu>
2016-07-27 00:56:38 -0400

    waves refactor after move to materialize

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/material.js
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/material.js
@@ -3,26 +3,22 @@
 // Add wave effect.
 Drupal.behaviors.foundation_access_waves = {
   attach: function (context, settings) {
+    var waveEffect = "waves-effect waves-orange";
     // MOOC
-    $(".mooc-helper-toc").addClass("waves-effect waves-button waves-classic");
-    $("#block-mooc-helper-mooc-helper-toc-nav-modal a").addClass("waves-effect waves-button waves-classic");
-    $(".book-sibling-nav-container li").addClass("waves-effect waves-button waves-classic");
+    $(".mooc-helper-toc, #block-mooc-helper-mooc-helper-toc-nav-modal a, .book-sibling-nav-container li").addClass(waveEffect);
     // GENERIC
-    $("button").addClass("waves-effect waves-button waves-classic");
+    $("button").addClass(waveEffect);
     // DROPDOWNS
-    $("#r-header__icon--advanced li, #courseToolsMenu li").addClass("waves-button waves-classic");
+    $("#r-header__icon--advanced li, #courseToolsMenu li").addClass(waveEffect);
     // TABS
-    $(".horizontal-tabs-list .horizontal-tab-button").addClass("waves-effect waves-button waves-classic");
+    $(".horizontal-tabs-list .horizontal-tab-button").addClass(waveEffect);
     // LMSLESS
-    $(".top-bar.etb-nav ul.left li.apps").addClass("waves-effect waves-classic");
-    $("#block-cis-lmsless-cis-lmsless-network-nav-modal a").addClass("waves-effect waves-button waves-classic");
+    $("#block-cis-lmsless-cis-lmsless-network-nav-modal a, .elmsln-network-button, .elmsln-user-button, .elmsln-syllabus-button, .elmsln-resource-button, .elmsln-help-button").addClass(waveEffect);
     // INPAGE WIDGET
     //$(".cis-filter-activity-item").addClass("waves-effect waves-button waves-classic");
+    $(".cis-filter-activity-item ul.submit-widget-links li").addClass(waveEffect);
     // IN PAGE MENU
-    $(".header-menu-options li").addClass("waves-effect waves-classic");
-    $(".elmsln-home-button-link").addClass("waves-effect waves-button waves-classic");
-    // submit widget
-    $(".cis-filter-activity-item ul.submit-widget-links li").addClass("waves-effect waves-button waves-classic waves-float");
+    $(".header-menu-options li, .elmsln-home-button-link").addClass(waveEffect);
   }
 };
   Drupal.behaviors.wavesInit = {
