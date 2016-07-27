293c756964e0ebfc5a3293704d8bff1642f6f49f
Bryan btopro Ollendyke <bto108@psu.edu>
2016-07-27 00:31:34 -0400

    better UX on scroll

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/features/foundation_access_ux/js/scrollspy.js
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/features/foundation_access_ux/js/scrollspy.js
@@ -4,7 +4,7 @@
     $('h2[data-scrollspy="scrollspy"],h3[data-scrollspy="scrollspy"],h4[data-scrollspy="scrollspy"]').addClass('scrollspy');
     // activate class
     $('.scrollspy').scrollSpy();
-    $('.scrollspy-toc').pushpin({top: $('#scrollspy-nav').offset().top });
+    $('.scrollspy-toc').pushpin({offset: 50, top: $('#scrollspy-nav').offset().top });
     $('.scrollspy-link').click(function(){
       $($(this).attr('href')).css('background-color', '#FF8866').animate({
         backgroundColor: 'transparent',
