aec8860a4302c2dc9a95aebf1e76583357b6a6f0
heyMP <mgp140@gmail.com>
2016-08-01 17:31:31 -0400

    Here is the new dashboard feature.

---
+++ core/dslmcode/profiles/cle-7.x-1.x/modules/features/cle_dashboard/js/cle-dashboard.js
@@ -0,0 +1,12 @@
+Drupal.behaviors.cleDashboard = {
+  attach: function (context, settings) {
+    angular.module('cleDashboard', ['ngSanitize'])
+      .controller('cleAssignmentsController', ['$scope', function($scope) {
+        if (Drupal.settings.cleDashboard) {
+          $scope.data = Drupal.settings.cleDashboard;
+          $scope.basePath = Drupal.settings.basePath;
+        }
+      }])
+    ; // end Angular
+  }
+};
---
+++ core/dslmcode/profiles/cle-7.x-1.x/modules/features/cle_dashboard/templates/cle-dashboard.tpl.php
@@ -0,0 +1,71 @@
+<div ng-app="cleDashboard" ng-controller="cleAssignmentsController" class="cle-dashboard">
+  <div class="collection with-header">
+    <div class="collection-header"><h3>My Assignments</h3></div>
+    <a class="cle-dashboard__assignment" href="{{basePath}}node/{{assignment.nid}}" ng-repeat="assignment in data.assignments" ng-if="assignment.nid" ng-cloak>
+      <div class="collapsible-header">
+        <i class="material-icons">assignment</i>
+        <b>{{assignment.node_title}}</b>
+        <div class="secondary-content">
+          <span class="chip" ng-if="assignment.active"> Open For Submissions </span>
+          <span class="chip" ng-if="assignment.closed"> Closed </span>
+          <span class="chip completed" ng-if="assignment.completed"> <i class="material-icons">check</i> Done </span>
+        </div>
+        <div class="post-date">
+          <span> Due: </span>
+          <span ng-bind-html="assignment.field_field_begin_date[0].rendered['#markup']"></span> to <span ng-bind-html="assignment.field_field_due_date[0].rendered['#markup']"></span>
+        </div>
+      </div>
+    </a>
+
+    <a class="cle-dashboard__critique" href="{{assignment.critique.url}}" ng-repeat="assignment in data.assignments" ng-if="assignment.critique" ng-if="assignment.nid" ng-cloak>
+      <div class="collapsible-header">
+        <i class="material-icons">subdirectory_arrow_right</i>
+        <b>{{assignment.node_title}} Peer Review</b>
+        <i class="material-icons right-align">comment</i>
+        <div class="secondary-content">
+          <span class="chip completed" ng-if="assignment.critique.completed"> <i class="material-icons">check</i> Done </span>
+        </div>
+        <div class="meta-content">
+        </div>
+        <div class="post-date">
+          <span> Due: </span>
+          <span ng-bind-html="assignment.field_field_critique_date[0].rendered['#markup']">  </span>
+        </div>
+      </div>
+    </a>
+  </div>
+
+  <div class="collection with-header">
+    <div class="collection-header"><h3>My Submissions</h3></div>
+    <a href="{{basePath}}node/{{submission.nid}}" class="avatar" ng-repeat="submission in data.submissions" ng-cloak>
+      <div class="collapsible-header"><i class="material-icons">subject</i>
+        <b>{{submission.node_title}}</b>
+        <div class="secondary-content">
+          <span class="chip" ng-bind-html="submission.field_field_submission_state[0]['rendered']['#markup']"></span>
+        </div>
+        <div class="meta-content">
+          <div>Assignment: {{submission.node_field_data_field_assignment_title}}</div>
+        </div>
+        <div class="post-date" ng-if="submission.node_created">
+          <span> Posted: </span> {{ submission.node_created | date: 'EEE, MM-dd-yy'}}
+        </div>
+      </div>
+    </a>
+  </div>
+
+  <div class="collection with-header">
+    <div class="collection-header"><h3>My Reviews</h3></div>
+    <a href="{{basePath}}node/{{critique.nid}}" class="avatar" ng-repeat="critique in data.critiques" ng-cloak>
+      <div class="collapsible-header"><i class="material-icons"> comment </i>
+        <b>{{critique.node_title}}</b>
+        <div class="meta-content">
+          <div>Submission: {{critique.node_field_data_field_cle_crit_sub_ref_title}}</div>
+        </div>
+        <div class="post-date" ng-if="critique.node_created">
+          <span> Posted: </span> {{ critique.node_created | date: 'EEE, MM-dd-yy'}}
+        </div>
+      </div>
+    </a>
+  </div>
+
+</div>
