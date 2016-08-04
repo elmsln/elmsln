2b6086897925f2cfe5e058153b355ba70c21ef62
Bryan btopro Ollendyke <bto108@psu.edu>
2016-08-04 19:40:57 -0400

    #1056 !

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/core/cis_filter/templates/cis-filter-render--iframe.tpl.php
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/core/cis_filter/templates/cis-filter-render--iframe.tpl.php
@@ -6,5 +6,6 @@
   //  $link - link to what the frame is pointing to
   //  $width - width of the frame
   //  $height - height of the frame
+  //  $hypothesis - possible xAPI hypothesis
 ?>
-<iframe id="<?php print $id; ?>" frameborder="0" class="<?php print $class; ?>" src="<?php print $link; ?>" width="<?php print $width; ?>" height="<?php print $height; ?>" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+<iframe id="<?php print $id; ?>" class="<?php print $class; ?>" src="<?php print $link; ?>" width="<?php print $width; ?>" height="<?php print $height; ?>" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-xapi-hypothesis="<?php print $hypothesis; ?>"></iframe>
