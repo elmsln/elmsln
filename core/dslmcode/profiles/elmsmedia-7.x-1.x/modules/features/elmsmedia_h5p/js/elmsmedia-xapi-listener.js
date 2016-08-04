752ba854d9ba9ce891637de9254864ea3c242d88
Bryan btopro Ollendyke <bto108@psu.edu>
2016-08-04 19:40:57 -0400

    #1056 !

--- core/dslmcode/profiles/elmsmedia-7.x-1.x/modules/features/elmsmedia_h5p/js/elmsmedia-xapi-listener.js
+++ core/dslmcode/profiles/elmsmedia-7.x-1.x/modules/features/elmsmedia_h5p/js/elmsmedia-xapi-listener.js
@@ -2,17 +2,31 @@
 // of this content item being framed to display elsewhere
 (function ($) {
   Drupal.elmslnMessage = function(e) {
-    // only read in data if this is a trusted domain via browser policy
-    if (e.isTrusted && e.data.subject == 'elmsln.xapiContext') {
-      var data = e.data;
-      Drupal.settings.h5pTincanBridge.course = data.course;
-      Drupal.settings.h5pTincanBridge.section = data.section;
-      Drupal.settings.h5pTincanBridge.title = data.title;
-      Drupal.settings.h5pTincanBridge.url = data.url;
-      Drupal.settings.h5pTincanBridge.role = data.role;
-      Drupal.settings.h5pTincanBridge.hypothesis = data.hypothesis;
+    if (e.data.subject == 'elmsln.xapiContext') {
+      var allowed = false;
+      if (e.isTrusted) {
+        allowed = true;
+      }
+      else {
+        // check frame
+        var frame = e.origin.split('.');
+        var local = window.location.origin.split('.')
+        // validate that the subdomain is the same in each
+        if (frame.pop() == local.pop() && frame.pop() == local.pop()) {
+          allowed = true;
+        }
+      }
+      // mix in this xAPI data if we are allowed
+      if (allowed) {
+        var data = e.data;
+        Drupal.settings.h5pTincanBridge.course = data.course;
+        Drupal.settings.h5pTincanBridge.section = data.section;
+        Drupal.settings.h5pTincanBridge.title = data.title;
+        Drupal.settings.h5pTincanBridge.url = data.url;
+        Drupal.settings.h5pTincanBridge.role = data.role;
+        Drupal.settings.h5pTincanBridge.hypothesis = data.hypothesis;
+      }
     }
   };
   window.addEventListener("message", Drupal.elmslnMessage, false);
-
 })(jQuery);
