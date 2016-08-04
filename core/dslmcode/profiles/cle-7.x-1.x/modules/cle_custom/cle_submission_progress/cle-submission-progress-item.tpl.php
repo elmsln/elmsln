249711e7d5833875462db9a8dd6694bdcbc81e8b
heyMP <mgp140@gmail.com>
2016-08-04 12:41:43 -0400

    Added a cle_submission_progress module.

---
+++ core/dslmcode/profiles/cle-7.x-1.x/modules/cle_custom/cle_submission_progress/cle-submission-progress-item.tpl.php
@@ -0,0 +1,7 @@
+<div class="cle-submission-progress__col">
+  <div class="cle-submission-progress__item <?php print implode(' ', $classes_array); ?>">
+    <i class="cle-submission-progress__icon material-icons"><?php print render($icon); ?></i>
+    <div class="cle-submission-progress__state"><?php print render($name); ?></div>
+    <div class="cle-submission-progress__description"><?php print render($description); ?></div>
+  </div>
+</div>
---
+++ core/dslmcode/profiles/cle-7.x-1.x/modules/cle_custom/cle_submission_progress/cle-submission-progress.tpl.php
@@ -0,0 +1,3 @@
+<div class="row cle-submission-progress">
+  <?php print render($content); ?>
+</div>
