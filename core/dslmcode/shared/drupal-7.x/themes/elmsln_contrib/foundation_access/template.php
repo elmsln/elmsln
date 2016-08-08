97406768fbf70fce305248e07bb6f9c71df851a2
Bryan btopro Ollendyke <bto108@psu.edu>
2016-08-08 11:56:29 -0400

    1 less resource load via CDN

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/template.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/template.php
@@ -39,9 +39,8 @@ function foundation_access_preprocess_html(&$variables) {
 
   drupal_add_css($css, array('type' => 'inline', 'group' => CSS_THEME, 'weight' => 999));
   drupal_add_css(drupal_get_path('theme', 'foundation_access') . '/bower_components/material-design-iconic-font/dist/css/material-design-iconic-font.min.css', array('group' => CSS_THEME, 'weight' => 1001));
-  // google font / icon cdns
-  drupal_add_css('//fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic|Open+Sans:300,600,700)', array('type' => 'external', 'group' => CSS_THEME, 'weight' => 1000));
-  drupal_add_css('//fonts.googleapis.com/icon?family=Material+Icons', array('type' => 'external', 'group' => CSS_THEME, 'weight' => 999));
+  // google font / icons from google
+  drupal_add_css('//fonts.googleapis.com/css?family=Material+Icons|Droid+Serif:400,700,400italic,700italic|Open+Sans:300,600,700)', array('type' => 'external', 'group' => CSS_THEME, 'weight' => 1000));
   // bring in materialize
   $libraries = libraries_get_libraries();
   // see if we have it locally before serviing CDN
