b4687ebb378e3081542b0ae2e4e60db50eb97b8c
Bryan btopro Ollendyke <bto108@psu.edu>
2016-08-09 16:46:02 -0400

    more scrollbar standardization

--- core/dslmcode/profiles/mooc-7.x-1.x/themes/mooc_foundation_access/templates/blocks/block--mooc-helper--mooc-helper-toc.tpl.php
+++ core/dslmcode/profiles/mooc-7.x-1.x/themes/mooc_foundation_access/templates/blocks/block--mooc-helper--mooc-helper-toc.tpl.php
@@ -1,4 +1,4 @@
-<div id="<?php print $block_html_id ?>-nav-modal" class="reveal-modal etb-nav-modal disable-scroll" data-reveal tabindex="-1">
+<div id="<?php print $block_html_id ?>-nav-modal" class="elmsln-scroll-bar reveal-modal etb-nav-modal disable-scroll" data-reveal tabindex="-1">
 <?php print l('<div class="mooc-helper-toc-icon icon-courses-black etb-modal-icons"></div><span>Home</span>', '<front>', array('html' => TRUE, 'attributes' => array('data-voicecommand' => t('home'), 'class' => array('mooc-home-button'))));?><h3><?php print t('Table of Contents'); ?></h3>
 <?php print $content; ?>
 <a href="#" class="close-reveal-modal" aria-label="Close" data-voicecommand="close (menu)" data-jwerty-key="Esc">&#215;</a>
