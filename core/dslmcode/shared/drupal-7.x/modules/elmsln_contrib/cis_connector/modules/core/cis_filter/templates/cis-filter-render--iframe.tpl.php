339e61635301eb81f24ad33203e40171d06c7319
Bryan btopro Ollendyke <bto108@psu.edu>
2016-08-12 10:18:49 -0400

    #847 now we can extract duration from our own media and pull it across if its available!

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/core/cis_filter/templates/cis-filter-render--iframe.tpl.php
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/core/cis_filter/templates/cis-filter-render--iframe.tpl.php
@@ -7,5 +7,15 @@
   //  $width - width of the frame
   //  $height - height of the frame
   //  $hypothesis - possible xAPI hypothesis
+  //  $response - the full response object from the CIS object request
+  $attributes = array();
+  // special support for video
+  if (isset($response['field_video']) && isset($response['field_video']['metadata']) && isset($response['field_video']['metadata']['duration'])) {
+    $attributes = array('data-duration' => $response['field_video']['metadata']['duration']);
+  }
+  // special support for audio
+  if (isset($response['field_audio']) && isset($response['field_audio']['metadata']) && isset($response['field_audio']['metadata']['duration'])) {
+    $attributes = array('data-duration' => $response['field_audio']['metadata']['duration']);
+  }
 ?>
-<iframe id="<?php print $id; ?>" class="<?php print $class; ?>" src="<?php print $link; ?>" width="<?php print $width; ?>" height="<?php print $height; ?>" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-xapi-hypothesis="<?php print $hypothesis; ?>"></iframe>
+<iframe <?php print drupal_attributes($attributes); ?> id="<?php print $id; ?>" class="<?php print $class; ?>" src="<?php print $link; ?>" width="<?php print $width; ?>" height="<?php print $height; ?>" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-xapi-hypothesis="<?php print $hypothesis; ?>"></iframe>
