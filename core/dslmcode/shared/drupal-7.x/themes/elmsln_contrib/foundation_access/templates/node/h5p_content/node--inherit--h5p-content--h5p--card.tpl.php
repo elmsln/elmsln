231b3c42c1098f88c7a0d491873871500f46bb3d
Bryan btopro Ollendyke <bto108@psu.edu>
2016-09-12 13:37:46 -0400

    more template clean up

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/node/h5p_content/node--inherit--h5p-content--h5p--card.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/node/h5p_content/node--inherit--h5p-content--h5p--card.tpl.php
@@ -1,6 +1,6 @@
 <div class="row">
   <div class="col s12 m10 push-m1 l8 push-l2 center-align card-panel hoverable">
     <h4><?php print render($title);?></h4>
-    <iframe id="node_<?php print $id;?>" class="entity_iframe entity_iframe_node entity_iframe_tool_elmsmedia elmsmedia_h5p_content" src="<?php print $iframe_path; ?>" width="100%" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+    <iframe id="node_<?php print $nid;?>" class="entity_iframe entity_iframe_node entity_iframe_tool_elmsmedia elmsmedia_h5p_content" src="<?php print $iframe_path; ?>" width="100%" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
   </div>
 </div>
