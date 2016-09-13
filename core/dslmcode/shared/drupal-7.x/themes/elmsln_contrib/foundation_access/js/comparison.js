3edfc6f00b50a98ebfe74048ab4ab561cfb38da9
Bryan btopro Ollendyke <bto108@psu.edu>
2016-09-13 13:12:33 -0400

    #1112 more comparison clean up

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/comparison.js
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/comparison.js
@@ -2,7 +2,7 @@
 	$(document).ready(function(){
     $(".resizable").resizable({
       maxHeight: 316,
-      maxWidth: 640,
+      maxWidth: 630,
       minHeight: 10,
       minWidth: 10,
 			handles: "e, s, se",
--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/node/image-gallery/node--inherit--image-gallery--image-gallery--comparison.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/node/image-gallery/node--inherit--image-gallery--image-gallery--comparison.tpl.php
@@ -1,7 +1,11 @@
 <div class="elmsln-image-gallery-wrapper rp-wrapper">
   <div class="image-compare">
   <?php foreach ($images as $key => $image) : ?>
-    <div class="resizable ui-widget-content comparison-image-<?php print $key;?>" style="background-image:url('<?php print file_create_url($image['#item']['uri']);?>')"></div>
+    <?php if ($key == 0) : ?>
+      <div class="comparison-image-<?php print ($key + 1);?>" style="background-image:url('<?php print file_create_url($image['#item']['uri']);?>')"></div>
+    <?php elseif ($key < 2) : ?>
+    <div class="resizable ui-widget-content comparison-image-<?php print ($key + 1);?>" style="background-image:url('<?php print file_create_url($image['#item']['uri']);?>')"></div>
+    <?php endif; ?>
   <?php endforeach; ?>
   </div>
 </div>
