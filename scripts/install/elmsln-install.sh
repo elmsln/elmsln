b0f34bfafe5a99d118ccd8a6ead4116949c905b4
Brad Fisher <bradallenfisher@gmail.com>
2016-09-13 10:48:38 -0400

    cleaning up tmp dir

--- scripts/install/elmsln-install.sh
+++ scripts/install/elmsln-install.sh
@@ -205,8 +205,7 @@ for tool in "${authoritylist[@]}"
   # setup private and tmp file directories
   sudo mkdir -p $drupal_priv/$tool
   sudo mkdir -p $drupal_priv/$tool/$tool
-  sudo mkdir -p $drupal_tmp/$tool
-  sudo mkdir -p $drupal_tmp/$tool/$tool
+  sudo mkdir -p $drupal_tmp
   sudo chown -R $wwwuser:$webgroup $drupal_priv
   sudo chown -R $wwwuser:$webgroup $drupal_tmp
   sudo chmod -R 755 $drupal_priv
@@ -234,7 +233,7 @@ for tool in "${authoritylist[@]}"
   # enable the cis_settings registry, set private path, temporary path, then execute clean up routines
   drush -y --uri=$protocol://$site_domain en $cissettings
   drush -y --uri=$protocol://$site_domain vset file_private_path ${drupal_priv}/$tool/$tool
-  drush -y --uri=$protocol://$site_domain vset file_temporary_path ${drupal_tmp}/$tool/$tool
+  drush -y --uri=$protocol://$site_domain vset file_temporary_path ${drupal_tmp}
   # distro specific additional install routine
   drush -y --uri=$protocol://$site_domain cook elmsln_$dist
   # clean up tasks per distro here
