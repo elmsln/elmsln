5f7f691f2437c9757744cd146643f32fd8342386
Bryan Ollendyke <bto108@psu.edu>
2016-09-14 12:34:27 -0400

    hit legacy cases

--- scripts/upgrade/system/0.4.1.sh
+++ scripts/upgrade/system/0.4.1.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # Restart PHP so that opcache clears out the symlink for the old shared settings.php
 service php-fpm restart
+# restart apache
+sudo /etc/init.d/httpd restart
+service apache2 restart
+service httpd restart
