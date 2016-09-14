1581909802d779b8380505649dd6c1b13f1e21b5
Brad Fisher <bradallenfisher@gmail.com>
2016-09-14 12:12:17 -0400

    removes symlink and creates real file (#1244)
    * fixes #1230 and removes the core hack in common.inc
    
    * Update shared_settings.php

--- core/dslmcode/shared/drupal-7.x/settings/shared_settings.php
+++ core/dslmcode/shared/drupal-7.x/settings/shared_settings.php
@@ -1,1 +1,6 @@
-../../../../../config/shared/drupal-7.x/settings/shared_settings.php
+<?php 
+// fix for core change in 7.50
+$conf['x_frame_options'] = '';
+// allow user deployment settings to always take priority
+include_once "/var/www/elmsln/config/shared/drupal-7.x/settings/shared_settings.php";
+
