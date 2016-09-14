b0e0e16989fbec09e0edb9dabfcdfdcf266e2d59
Bryan Ollendyke <bto108@psu.edu>
2016-09-14 12:00:07 -0400

    Update shared_settings.php

--- core/dslmcode/shared/drupal-7.x/settings/shared_settings.php
+++ core/dslmcode/shared/drupal-7.x/settings/shared_settings.php
@@ -1,3 +1,6 @@
 <?php 
+// fix for core change in 7.50
+$conf['x_frame_options'] = '';
+// allow user deployment settings to always take priority
 include_once "/var/www/elmsln/config/shared/drupal-7.x/settings/shared_settings.php";
 
