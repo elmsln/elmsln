3cc74d14ba09b42f05153599e44dab8d71d95904
Bryan Ollendyke <bto108@psu.edu>
2016-09-14 12:36:07 -0400

    Update 0.4.1.sh

--- scripts/upgrade/system/0.4.1.sh
+++ scripts/upgrade/system/0.4.1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Restart PHP so that opcache clears out the symlink for the old shared settings.php
 service php-fpm restart
+service php5-fpm restart
 # restart apache
 /etc/init.d/httpd restart
 service apache2 restart
