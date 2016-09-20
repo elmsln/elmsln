c88b01f93438237026c33d35e16656e16e729814
Bryan btopro Ollendyke <bto108@psu.edu>
2016-09-20 10:45:50 -0400

    #1268

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/views/views-view-table.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/views/views-view-table.tpl.php
@@ -20,34 +20,32 @@
  */
 $classes = 's12 center-align col ' . $classes;
 ?>
-<div class="container">
-  <div class="row">
-    <table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?>>
-       <?php if (!empty($title) || !empty($caption)) : ?>
-         <caption><?php print $caption . $title; ?></caption>
-      <?php endif; ?>
-      <?php if (!empty($header)) : ?>
-        <thead>
-          <tr>
-            <?php foreach ($header as $field => $label): ?>
-              <th <?php if ($header_classes[$field]) { print 'class="'. $header_classes[$field] . '" '; } ?>>
-                <?php print $label; ?>
-              </th>
-            <?php endforeach; ?>
-          </tr>
-        </thead>
-      <?php endif; ?>
-      <tbody>
-        <?php foreach ($rows as $row_count => $row): ?>
-          <tr <?php if ($row_classes[$row_count]) { print 'class="' . implode(' ', $row_classes[$row_count]) .'"';  } ?>>
-            <?php foreach ($row as $field => $content): ?>
-              <td <?php if ($field_classes[$field][$row_count]) { print 'class="'. $field_classes[$field][$row_count] . '" '; } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
-                <?php print $content; ?>
-              </td>
-            <?php endforeach; ?>
-          </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
-  </div>
+<div class="row">
+  <table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?>>
+     <?php if (!empty($title) || !empty($caption)) : ?>
+       <caption><?php print $caption . $title; ?></caption>
+    <?php endif; ?>
+    <?php if (!empty($header)) : ?>
+      <thead>
+        <tr>
+          <?php foreach ($header as $field => $label): ?>
+            <th <?php if ($header_classes[$field]) { print 'class="'. $header_classes[$field] . '" '; } ?>>
+              <?php print $label; ?>
+            </th>
+          <?php endforeach; ?>
+        </tr>
+      </thead>
+    <?php endif; ?>
+    <tbody>
+      <?php foreach ($rows as $row_count => $row): ?>
+        <tr <?php if ($row_classes[$row_count]) { print 'class="' . implode(' ', $row_classes[$row_count]) .'"';  } ?>>
+          <?php foreach ($row as $field => $content): ?>
+            <td <?php if ($field_classes[$field][$row_count]) { print 'class="'. $field_classes[$field][$row_count] . '" '; } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+              <?php print $content; ?>
+            </td>
+          <?php endforeach; ?>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
 </div>
