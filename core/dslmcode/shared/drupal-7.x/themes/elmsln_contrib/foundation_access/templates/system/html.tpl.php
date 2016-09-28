1de666a3e0dcc8d7d3290135b8c9306eeac2c34a
Bryan btopro Ollendyke <bto108@psu.edu>
2016-09-28 17:01:19 -0400

    #1273 good bye foundation js

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/system/html.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/system/html.tpl.php
@@ -101,10 +101,5 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
-  <script>
-    (function ($, Drupal, window, document, undefined) {
-      $(document).foundation();
-    })(jQuery, Drupal, this, this.document);
-  </script>
   </body>
 </html>
