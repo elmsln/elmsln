1b1eb0cc9e867e593b5fa1da617f94909434066a
Bryan btopro Ollendyke <bto108@psu.edu>
2016-09-29 08:44:28 -0400

    minor cleanup of js post merge

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/dist/app.js
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/js/dist/app.js
@@ -135,18 +135,19 @@ var clipboardjs = require('./components/clipboardjs.js');
 module.exports = function() {
   (function ($) {
     'use strict';
+      // Replicate label if id exists with guid to match
+      if (typeof Drupal.clipboard !== typeof undefined) {
+        Drupal.clipboard.on('success', function (e) {
+          var alertStyle = $(e.trigger).data('clipboardAlert');
+          var alertText = $(e.trigger).data('clipboardAlertText');
+          var target = $(e.trigger).data('clipboardTarget');
 
-      Drupal.clipboard.on('success', function (e) {
-        var alertStyle = $(e.trigger).data('clipboardAlert');
-        var alertText = $(e.trigger).data('clipboardAlertText');
-        var target = $(e.trigger).data('clipboardTarget');
-
-        // Display as Materialize toast.
-        if (alertStyle === 'toast') {
-          Materialize.toast(alertText, 2000);
-        }
-      });
-
+          // Display as Materialize toast.
+          if (alertStyle === 'toast') {
+            Materialize.toast(alertText, 2000);
+          }
+        });
+      }
   })(jQuery);
 };
 
