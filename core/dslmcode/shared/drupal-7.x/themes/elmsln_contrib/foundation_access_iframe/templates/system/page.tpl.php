b4d9ca0c111d04f9b0f30b022ac0ad3ba02e83b5
Bryan btopro Ollendyke <bto108@psu.edu>
2016-10-06 11:47:03 -0400

    iframe height and markup clean up

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access_iframe/templates/system/page.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access_iframe/templates/system/page.tpl.php
@@ -7,53 +7,17 @@
  * @see template_preprocess_page()
  * @see template_process()
  */
-  if (!isset($cis_lmsless['active'])) {
-    $cis_lmsless['active']['title'] = '';
-  }
 ?>
-    <div id="etb-tool-nav" class="off-canvas-wrap" data-offcanvas>
-      <div class="inner-wrap">
-        <section class="main-section etb-book">
-          <?php if (!empty($messages)): ?>
-            <div class="region-messeges row">
-              <?php print $messages; ?>
-            </div>
-          <?php endif; ?>
-
-          <?php if (!empty($page['header'])): ?>
-            <div class="region-header row">
-              <?php print render($page['header']); ?>
-            </div>
-          <?php endif; ?>
-
-          <?php if (!empty($page['help'])): ?>
-            <div class="region-help row">
-              <?php print render($page['help']); ?>
-            </div>
-          <?php endif; ?>
-
-          <div class="row">
-            <div class="content-element-region l12 col">
-              <div class="row">
-                <div class="s12 m12 push-l1 l10 col" role="content">
-                  <?php if (!empty($page['highlighted'])): ?>
-                    <div class="highlight panel callout">
-                      <?php print render($page['highlighted']); ?>
-                    </div>
-                  <?php endif; ?>
-                  <a id="main-content"></a>
-                  <?php if ($title): ?>
-                    <?php print render($title_prefix); ?>
-                      <h1 id="page-title" class="title"><?php print $title; ?></h1>
-                    <?php print render($title_suffix); ?>
-                  <?php endif; ?>
-                  <?php print render($page['content']); ?>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
-      <a class="exit-off-canvas"></a>
-      </div>
-    </div>
+<main id="etb-tool-nav" class="off-canvas-wrap" data-offcanvas>
+  <div class="inner-wrap">
+    <section class="main-section etb-book">
+    <a id="main-content"></a>
+    <?php if ($title && arg(2) != 'edit'): ?>
+      <?php print render($title_prefix); ?>
+        <h1 id="page-title" class="title"><?php print $title; ?></h1>
+      <?php print render($title_suffix); ?>
+    <?php endif; ?>
+    <?php print render($page['content']); ?>
+    </section>
+  </div>
+</main>
