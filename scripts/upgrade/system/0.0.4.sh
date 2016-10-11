57e7158d4038e9113c8829e88be8dce5830686c9
Brad Fisher <bradallenfisher@gmail.com>
2016-10-11 12:25:10 -0400

    Changing the path to include new composer
    This works now

--- scripts/upgrade/system/0.0.4.sh
+++ scripts/upgrade/system/0.0.4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # ensure root / ulmus / ulmusdrush users have their drush pluggins updated
 /var/www/elmsln/scripts/utilities/refresh-drush-config.sh
-su -c 'HOME=/home/ulmus/ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/ulmus/.composer/vendor/bin /var/www/elmsln/scripts/utilities/refresh-drush-config.sh' ulmus
-su -c 'HOME=/home/ulmusdrush/ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/ulmusdrush/.composer/vendor/bin /var/www/elmsln/scripts/utilities/refresh-drush-config.sh' ulmusdrush
+su -c 'HOME=/home/ulmus/ PATH=/home/ulmus/.composer/vendor/bin:/home/ulmus/.config/composer/vendor/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/gam$
+su -c 'HOME=/home/ulmusdrush/ PATH=/home/ulmusdrush/.composer/vendor/bin:/home/ulmusdrush/.config/composer/vendor/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games$
