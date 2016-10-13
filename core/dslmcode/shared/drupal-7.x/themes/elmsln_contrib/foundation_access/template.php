2e089226243d77a3498b0abb5149cbd14f9e99a1
Brad Fisher <bradallenfisher@gmail.com>
2016-10-13 13:26:21 -0400

    Foundation Access front-end stack overhaul :)

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/template.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/template.php
@@ -53,7 +53,7 @@ function foundation_access_preprocess_html(&$variables) {
   $variables['theme_path'] = base_path() . drupal_get_path('theme', 'foundation_access');
 
   drupal_add_css($css, array('type' => 'inline', 'group' => CSS_THEME, 'weight' => 999));
-  drupal_add_css(drupal_get_path('theme', 'foundation_access') . '/bower_components/material-design-iconic-font/dist/css/material-design-iconic-font.min.css', array('group' => CSS_THEME, 'weight' => 1001));
+  drupal_add_css(drupal_get_path('theme', 'foundation_access') . '/legacy/bower_components/material-design-iconic-font/dist/css/material-design-iconic-font.min.css', array('group' => CSS_THEME, 'weight' => 1001));
   // google font / icons from google
   drupal_add_css('//fonts.googleapis.com/css?family=Material+Icons|Droid+Serif:400,700,400italic,700italic|Open+Sans:300,600,700)', array('type' => 'external', 'group' => CSS_THEME, 'weight' => 1000));
   // bring in materialize
@@ -1169,7 +1169,7 @@ function foundation_access_html_head_alter(&$head_elements) {
       '#attributes' => array(
         'type' => 'text/css',
         'rel' => 'stylesheet',
-        'href' => base_path() . drupal_get_path('theme', 'foundation_access') . '/css/app.css',
+        'href' => base_path() . drupal_get_path('theme', 'foundation_access') . '/legacy/css/app.css',
       ),
     );
   }
@@ -1245,7 +1245,7 @@ function foundation_access_form_page_node_form_alter(&$form, &$form_state) {
   // support for images in significance dropdown
   $form['field_instructional_significance']['und']['#materialize'] = array(
     'class' => 'left',
-    'icon_path' => drupal_get_path('theme', 'foundation_access') . '/icons/pedagogy/',
+    'icon_path' => drupal_get_path('theme', 'foundation_access') . '/legacy/icons/pedagogy/',
   );
 }
 
