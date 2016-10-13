94cd43b1f87b3ccf596aa8bf3ddbabf42b8fc786
Brad Fisher <bradallenfisher@gmail.com>
2016-10-13 11:49:20 -0400

    Removed old node modules.

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/node_modules/grunt-grunticon/site/src/js/head.js
+++
@@ -1,8 +0,0 @@
-// call grunticon
-var cb = function(){
-	grunticon.svgLoadedCallback();
-	if( grunticon.method ){
-		window.document.documentElement.className += " grunticon-" + grunticon.method;
-	}
-};
-grunticon(["grunticon/icons.data.svg.css", "grunticon/icons.data.png.css", "grunticon/icons.fallback.css"], cb );
