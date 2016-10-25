fcfb4ce89d259ba4ca503f6914eb31933add6ef5
Brad Fisher <bradallenfisher@gmail.com>
2016-10-25 13:58:37 -0400

    Update cleanup.sh

--- scripts/vagrant/cleanup.sh
+++ scripts/vagrant/cleanup.sh
@@ -21,12 +21,12 @@ sudo /etc/init.d/mysqld restart
 sudo chkconfig varnish off
 
 # Install front end stack in case users wish to develop with sass.
-#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-#source .bashrc
-#nvm install 6.7
-#cd /var/www/elmsln/core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/app
-#npm update --save-dev
-#npm install gulp --save-dev
-#npm install -g bower
-#npm install
-#bower install
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+source .bashrc
+nvm install 6.7
+cd /var/www/elmsln/core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/app
+npm update --save-dev
+npm install gulp --save-dev
+npm install -g bower
+npm install
+bower install
