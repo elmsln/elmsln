7a0e3c5c9456324b33eaa0a83e133cf55011e16c
Bryan btopro Ollendyke <bto108@psu.edu>
2016-10-27 15:44:56 -0400

    #1152

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/legacy/js/vibrate-enable.js
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/legacy/js/vibrate-enable.js
@@ -1,6 +1,6 @@
 (function ($) {
   $(document).ready(function(){
-    $('[data-activates],a[href="#"]').vibrate(50);
-    $('a[href!="#"]').vibrate(30);
+    $('[data-activates],a[href^="#"]').vibrate(50);
+    $('a:not([href^="#"])').vibrate(30);
   });
 })(jQuery);
