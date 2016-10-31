7910fa9af997a3cca1f22fdfb04212ba4b859ba1
Bryan btopro Ollendyke <bto108@psu.edu>
2016-10-31 10:26:48 -0400

    #1426

--- core/dslmcode/profiles/cpr-7.x-1.x/themes/cpr_foundation_access/template.php
+++ core/dslmcode/profiles/cpr-7.x-1.x/themes/cpr_foundation_access/template.php
@@ -4,5 +4,4 @@
  * User profile template preprocess.
  */
 function cpr_foundation_access_preprocess_user_profile(&$vars) {
-  $vars['user_profile']['field_photo']['#attributes']['class'] = array('circle');
 }
