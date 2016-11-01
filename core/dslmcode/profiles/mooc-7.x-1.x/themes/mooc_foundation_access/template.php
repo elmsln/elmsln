9dd1905e31eea35767b112f64ab6bb55ba9a3f8f
Bryan btopro Ollendyke <bto108@psu.edu>
2016-11-01 08:38:23 -0400

    #1431

--- core/dslmcode/profiles/mooc-7.x-1.x/themes/mooc_foundation_access/template.php
+++ core/dslmcode/profiles/mooc-7.x-1.x/themes/mooc_foundation_access/template.php
@@ -20,12 +20,6 @@ function mooc_foundation_access_preprocess_page(&$variables) {
       unset($variables['tabs']['#secondary']);
     }
   }
-  // support for add child page shortcut
-  $node = menu_get_object();
-  if ($node && user_access('access printer-friendly version')) {
-    $variables['tabs_extras'][200][] = '<div class="divider"></div>';
-    $variables['tabs_extras'][200][] = l(t('Print'), 'book/export/html/' . arg(1));
-  }
   $child_type = variable_get('book_child_type', 'book');
   if ($node && !empty($node->book) && (user_access('add content to books') || user_access('administer book outlines')) && node_access('create', $child_type) && $node->status == 1 && isset($node->book['depth']) && $node->book['depth'] < MENU_MAX_DEPTH) {
     $variables['tabs_extras'][200][] = '<div class="divider"></div>';
