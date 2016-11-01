d24c5ec401ea559dbe021207b8617c778de588a1
Bryan btopro Ollendyke <bto108@psu.edu>
2016-11-01 10:54:25 -0400

    minor notice #1431

--- core/dslmcode/profiles/mooc-7.x-1.x/themes/mooc_foundation_access/template.php
+++ core/dslmcode/profiles/mooc-7.x-1.x/themes/mooc_foundation_access/template.php
@@ -21,6 +21,7 @@ function mooc_foundation_access_preprocess_page(&$variables) {
     }
   }
   $child_type = variable_get('book_child_type', 'book');
+  $node = menu_get_object();
   if ($node && !empty($node->book) && (user_access('add content to books') || user_access('administer book outlines')) && node_access('create', $child_type) && $node->status == 1 && isset($node->book['depth']) && $node->book['depth'] < MENU_MAX_DEPTH) {
     $variables['tabs_extras'][200][] = '<div class="divider"></div>';
     $variables['tabs_extras'][200][] = '<span class="nolink cis-lmsless-text">' . t('Operations') . '</strong>';
