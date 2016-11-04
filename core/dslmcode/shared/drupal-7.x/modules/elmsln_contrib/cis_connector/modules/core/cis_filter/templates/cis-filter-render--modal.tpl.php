ff1fb9e56251569ae7f97efdfe3f210a9eadcebe
Bryan Ollendyke <btopro@psu.edu>
2016-11-04 14:13:36 -0400

    additional classes / cleanup off the edges

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/core/cis_filter/templates/cis-filter-render--modal.tpl.php
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/core/cis_filter/templates/cis-filter-render--modal.tpl.php
@@ -6,7 +6,7 @@
   //  $tool - the tool that this was requested from, useful for icon formation
 ?>
 <!-- Modal Trigger -->
-<a class="elmsln-modal-trigger waves-effect waves-<?php print $lmsless_classes[$tool]['color'];?> waves-light btn vibrate <?php print $lmsless_classes[$tool]['text'];?> <?php print $lmsless_classes[$tool]['color'];?> <?php print $lmsless_classes[$tool]['light'];?> <?php print $lmsless_classes[$tool]['outline'];?>" aria-controls="accessibility-drop" aria-expanded="false" href="#modal-<?php print $trigger;?>"><?php print $icon; ?> <?php print $label; ?></a>
+<a class="elmsln-modal-trigger elmsln-filter-tpl-modal waves-effect waves-<?php print $lmsless_classes[$tool]['color'];?> waves-light btn vibrate <?php print $lmsless_classes[$tool]['text'];?> <?php print $lmsless_classes[$tool]['color'];?> <?php print $lmsless_classes[$tool]['light'];?> <?php print $lmsless_classes[$tool]['outline'];?>" aria-controls="accessibility-drop" aria-expanded="false" href="#modal-<?php print $trigger;?>"><?php print $icon; ?> <?php print $label; ?></a>
 <!-- Modal Structure -->
 <div id="modal-<?php print $trigger;?>" class="elmsln-modal modal elmsln-modal-append-container" aria-hidden="true" role="dialog" tabindex="-1" aria-label="<?php print $title; ?>">
   <div class="center-align valign-wrapper elmsln-modal-title-wrapper <?php print $lmsless_classes[$tool]['color'] . ' ' . $lmsless_classes[$tool]['light'] . ' ' . $lmsless_classes[$tool]['color'];?>-border">
