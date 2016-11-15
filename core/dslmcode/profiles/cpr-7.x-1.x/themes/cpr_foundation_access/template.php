059c31a1a0fd3f1ee0abdfe06d8866c065bac631
Bryan Ollendyke <btopro@psu.edu>
2016-11-15 09:07:57 -0500

    fix user picture on services

--- core/dslmcode/profiles/cpr-7.x-1.x/themes/cpr_foundation_access/template.php
+++ core/dslmcode/profiles/cpr-7.x-1.x/themes/cpr_foundation_access/template.php
@@ -26,7 +26,8 @@ function cpr_foundation_access_preprocess_user_profile(&$vars) {
     $vars['photo'] = theme('image', $vars['field_user_photo'][0]);
   }
   else {
-    $vars['photo'] = '<img src="' . base_path() . drupal_get_path('theme', 'foundation_access') . '/img/user.png" class="ferpa-protect circle" />';
+    $photolink = url(drupal_get_path('theme', 'foundation_access') . '/img/user.png', array('absolute' => TRUE));
+    $vars['photo'] = '<img src="' . $photolink . '" class="ferpa-protect circle" />';
   }
   if (!empty($vars['field_user_banner'])) {
     $vars['banner'] = $vars['user_profile']['field_user_banner'];
