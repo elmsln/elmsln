225df3c20cea311eac63ef33a4d98c40b570d3bc
Bryan Ollendyke <btopro@psu.edu>
2016-11-16 09:05:36 -0500

    stop requesting once we get to step 6

--- domains/_installer/js/init.js
+++ domains/_installer/js/init.js
@@ -3,11 +3,13 @@
     $('.button-collapse').sideNav();
   }); // end of document ready
   setInterval(function(){
+  	if ($('.steptext').html() != 'Step 6 of 6: Install complete!')
 	    $.ajax({ url: "step.php", success: function(data){
 	        $('.steptext').html(data);
 	    }, dataType: "json"});
 	    $.ajax({ url: "log.php", success: function(data){
 	        $('.logarea').html(data);
 	    }, dataType: "json"});
+	}
 	}, 3000);
 })(jQuery); // end of jQuery name space
