e2e4c89fc037b0b0ba28c14d13995288c29df691
heyMP <mgp140@gmail.com>
2017-01-04 22:42:44 -0500

    loading imporvment

--- core/dslmcode/profiles/cle-7.x-2.x/modules/features/cle_app/app/src/app/projects/projects-list/projects-list.component.ts
+++ core/dslmcode/profiles/cle-7.x-2.x/modules/features/cle_app/app/src/app/projects/projects-list/projects-list.component.ts
@@ -25,7 +25,6 @@ export class ProjectsListComponent implements OnInit {
     private router: Router,
     private store: Store<{}>
   ) {
-    store.dispatch(loadAssignments());
   }
 
   ngOnInit() {
