b27b485a26e4111c81083908a6dfff8fb0cfe216
heyMP <mgp140@gmail.com>
2017-01-18 22:38:44 -0500

    added allow late submissions option

--- core/dslmcode/profiles/cle-7.x-2.x/modules/features/cle_app/app/src/app/assignment.ts
+++ core/dslmcode/profiles/cle-7.x-2.x/modules/features/cle_app/app/src/app/assignment.ts
@@ -7,14 +7,11 @@ export class Assignment {
   created:number = null;
   startDate:string = null;
   endDate:string = null;
+  allowLateSubmissions: boolean = false;
   project:number = null;
   body: string = null;
-  critiqueMethod:string = null;
+  critiqueMethod:string = 'none';
   critiquePrivacy:boolean = null;
   critiqueStyle:string = null;
   metadata:any = {};
-
-  constructor() {
-    this.critiqueMethod = 'none'
-  }
 }
