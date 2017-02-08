986d94c68ba490fabf38d3874a8988c82892545a
btopro <btopro@outlook.com>
2017-02-08 15:50:50 -0500

    #1855 web components working!!!!!!

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/webcomponents/modules/webcomponents_ds/templates/webcomponent.tpl.php
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/webcomponents/modules/webcomponents_ds/templates/webcomponent.tpl.php
@@ -8,9 +8,5 @@
  * - $tag: the custom web component "html" tag
  * - $properties: an array of attributes / values to fill in.
  */
-// just to be safe since drupal_attributes blows up if node system doesn't populate
-if (empty($properties)) {
-  $properties = array();
-}
 ?>
 <<?php print $tag;?><?php print drupal_attributes($properties);?>></<?php print $tag; ?>>
