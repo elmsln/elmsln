f9f46ac7a3154f7441db9e9532568ea3bd49bddb
heyMP <mgp140@gmail.com>
2017-02-09 13:19:40 -0500

    added assignment detail component

---
+++ core/dslmcode/profiles/cle-7.x-2.x/modules/features/cle_app/app/src/app/assignment/assignment-detail/assignment-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Assignment } from '../../assignment';
+
+@Component({
+  selector: 'app-assignment-detail',
+  templateUrl: './assignment-detail.component.html',
+  styleUrls: ['./assignment-detail.component.css']
+})
+export class AssignmentDetailComponent implements OnInit {
+  @Input() assignment:Assignment;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
