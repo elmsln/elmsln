042b4cb0ea556feb52faa1f2443b7621813f2211
btopro <btopro@outlook.com>
2017-02-14 08:08:36 -0600

    #1871

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/features/elmsln_core/js/elmsln-core-error-messages.js
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/features/elmsln_core/js/elmsln-core-error-messages.js
@@ -1,7 +1,6 @@
 (function ($) {
   // apply jwerty keys to listen for
   $(document).ready(function(){
-    Drupal.voicecommander.say('I\'m sorry Dave. I\'m afraid I can\'t do that.', 0.1, .6);
     setInterval(function(){ $('.error-page-hal9000').addClass('on'); }, 1000);
     setInterval(function(){ $('.error-page-hal9000').removeClass('on'); }, 2000);
   });
