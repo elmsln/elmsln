e1c50ffb0aefb24a06a4e90f187712e34aec780a
btopro <btopro@outlook.com>
2017-02-27 20:53:58 -0700

    edge case where backend calls won't go out the door if path cache included

--- core/dslmcode/cores/drupal-7/elmsln.php
+++ core/dslmcode/cores/drupal-7/elmsln.php
@@ -16,14 +16,12 @@ include_once DRUPAL_ROOT . '/../../elmsln_environment/elmsln_environment.php';
  */
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 require_once DRUPAL_ROOT . '/includes/common.inc';
-require_once DRUPAL_ROOT . '/' . variable_get('path_inc', 'includes/path.inc');
 require_once DRUPAL_ROOT . '/includes/module.inc';
 require_once DRUPAL_ROOT . '/includes/file.inc';
 require_once DRUPAL_ROOT . '/includes/unicode.inc';
-
 // Bootstrap Drupal to at least the database level so it can be accessed.
 drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
-
+require_once DRUPAL_ROOT . '/' . variable_get('path_inc', 'includes/path.inc');
 // Load the ELMSLN API module and execute request.
 drupal_load('module', 'elmsln_api');
 elmsln_api_execute_request();
