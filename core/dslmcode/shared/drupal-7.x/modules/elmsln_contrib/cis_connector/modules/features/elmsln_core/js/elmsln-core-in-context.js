feb2b0dfd3d419d4a6639695cc683cc9555a7887
btopro <btopro@outlook.com>
2017-05-03 13:57:23 -0400

    #1460

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/features/elmsln_core/js/elmsln-core-in-context.js
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/features/elmsln_core/js/elmsln-core-in-context.js
@@ -10,6 +10,15 @@
       	}
       });
     });
+    // initial setup after ready
+    Drupal.settings.elmslnCore.inContext = 'destination=elmsln/redirect/' + Drupal.settings.distro + '/' + Drupal.settings.elmslnCore.path + '&elmsln_course=' + Drupal.settings.elmslnCore.course + '&addHash=end';
+    $('.elmsln-add-menu-items li a').each(function() {
+      if ($(this).attr('href').indexOf('elmsln/redirect/') != -1) {
+        var url = $(this).attr('href').split('?');
+        $(this).attr('href', url[0] + '?' + Drupal.settings.elmslnCore.inContext);
+      }
+    });
+    // hover state
     $('.elmsln-core-in-context').hover(function(e){
       $(this).addClass('elmsln-core-in-context-hover ' + Drupal.settings.cis_lmsless[Drupal.settings.distro]['color'] + '-border');
     }, function(e) {
