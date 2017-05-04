6b7b01a8849659f73eaec41710cc2bda8457f82a
btopro <btopro@outlook.com>
2017-05-04 10:15:41 -0400

    home word removed from course outline cause @michael-collins always hated it

--- core/dslmcode/profiles/mooc-7.x-1.x/themes/mooc_foundation_access/templates/blocks/block--mooc-helper--mooc-helper-toc.tpl.php
+++ core/dslmcode/profiles/mooc-7.x-1.x/themes/mooc_foundation_access/templates/blocks/block--mooc-helper--mooc-helper-toc.tpl.php
@@ -1,6 +1,5 @@
 <section id="<?php print $block_html_id ?>-nav-modal" class="modal elmsln-modal elmsln-modal-hidden disable-scroll" tabindex="-1" aria-label="<?php print t('Table of contents'); ?>" aria-hidden="true" role="dialog">
   <div class="center-align valign-wrapper elmsln-modal-title-wrapper cis-lmsless-dark cis-lmsless-border">
-  <?php print l('<i class="material-icons black-text left">home</i><span class="hide-on-med-and-down">' . t('Home') . '</span>', '<front>', array('html' => TRUE, 'attributes' => array('data-voicecommand' => t('home'), 'class' => array('waves-effect waves-light btn-large white black-text mooc-home-button'))));?>
   <h2 class="flow-text valign elmsln-modal-title white-text"><?php print t('Table of contents'); ?></h2>
   <a href="#" class="close-reveal-modal white-text" aria-label="<?php print t('Close'); ?>" data-voicecommand="close (menu)" data-jwerty-key="Esc">&#215;</a>
   </div>
