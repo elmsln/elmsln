72e434f7c47ee8663f0d37c409e929bcdd00b1d4
btopro <btopro@outlook.com>
2017-05-31 00:53:57 -0400

    #2037 API updated to match

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/webcomponents/modules/webcomponents_app/webcomponents_app.api.php
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/webcomponents/modules/webcomponents_app/webcomponents_app.api.php
@@ -22,14 +22,14 @@ function hook_register_webcomponent_apps() {
       'path' => 'sites/all/libraries/webcomponents/polymer/apps/my-one-page/src/my-one-page-app/my-one-page-app.html',
       // a human readable title
       'title' => t('Open studio'),
-      // optional: module it comes from
-      'module' => 'my_custom_module',
       // optional: menu for making this visible in drupal's menu system
       'menu' => array(
         'type' => MENU_NORMAL_ITEM,
         'menu_name' => 'menu-navigation',
         'weight' => -10,
       ),
+      // optional: support for automatically generating a block that theme's the web component
+      'block' => TRUE,
       // optional: adding a data router for getting information back in
       // a consistent way. This isn't required but will be used almost
       // all of the time unless paths are hard wired into the app itself
@@ -40,6 +40,8 @@ function hook_register_webcomponent_apps() {
         // the name of the property the element uses to pull data
         'property' => 'source-path',
       ),
+      // optional: module it comes from
+      'module' => 'my_custom_module',
       // optional: completely optional but also is hooked in from where ever it comes from
       // this allows you to do any other kind of contextual operation you would need.
       // For example, elmsln uses the 'distro' context value to match against the currently
