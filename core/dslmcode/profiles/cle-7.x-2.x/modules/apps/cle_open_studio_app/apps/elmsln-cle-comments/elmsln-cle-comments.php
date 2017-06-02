6ac6650684431fffbfb0ccb6c472e8a431beba37
btopro <btopro@outlook.com>
2017-06-02 09:56:27 -0400

    CLE comments app start

---
+++ core/dslmcode/profiles/cle-7.x-2.x/modules/apps/cle_open_studio_app/apps/elmsln-cle-comments/elmsln-cle-comments.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Callback for getting comment data out of Drupal
+ */
+function _elmsln_cle_comments_data($machine_name, $app_route) {
+  $comments = array();
+  $status = 404;
+  $query = new EntityFieldQuery();
+  $query
+  ->entityCondition('entity_type', 'comment')
+  ->propertyCondition('status', 1)
+  ->propertyOrderBy('changed', 'DESC')
+  ->range(0, 10);
+  $result = $query->execute();
+  // flip the results if it found them
+  if (isset($result['comment'])) {
+	  foreach ($result['comment'] as $commentdata) {
+	    $comment = comment_load($commentdata->cid);
+	    $comments[$comment->cid] = array(
+	    	'title' => $comment->subject,
+	    	'body' => $comment->comment_body['und'][0]['safe_value'],
+	    	'view' => base_path() . '/comment/'. $comment->cid .'#comment-' . $comment->cid
+	    );
+	  }
+	  $status = 200;
+  }
+  return array(
+    'status' => $status,
+    'data' => $comments
+  );
+}
