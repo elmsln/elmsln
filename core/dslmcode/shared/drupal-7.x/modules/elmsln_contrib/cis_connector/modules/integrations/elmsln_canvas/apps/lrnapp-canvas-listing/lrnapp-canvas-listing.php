79463a524352217474f271e1fbb3e7b99a98462c
btopro <btopro@outlook.com>
2017-06-27 18:57:27 -0400

    #1663 CURL ops working w/ deep requests it appears

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/integrations/elmsln_canvas/apps/lrnapp-canvas-listing/lrnapp-canvas-listing.php
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/integrations/elmsln_canvas/apps/lrnapp-canvas-listing/lrnapp-canvas-listing.php
@@ -6,35 +6,45 @@
  * @return array               data to be json encoded for the front end
  */
 function _elmsln_canvas_course_list($machine_name, $app_route, $params, $args) {
-  $canvas = canvas_api('courses');
-  $courses = $canvas->getCourse(NULL);
-  dpm($courses);
-  $rosters = array();
-  foreach ($courses as $course) {
-    $canvas = canvas_api('sections');
-    $sections = $canvas->sectionsByCourseID('sis_course_id:' . $course['sis_course_id']);
-    dpm($sections);
-    $canvas = canvas_api('enrollment');
-    foreach ($sections as $section) {
-      $rosters[$course['sis_course_id']][] = $canvas->getEnrollment('sis_section_id:' . $section['sis_section_id']);
+  if ($params['return'] == 'courses') {
+    $canvas = canvas_api('courses');
+    $return = $canvas->getCourse(NULL);
+  }
+  else if ($params['return'] == 'sections') {
+    $canvas = canvas_api('courses');
+    $return = $canvas->getCourse(NULL);
+    foreach ($courses as $course) {
+      $canvas = canvas_api('sections');
+      $sections[] = $canvas->sectionsByCourseID('sis_course_id:' . $course['sis_course_id']);
     }
-    foreach ($rosters[$course['sis_course_id']] as $key1 => $enrollments) {
-      // drop things that don't have enrollments
-      if (empty($enrollments)) {
-        unset($rosters[$course['sis_course_id']][$key1]);
+    $return = $sections;
+  }
+  else if ($params['return'] == 'rosters') {
+    foreach ($courses as $course) {
+      $canvas = canvas_api('sections');
+      $sections = $canvas->sectionsByCourseID('sis_course_id:' . $course['sis_course_id']);
+      $canvas = canvas_api('enrollment');
+      foreach ($sections as $section) {
+        $rosters[$course['sis_course_id']][] = $canvas->getEnrollment('sis_section_id:' . $section['sis_section_id']);
       }
-      else {
-        // look through enrollments for people that are active
-        foreach ($enrollments as $key2 => $enrollment) {
-          // only keep people who are active
-          if ($enrollment['enrollment_state'] != 'active') {
-            unset($rosters[$course['sis_course_id']][$key1][$key2]);
+      foreach ($rosters[$course['sis_course_id']] as $key1 => $enrollments) {
+        // drop things that don't have enrollments
+        if (empty($enrollments)) {
+          unset($rosters[$course['sis_course_id']][$key1]);
+        }
+        else {
+          // look through enrollments for people that are active
+          foreach ($enrollments as $key2 => $enrollment) {
+            // only keep people who are active
+            if ($enrollment['enrollment_state'] != 'active') {
+              unset($rosters[$course['sis_course_id']][$key1][$key2]);
+            }
           }
         }
       }
     }
+    $return = $rosters;
   }
-  dpm($rosters);
   return array(
     'status' => 200,
     'data' => $return
