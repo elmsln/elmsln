ebd2a8f6bcfdcff54d90151329da5d4175a48e9d
btopro <btopro@outlook.com>
2017-06-28 17:01:08 -0400

    #1663 getting there

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/integrations/elmsln_canvas/apps/lrnapp-canvas-listing/lrnapp-canvas-listing.php
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/integrations/elmsln_canvas/apps/lrnapp-canvas-listing/lrnapp-canvas-listing.php
@@ -12,25 +12,50 @@ function _elmsln_canvas_course_list($machine_name, $app_route, $params, $args) {
   // Clicking to expand those can do a individual section query to pull up some more stats
   if ($params['return'] == 'courses') {
     $canvas = canvas_api('courses');
-    $return = $canvas->getCourse(NULL);
-  }
-  else if ($params['return'] == 'sections') {
-    $canvas = canvas_api('courses');
-    $courses = $canvas->getCourse(NULL);
-    $sections = array();
-    foreach ($courses as $course) {
-      $canvas = canvas_api('sections');
-      $sections[] = $canvas->sectionsByCourseID('sis_course_id:' . $course['sis_course_id']);
+    $canvas->params['include'] = array(
+      'sections',
+      'total_students',
+      'course_image',
+    );
+    $response = $canvas->getCourse(NULL);
+    $courses = array();
+    foreach ($response as $data) {
+      $course = array(
+        'name' => $data['name'],
+        'course_code' => $data['course_code'],
+        'sections' => count($data['sections']),
+        'student_count' => $data['total_students'],
+        'image' => $data['image_download_url'],
+        'sis_course_id' => $data['sis_course_id'],
+        'workflow_state' => $data['workflow_state'],
+      );
+      $courses[$data['sis_course_id']] = $course;
     }
-    $return = $sections;
+    // @todo query against current SIS mappings to put that in output
+    $return = $courses;
   }
-  else if ($params['return'] == 'rosters') {
+  else if ($params['return'] == 'users') {
     $canvas = canvas_api('courses');
-    $courses = $canvas->getCourse(NULL);
-    foreach ($courses as $course) {
-      $canvas = canvas_api('sections');
-      $sections = $canvas->sectionsByCourseID('sis_course_id:' . $course['sis_course_id']);
-      $canvas = canvas_api('enrollment');
+    $canvas->params = array(
+      'enrollment_state' => array('active'),
+      'include' => array(
+        'avatar_url',
+        'enrollments',
+      ),
+    );
+    $return = $canvas->usersByCourse('sis_course_id:' . $params['sis_course_id']);
+    /*$canvas = canvas_api('sections');
+    $canvas->params['include'] = array(
+      'students',
+      'avatar_url',
+      'enrollments',
+      'total_students',
+    );
+    $return = $canvas->sectionsByCourseID('sis_course_id:'. $params['sis_course_id']);*/
+
+    //}
+    //$return = $courses;
+      /*$canvas = canvas_api('enrollment');
       foreach ($sections as $section) {
         $rosters[$course['sis_course_id']][] = $canvas->getEnrollment('sis_section_id:' . $section['sis_section_id']);
       }
@@ -49,8 +74,7 @@ function _elmsln_canvas_course_list($machine_name, $app_route, $params, $args) {
           }
         }
       }
-    }
-    $return = $rosters;
+    }*/
   }
   return array(
     'status' => 200,
