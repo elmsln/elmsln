9393b4aa3b1ae8507d032a7d3ff1751c27a9cec9
btopro <btopro@outlook.com>
2017-06-29 14:04:58 -0400

    studio response data updated #1925

--- core/dslmcode/profiles/cle-7.x-2.x/modules/apps/cle_open_studio_app/apps/lrnapp-open-studio/lrnapp-open-studio.php
+++ core/dslmcode/profiles/cle-7.x-2.x/modules/apps/cle_open_studio_app/apps/lrnapp-open-studio/lrnapp-open-studio.php
@@ -30,32 +30,27 @@ function _cle_open_studio_app_data($machine_name, $app_route, $params, $args) {
       $return[$item->nid]->author = $item->name;
       $return[$item->nid]->body = strip_tags($item->field_submission_text['und'][0]['safe_value']);
       $return[$item->nid]->url = base_path() . $app_route . '/data?nid=' . $item->nid . '&token=' . drupal_get_token('webcomponentapp');
-      $return[$item->nid]->edit_url = base_path() . 'node/' . $item->nid . '/edit?destination=' . $app_route;
+      $return[$item->nid]->view_url = base_path() . 'node/' . $item->nid . '#comments';
       if (!empty($item->field_images)) {
         $images = array();
         foreach ($item->field_images['und'] as $image) {
-          $images[$image['fid']] = file_create_url($image['uri']);
+          $images[] = file_create_url($image['uri']);
         }
-        if (count($images) == 1) {
-          $return[$item->nid]->image = array_pop($images);
-        }
-        else if (count($images) > 1) {
-          $return[$item->nid]->images = $images;
-        }
+        $return[$item->nid]->images = $images;
       }
       if (!empty($item->field_files)) {
         foreach ($item->field_files['und'] as $file) {
-          $return[$item->nid]->file = file_create_url($file['uri']);
+          $return[$item->nid]->files[] = file_create_url($file['uri']);
         }
       }
       if (!empty($item->field_video)) {
         foreach ($item->field_video['und'] as $video) {
-          $return[$item->nid]->video = $video['video_url'];
+          $return[$item->nid]->videos[] = $video['video_url'];
         }
       }
       if (!empty($item->field_links)) {
         foreach ($item->field_links['und'] as $link) {
-          $return[$item->nid]->link = $link['url'];
+          $return[$item->nid]->links[] = $link['url'];
         }
       }
     }
