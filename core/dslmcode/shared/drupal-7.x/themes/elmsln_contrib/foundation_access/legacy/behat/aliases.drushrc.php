9b69e19a8fc879b5773325c4998c41be3a0ddcd3
btopro <btopro@outlook.com>
2017-07-10 14:42:50 -0400

    removed more legacy files from front end stack

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/legacy/behat/aliases.drushrc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$aliases['negawatt'] = array(
-  'uri' => 'http://127.0.0.1:8080',
-  'root' => '/home/travis/build/drupalprojects/foundation_access/www',
-  'db-url' => 'mysql://root:@127.0.0.1/drupal',
-);
