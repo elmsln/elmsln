cdfe2b7fafa94c7941ec6caa28c13f95cca6bb05
btopro <btopro@outlook.com>
2017-08-07 15:41:54 -0400

    #2014

--- core/dslmcode/profiles/cle-7.x-2.x/modules/apps/cle_open_studio_app/apps/lrnapp-studio-kanban/lrnapp-studio-kanban.php
+++ core/dslmcode/profiles/cle-7.x-2.x/modules/apps/cle_open_studio_app/apps/lrnapp-studio-kanban/lrnapp-studio-kanban.php
@@ -6,6 +6,32 @@
  */
 function _cle_studio_kanban_project_data($machine_name, $app_route, $params, $args) {
   $return = array();
+  $status = 200;
+  // see if we're doing updates from the app
+  if (isset($params['submissionid'])) {
+    // load the submission node, typecasting
+    $submission = node_load($params['submissionid']);
+    // ensure they have access to this
+    if (entity_access('update', 'node', $submission)) {
+      // simple toggle of true / false on submission state
+      if ($params['status'] == 'true') {
+        $submission->field_submission_state[LANGUAGE_NONE][0]['value'] = 'submission_ready';
+      }
+      else {
+        $submission->field_submission_state[LANGUAGE_NONE][0]['value'] = 'submission_in_progress';
+      }
+      // save the node
+      node_save($submission);
+      // load the assignment this was related to
+      $assignment = node_load($submission->field_assignment[LANGUAGE_NONE][0]['target_id']);
+      // rebuild the part of the metadata array for the front-end and ship it off
+      $return = _cle_submission_submission_status($assignment);
+    }
+    else {
+      $status = 403;
+    }
+  }
+  else {
     // find all the projects for the current section
     $section_id = _cis_connector_section_context();
     $section = _cis_section_load_section_by_id($section_id);
@@ -39,33 +65,14 @@ function _cle_studio_kanban_project_data($machine_name, $app_route, $params, $ar
         $tmp = array();
         $assignment = node_load($step['target_id']);
         if (isset($assignment->nid)) {
-          $metadata = array(
-            'canCritique' => 0,
-            'canUpdate' => 0,
-            'canDelete' => 0,
-            'critiqueLink' => '',
-            'deleteLink' => base_path() . 'node/' . $assignment->nid . '/delete?destination=' . $app_route,
-            'updateLink' => base_path() . 'node/' . $assignment->nid . '/edit?destination=' . $app_route,
-          );
-          // see the operations they can perform here
-          if (entity_access('update', 'node', $assignment)) {
-            $metadata['canUpdate'] = 1;
-          }
-          if (entity_access('delete', 'node', $assignment)) {
-            $metadata['canDelete'] = 1;
-          }
-          $tmp['title'] = $assignment->title;
-          $tmp['icon'] = 'assignment';
-          $tmp['id'] = $assignment->nid;
-          $tmp['metadata'] = $metadata;
-          $tmp['body'] = $assignment->field_assignment_description['und'][0]['safe_value'];
-          $return['project-' . $project->nid]->assignments['assignment-' . $step['target_id']] = $tmp;
+          $return['project-' . $project->nid]->assignments['assignment-' . $assignment->nid] = _cle_assignment_v1_assignment_output($assignment, $app_route);
         }
       }
       $return['project-' . $project->nid]->body = $project->field_project_description['und'][0]['safe_value'];
+    }
   }
   return array(
-    'status' => 200,
+    'status' => $status,
     'data' => $return
   );
 }
