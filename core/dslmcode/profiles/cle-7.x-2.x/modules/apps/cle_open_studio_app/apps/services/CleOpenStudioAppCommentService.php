b0ede2d7380ee474e0ea7baec6f28d147ca1e276
btopro <btopro@outlook.com>
2017-08-08 11:44:27 -0400

    more kanban clean up and getting comments ready

--- core/dslmcode/profiles/cle-7.x-2.x/modules/apps/cle_open_studio_app/apps/services/CleOpenStudioAppCommentService.php
+++ core/dslmcode/profiles/cle-7.x-2.x/modules/apps/cle_open_studio_app/apps/services/CleOpenStudioAppCommentService.php
@@ -1,8 +1,34 @@
 <?php
 
-
 class CleOpenStudioAppCommentService {
-
+  /**
+   * Create Stub Comment based on assignment
+   */
+  public function createStubComment($comment_id) {
+    global $user;
+    $comment = new stdClass();
+    $comment->subject = t('Feedback from @name', array('@name' => $user->name));
+    $comment->type = 'comment';
+    $comment->uid = $user->uid;
+    $comment->name = $user->name;
+    $comment->status = 1;
+    $comment->language = LANGUAGE_NONE;
+    $comment->pid = $comment_id;
+     array(
+      'name' => '',
+      'mail' => '',
+      'homepage' => '',
+      'subject' => '',
+      'comment' => '',
+    );
+    comment_save($comment);
+    if (isset($comment->cid)) {
+      return $this->encodeSubmission($comment);
+    }
+    else {
+      return FALSE;
+    }
+  }
   /**
    * Get a list of comments
    * This will take into concideration what section the user is in and what section
@@ -39,42 +65,42 @@ class CleOpenStudioAppCommentService {
     return $a->thread - $b->thread;
   }
 
-  // /**
-  //  * Get a single comments
-  //  * This will take into concideration what section the user is in and what section
-  //  * they have access to.
-  //  *
-  //  * @param string $id
-  //  *    Nid of the comments
-  //  *
-  //  * @return object
-  //  */
-  // public function getComment($id) {
-  //   $item = array();
-  //   $section_id = _cis_connector_section_context();
-  //   $section = _cis_section_load_section_by_id($section_id);
-  //   $field_conditions = array(
-  //     'og_group_ref' => array('target_id', $section, '='),
-  //   );
-  //   $property_conditions = array('status' => array(NODE_PUBLISHED, '='));
-  //   if (isset($id)) {
-  //     $property_conditions['nid'] = array($id, '=');
-  //   }
-  //   $orderby = array();
-  //   $items = _cis_connector_assemble_entity_list('node', 'cle_comments', 'nid', '_entity', $field_conditions, $property_conditions, $orderby);
-  //   /**
-  //    * @todo add better checks to return status codes based on if none were found or if more than
-  //    *       one was found.
-  //    */
-  //   if (count($items) == 1) {
-  //     $item = $this->encodeComment(array_shift($items));
-  //   }
-  //   return $item;
-  // }
+  /**
+   * Get a single comments
+   * This will take into concideration what section the user is in and what section
+   * they have access to.
+   *
+   * @param string $id
+   *    Nid of the comments
+   *
+   * @return object
+   */
+  public function getComment($id) {
+    $item = array();
+    $section_id = _cis_connector_section_context();
+    $section = _cis_section_load_section_by_id($section_id);
+    $field_conditions = array(
+      'og_group_ref' => array('target_id', $section, '='),
+    );
+    $property_conditions = array('status' => array(NODE_PUBLISHED, '='));
+    if (isset($id)) {
+      $property_conditions['cid'] = array($id, '=');
+    }
+    $orderby = array();
+    $items = _cis_connector_assemble_entity_list('node', 'cle_comments', 'nid', '_entity', $field_conditions, $property_conditions, $orderby);
+    /**
+     * @todo add better checks to return status codes based on if none were found or if more than
+     *       one was found.
+     */
+    if (count($items) == 1) {
+      $item = $this->encodeComment(array_shift($items));
+    }
+    return $item;
+  }
 
   /**
    * Prepare a list of comments to be outputed in json
-   * 
+   *
    * @param array $comments
    *  An array of comments node objects
    *
@@ -94,8 +120,8 @@ class CleOpenStudioAppCommentService {
 
   /**
    * Prepare a single comments to be outputed in json
-   * 
-   * @param object $comment 
+   *
+   * @param object $comment
    *  A comments node object
    *
    * @return Object
@@ -112,21 +138,27 @@ class CleOpenStudioAppCommentService {
       $encoded_comments->attributes->thread = $comment->thread;
       $tmp = explode(".", $comment->thread);
       $encoded_comments->attributes->threadDepth = count($tmp);
-      $encoded_comments->attributes->parent_comment = $comment->pid;
-      // Meta Info
-      $encoded_comments->meta = new stdClass();
-      $encoded_comments->meta->created = Date('c', $comment->created);
-      $encoded_comments->meta->changed = Date('c', $comment->changed);
+      $encoded_comments->attributes->parentComment = $comment->pid;
+      $encoded_comments->attributes->created = Date('c', $comment->created);
+      $encoded_comments->attributes->changed = Date('c', $comment->changed);
+      // Metadata info
+      $encoded_comments->metadata = new stdClass();
+      $encoded_comments->metadata->editing = false;
+      $encoded_comments->metadata->disabled = false;
       // Relationships
       $encoded_comments->relationships = new stdClass();
-      // author
+      // Author
       $encoded_comments->relationships->author = new stdClass();
       $encoded_comments->relationships->author->data = new stdClass();
       $encoded_comments->relationships->author->data->type = 'user';
       $encoded_comments->relationships->author->data->id = $comment->uid;
       $encoded_comments->relationships->author->data->name = $comment->name;
       // Actions
-      $encoded_comments->actions = null;
+      $encoded_comments->actions = new stdClass();
+      $encoded_comments->actions->reply = entity_access('view', 'comment', $comment);
+      $encoded_comments->actions->edit = entity_access('update', 'comment', $comment);
+      $encoded_comments->actions->delete = entity_access('delete', 'comment', $comment);
+
       drupal_alter('cle_open_studio_app_encode_comments', $encoded_comments);
       return $encoded_comments;
     }
