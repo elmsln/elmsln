2aa97d5838ae0f902a0e676681c0a1f91f390b7e
btopro <btopro@outlook.com>
2017-08-11 11:30:39 -0400

    #2014 projects can be displayed if author / project selected

--- core/dslmcode/profiles/cle-7.x-2.x/modules/apps/cle_open_studio_app/apps/lrnapp-open-studio/lrnapp-open-studio.php
+++ core/dslmcode/profiles/cle-7.x-2.x/modules/apps/cle_open_studio_app/apps/lrnapp-open-studio/lrnapp-open-studio.php
@@ -5,6 +5,7 @@
  */
 define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__.'/services/CleOpenStudioAppSubmissionService.php');
+require_once(__ROOT__.'/services/CleOpenStudioAppProjectService.php');
 require_once(__ROOT__.'/services/CleOpenStudioAppFileService.php');
 require_once(__ROOT__.'/services/CleOpenStudioAppCommentService.php');
 
@@ -14,36 +15,22 @@ require_once(__ROOT__.'/services/CleOpenStudioAppCommentService.php');
  * @return array               data to be json encoded for the front end
  */
 function _cle_open_studio_app_data($machine_name, $app_route, $params, $args) {
+  $status = 200;
   $data = array();
-  // @todo need a better render method then this as this is lazy for now
-  if (isset($params['nid']) && is_numeric($params['nid'])) {
-    $node = node_load($params['nid']);
-    $node_view = node_view($node);
-    $rendered_node = drupal_render($node_view);
-    $data = $rendered_node;
-  }
-  else {
-    // @todo need to pull just the most recent submissions, 1 per project
-    // which might be too complex of logic for this efq to express
-    // get all submissions
-    // unique per project
-    // sort by most recent
-    // ... ugh... this is more complex then this
-    // pull together all the submissions they should be seeing
-    $options = new stdClass();
-    // only show things marked ready for feedback
-    $options->state = array('submission_ready', '=');
-    // @todo we need to pull only what we're asked for as a range
-    // this way we don't destroy the UI in huge classes
-    // invoke our submission service to get submissions
-    $service = new CleOpenStudioAppSubmissionService();
-    $data = $service->getSubmissions($options);
-    if (!empty($data)) {
-      $status = 200;
-    }
-  }
+  // pull together all the submissions they should be seeing
+  $options = new stdClass();
+  // only show things marked ready for feedback
+  $options->state = array('submission_ready', '=');
+  // @todo we need to pull only what we're asked for as a range
+  // this way we don't destroy the UI in huge classes
+  // invoke our submission service to get submissions
+  $service = new CleOpenStudioAppSubmissionService();
+  $data['submissions'] = $service->getSubmissions($options);
+  // snag projects
+  $service = new CleOpenStudioAppProjectService();
+  $data['projects'] = $service->getProjects();
   return array(
-    'status' => 200,
+    'status' => $status,
     'data' => $data
   );
 }
