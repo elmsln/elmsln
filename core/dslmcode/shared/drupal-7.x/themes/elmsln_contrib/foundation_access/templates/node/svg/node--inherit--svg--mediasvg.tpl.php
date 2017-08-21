52d3d1ef2c28ff913e39f241f7b720937fa313f4
btopro <btopro@outlook.com>
2017-08-21 08:45:48 -0400

    accessibility iconography on fallback

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/node/svg/node--inherit--svg--mediasvg.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/node/svg/node--inherit--svg--mediasvg.tpl.php
@@ -3,7 +3,7 @@
     <div class="element-invisible">
       <?php print t('The following media is highly interactive in nature and may not be immediately accessible to you. If you would like a more accessible version of this media please access the next link.');?>
     </div>
-    <?php print l(t('Enable more accessible form of this media.'), 'entity_iframe/node/' . $nid . '/accessible_fallback');?>
+    <?php print l(t('Enable more accessible form of this media.'), 'entity_iframe/node/' . $nid . '/accessible_fallback', array('attributes' => array('icon' => 'accessibility'))); ?>
   <?php endif; ?>
   <?php if (isset($content['field_figurelabel_ref'])): ?>
     <?php print render($content['field_figurelabel_ref'][0]); ?>
