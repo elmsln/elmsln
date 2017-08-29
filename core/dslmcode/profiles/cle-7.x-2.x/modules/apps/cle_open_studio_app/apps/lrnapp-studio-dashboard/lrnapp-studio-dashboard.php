ebe60b05eaa00d4522c98f7be3b67906502b4dc4
btopro <btopro@outlook.com>
2017-08-29 00:57:12 -0400

    only show submission ready status on dashboard

--- core/dslmcode/profiles/cle-7.x-2.x/modules/apps/cle_open_studio_app/apps/lrnapp-studio-dashboard/lrnapp-studio-dashboard.php
+++ core/dslmcode/profiles/cle-7.x-2.x/modules/apps/cle_open_studio_app/apps/lrnapp-studio-dashboard/lrnapp-studio-dashboard.php
@@ -72,7 +72,7 @@ function _lrnapp_studio_dashboard_recent_submissions($machine_name, $app_route, 
   $data = array();
   $status = 200;
   $options = new stdClass();
-  $options->state = array('submission_ready', '=');
+  $options->filter['state'] = array('submission_ready', '=');
   $options->order = array('property' => array(array('changed', 'DESC')));
   $options->limit = array(0, 3);
   // invoke our submission service to get submissions
@@ -91,7 +91,7 @@ function _lrnapp_studio_dashboard_need_feedback($machine_name, $app_route, $para
   $data = array();
   $status = 200;
   $options = new stdClass();
-  $options->state = array('submission_ready', '=');
+  $options->filter['state'] = array('submission_ready', '=');
   $options->order = array('property' => array(array('changed', 'DESC')));
   // don't show student's own submissions in needing feedback column
   $options->filter = array('author' => array($GLOBALS['user']->uid, '<>'));
