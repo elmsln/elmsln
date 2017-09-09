5d941c43fac24aefca3253134ed0fc9e50cd1b04
btopro <btopro@outlook.com>
2017-09-09 13:24:26 -0400

    #2236 download link and better initial sizing on images

--- core/dslmcode/shared/drupal-7.x/libraries/webcomponents/polymer/LRNWebComponents/service-worker.js
+++ core/dslmcode/shared/drupal-7.x/libraries/webcomponents/polymer/LRNWebComponents/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","3eef2c965d724110f0d4db822a33d045"]];
+var precacheConfig = [["index.html","a5d531bb796e8a149343034cdbd7b675"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
