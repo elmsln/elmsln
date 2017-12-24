d7b3f315cb3f2faab24b484a0fcbe23794480b08
Michael Potter <mgp140@gmail.com>
2017-12-23 21:24:45 -0500

    added elmsmedia-dashboard app

---
+++ core/webcomponents/apps/elmsmedia-dashboard/elmsmedia-dashboard.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Require the submission service.
+ */
+define('__ROOT__', dirname(dirname(dirname(__FILE__))));
+require_once(__ROOT__.'/services/ELMSMediaService.php');
+
+/**
+ * Callback for apps/elmsmedia-dashboard/data.
+ */
+function _elmsmedia_dashboard_data($machine_name, $app_route, $params, $args) {
+  $media_service = new ELMSMediaService();
+  $return = $media_service->getMedia();
+  return array(
+    'status' => 200,
+    'data' => $return,
+  );
+}
