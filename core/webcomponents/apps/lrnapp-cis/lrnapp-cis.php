7b90bc4ddc27686d171dfddc11700f33238fc6fa
btopro <btopro@outlook.com>
2018-01-18 12:37:54 -0500

    #1464

--- core/webcomponents/apps/lrnapp-cis/lrnapp-cis.php
+++ core/webcomponents/apps/lrnapp-cis/lrnapp-cis.php
@@ -5,7 +5,7 @@
 function _lrnapp_cis_app_data($machine_name, $app_route, $params, $args) {
   $return = array();
   $query = new EntityFieldQuery();
-  $query->entityCondition('entity_type', 'node')
+  $result = $query->entityCondition('entity_type', 'node')
   ->entityCondition('bundle', 'course')
   ->propertyCondition('status', NODE_PUBLISHED)
   ->propertyOrderBy('title', 'DESC')
@@ -14,6 +14,7 @@ function _lrnapp_cis_app_data($machine_name, $app_route, $params, $args) {
   if (isset($result['node'])) {
     foreach ($result['node'] as $item) {
       $node = node_load($item->nid);
+      $return[$node->nid] = new stdClass();
       $return[$node->nid]->id = $node->nid;
       $return[$node->nid]->data = new stdClass();
       $return[$node->nid]->data->machine_name = $node->field_machine_name['und'][0]['value'];
@@ -21,29 +22,29 @@ function _lrnapp_cis_app_data($machine_name, $app_route, $params, $args) {
       $return[$node->nid]->data->title = $node->field_course_title['und'][0]['value'];
       $return[$node->nid]->data->image = file_create_url($node->field_banner['und'][0]['uri']);
       $return[$node->nid]->data->color = 'red';
-      $return[$node->nid]->data->uri = 'node/' . $node->nid;
+      $return[$node->nid]->data->uri = base_path() . 'node/' . $node->nid;
       // build out the relationships
       $return[$node->nid]->relationships = new stdClass();
-      $return[$node->nid]->program = new stdClass();
-      $return[$node->nid]->program->data = new stdClass();
-      $return[$node->nid]->academic = new stdClass();
-      $return[$node->nid]->academic->data = new stdClass();
+      $return[$node->nid]->relationships->program = new stdClass();
+      $return[$node->nid]->relationships->program->data = new stdClass();
+      $return[$node->nid]->relationships->academic = new stdClass();
+      $return[$node->nid]->relationships->academic->data = new stdClass();
       // test and make sure program home exists
       if (isset($node->field_program_classification['und'][0]['target_id'])) {
         $program = node_load($node->field_program_classification['und'][0]['target_id']);
-        $return[$node->nid]->program->id = $program->nid;
-        $return[$node->nid]->program->data->name = $program->title;
+        $return[$node->nid]->relationships->program->id = $program->nid;
+        $return[$node->nid]->relationships->program->data->name = $program->title;
       }
       // test and make sure academic home exists
       if (isset($node->field_academic_home['und'][0]['target_id'])) {
         $academic = node_load($node->field_academic_home['und'][0]['target_id']);
-        $return[$node->nid]->academic->id = $academic->nid;
-        $return[$node->nid]->academic->data->name = $academic->title;
+        $return[$node->nid]->relationships->academic->id = $academic->nid;
+        $return[$node->nid]->relationships->academic->data->name = $academic->title;
       }
     }
   }
   return array(
     'status' => 200,
-    'data' => $return
+    'data' => array('courses' => $return),
   );
 }
