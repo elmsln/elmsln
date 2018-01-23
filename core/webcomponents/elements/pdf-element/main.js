96e8cdc455b37fb59627148d3297973331f363e3
btopro <btopro@outlook.com>
2018-01-23 11:11:05 -0500

    #2218 PDF viewer working! Great work @proconnor15

--- core/webcomponents/elements/pdf-element/main.js
+++ core/webcomponents/elements/pdf-element/main.js
@@ -75,6 +75,7 @@
   };
 
   Reader.prototype.queueRenderPage = function(num) {
+    this.pdfExists = true;
     if (this.pageRendering) {
       this.pageNumPending = num;
     } else {
@@ -95,6 +96,8 @@
       self.totalPagesNum = self.PDF.numPages;
       self.currentZoomVal = self.fitZoomVal = self.widthZoomVal = 0;
       self.createDownloadLink();
+    }).catch((ex) => {
+      this.pdfExists = false;
     });
   };
 
@@ -125,7 +128,7 @@
     var pdfObj = null;
 
     //If there is already a sidebar loaded
-    if(self.container.innerHTML != " "){
+    if(self.container.innerHTML.length != 0){
       if(currentThis.changedSideBar){   //Check if the pdf has been changed
         self.container.innerHTML = "";
         // Asynchronous download PDF
@@ -148,27 +151,27 @@
     }
 
 
-    //Else sidebar has not been loaded for first time
-    else{
-      self.container.innerHTML = "";
-      // Asynchronous download PDF
-      PDFJS.getDocument(this.SRC).then(function(pdf) {
+      //Else sidebar has not been loaded for first time
+      else{
+        self.container.innerHTML = "";
+        // Asynchronous download PDF
+        PDFJS.getDocument(this.SRC).then(function(pdf) {
 
-        //Set PDFJS global object (so we can easily access in our page functions
-        pdfObj = pdf;
+          //Set PDFJS global object (so we can easily access in our page functions
+          pdfObj = pdf;
 
-        //How many pages it has
-        numPages = pdfObj.numPages;
-        //numPages = 5;
+          //How many pages it has
+          numPages = pdfObj.numPages;
 
-        // Get div#container and cache it for later use
-        var container = self.container;
-        var counter = 0;
+          // Get div#container and cache it for later use
+          var container = self.container;
+          var counter = 0;
 
-        pdf.getPage( 1 ).then( handlePages );
-      });
-      self.setViewportPos(true);
-    }
+          pdf.getPage( 1 ).then( handlePages );
+        });
+        self.setViewportPos(true);
+      }
+    
       function handlePages(page){
         var scale = 0.14;
         var scaleWidth = 0;
@@ -179,7 +182,7 @@
         else{
           scaleWidth = self.WIDTH;
         }
-        scale = scaleWidth * .0004023;
+        scale = scaleWidth * .00035;
         var viewport = page.getViewport(scale);
         var div = document.createElement("div");
 
