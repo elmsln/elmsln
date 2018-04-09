2899295861b58925dce81d2761b0b3fd72a35694
btopro <btopro@outlook.com>
2018-04-09 14:40:31 -0400

    #264 slot management drastically improved + UX feedback + build updates

--- core/webcomponents/elements/_build/service-worker.js
+++ core/webcomponents/elements/_build/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","66c98c6e0ee516f5ca107f12ec8285eb"]];
+var precacheConfig = [["index.html","7ec0c71c1522d812d30bc86f8bddb4b0"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
