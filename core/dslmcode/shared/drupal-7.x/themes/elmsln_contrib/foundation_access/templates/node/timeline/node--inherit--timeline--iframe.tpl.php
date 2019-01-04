451b0f7eef2a1fc3ba3734d6679bcfb52197d6a9
btopro <btopro@outlook.com>
2019-01-04 12:45:49 -0500

    #2579 enable modules and supply template

---
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/node/timeline/node--inherit--timeline--iframe.tpl.php
@@ -0,0 +1,1 @@
+<iframe id="node_<?php print $nid;?>" class="entity_iframe entity_iframe_node entity_iframe_tool_timeline elmsmedia_timeline_content" src="<?php print $iframe_path; ?>" width="100%" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
