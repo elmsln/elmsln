44dcbd0b2286a9fd226068a5246e90142a87ec1a
btopro <btopro@outlook.com>
2019-01-08 12:02:51 -0500

    #2581

---
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/node/assessment/node--inherit--quiz--iframe.tpl.php
@@ -0,0 +1,1 @@
+<iframe id="node_<?php print $nid;?>" class="entity_iframe entity_iframe_node entity_iframe_tool_quiz elmsmedia_quiz_content" src="<?php print $iframe_path; ?>" width="100%" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
