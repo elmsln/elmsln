814fdd5bb85309c5478d7e248568d9cd9e934485
btopro <btopro@outlook.com>
2019-01-14 08:05:26 -0500

    remove gravcms and haxcms from core #2584

--- core/dslmcode/cores/haxcms-1/_config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// a private key to do an additional hash via
-$HAXCMS->privateKey = 'hax-the-web-key';
-// super admin account
-$HAXCMS->superUser->name = 'jeff';
-// super admin password, you must set this in order for HAX to work
-$HAXCMS->superUser->password = 'jimmerson';
-// set basePath to be the haxCMS location we've got this placed at
-$HAXCMS->basePath = '/haxcms/';
