d153ed64dfb39ed8bb34578fcefa5ac11b08398e
btopro <btopro@outlook.com>
2019-02-21 13:57:21 -0500

    missed one template

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/node/external_video/node--inherit--external-video.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/node/external_video/node--inherit--external-video.tpl.php
@@ -2,6 +2,14 @@
 /**
  * For Media Poster, we will add put the poster within the video wrapper to accomidate intrinsic ratio.
  */
+  $track = '';
+  if (isset($node->field_caption['und'][0]['uri'])) {
+    $track = '<track
+  src="' . file_create_url($node->field_caption['und'][0]['uri']) . '"
+  kind="subtitles"
+  label="English"
+  slot="track">';
+  }
 ?>
 <video-player
  id="node-<?php print $node->nid; ?>"
@@ -9,4 +17,6 @@
  class="iframe <?php print $classes; ?>"
  accent-color="red"
  sticky-corner="none"
- <?php print $attributes; ?>></video-player>
+ <?php print $attributes; ?>>
+<?php print $track;?>
+</video-player>
