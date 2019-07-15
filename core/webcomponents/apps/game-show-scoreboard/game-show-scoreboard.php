2c629beb0313c5694b8c333304052d163503e5da
btopro <btopro@outlook.com>
2019-07-15 17:58:04 -0400

    more clean  up in game show

--- core/webcomponents/apps/game-show-scoreboard/game-show-scoreboard.php
+++ core/webcomponents/apps/game-show-scoreboard/game-show-scoreboard.php
@@ -5,16 +5,15 @@
  */
 function _game_show_scoreboard_options($machine_name, $app_route, $params, $args) {
   // load a list of sections for the active user
-  $sections = array();
   $field_conditions = array();
   $property_conditions = array('status' => array(NODE_PUBLISHED, '='));
-  if (isset($filter['section'])) {
-    $property_conditions['nid'] = array($filter['section'], '=');
-  }
   $orderby = array('field_cis_active' => array('value', 'DESC'));
-  $sections = _cis_connector_assemble_entity_list('node', 'section', 'nid', '_entity', $field_conditions, $property_conditions, $orderby);
-  
-
+  $sections = array_merge(array("" => t("All")), _cis_connector_assemble_entity_list('node', 'section', 'field_section_id', 'title', $field_conditions, $property_conditions, $orderby));
+  $jeopardys = array(
+    "" => t("All"),
+    'Number One' => 'Number One',
+    "Number two" => "Number two"
+  );
   return array(
     'status' => 200,
     'data' => array(
@@ -31,32 +30,34 @@ function _game_show_scoreboard_data($machine_name, $app_route, $params, $args) {
   // connect to the database table
   // return data like this per row
   $rows = array();
-  $result = db_select('game_show_quiz', 'gsq')
-    ->fields('gsq');
-    if (isset($args['game'])) {
-      $result->condition('game', $args['game'], '=');
+  $query = db_select('game_show_quiz', 'gsq')
+      ->fields('gsq');
+    if (isset($params['section']) && $params['section'] != '') {
+      $query->condition('section', $params['section'], '=');
     }
-    if (isset($args['section'])) {
-      $result->condition('section', $args['section'], '=');
+    if (isset($params['game']) && $params['game'] != '') {
+      $query->condition('game', $params['game'], '=');
     }
-    $result->execute()
-    ->fetchAssoc();
-  foreach ($result as $values) {
-    $usr = user_load($values['uid']);
-    $scores = json_decode($values['scores']);
+    $result = $query->execute()->fetchAllAssoc('uuid');
+    foreach($result as $values) {
+    $usr = user_load($values->uid);
+    $scores = json_decode($values->scores);
+    $dates = json_decode($values->dates);
     $row = array(
       "high" => max($scores),
-      "dates" => $values['dates'],
-      "scores" => implode(', ', $scores),
-      "display_name" => _elmsln_core_get_user_name('full', $values['uid']),
-      "avatar" => _elmsln_core_get_user_picture('avatar', $values['uid']),
-      "visual" => _elmsln_core_get_user_extras($values['uid']),
-      "sis" => _elmsln_core_get_sis_user_data($values['uid']),
+      "dates" => $dates,
+      "scores" => $scores,
+      "game" => $values->game,
+      "section" => $values->section,
+      "display_name" => _elmsln_core_get_user_name('full', $values->uid),
+      "avatar" => _elmsln_core_get_user_picture('avatar', $values->uid),
+      "visual" => _elmsln_core_get_user_extras($values->uid),
+      "sis" => _elmsln_core_get_sis_user_data($values->uid),
     );
     $rows[] = $row;
   }
   return array(
     'status' => 200,
-    'data' => $data,
+    'data' => $rows,
   );
 }
