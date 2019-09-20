d836eb3c833834e2ea924b1c2a6dd06bd5834bf4
btopro <btopro@outlook.com>
2019-09-20 15:43:23 -0400

    #2667

---
+++ core/dslmcode/cores/haxcms-1/build/es6/dist/build-install.js
@@ -0,0 +1,1 @@
+import"../node_modules/@lrnwebcomponents/hax-logo/hax-logo.js";import"../node_modules/@polymer/paper-card/paper-card.js";import"../node_modules/@polymer/paper-button/paper-button.js";import"../node_modules/@polymer/iron-icons/iron-icons.js";import"../node_modules/@lrnwebcomponents/git-corner/git-corner.js";
