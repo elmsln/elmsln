1937c8be474f654c7236b9d4dbf55145ac6327a1
btopro <btopro@outlook.com>
2020-03-31 19:48:32 -0400

    try and account for null password in travis

--- scripts/install/elmsln-install.sh
+++ scripts/install/elmsln-install.sh
@@ -11,6 +11,12 @@ cd $DIR
 source ../../config/scripts/drush-create-site/config.cfg
 # load password config
 source ../../config/scripts/drush-create-site/configpwd.cfg
+  if [ $dbsupw == '' ];
+    then
+      dbpwstring=""
+    else
+      dbpwstring="--db-su-pw=$dbsupw"
+  fi
 # provide messaging colors for output to console
 txtbld=$(tput bold)             # Bold
 bldgrn=$(tput setaf 2) #  green
@@ -118,7 +124,7 @@ for build in "${buildlist[@]}"
   # install default site for associated stacks in the build list
   cd $stacks/$build
   elmslnecho "drush installing service placeholder: $build"
-  drush site-install minimal --v --y --db-url=mysql://elmslndfltdbo:$dbpw@127.0.0.1/default_$build --db-su="$dbsu" --db-su-pw="$dbsupw" --account-mail="$admin" --site-mail="$site_email"
+  drush site-install minimal --v --y --db-url=mysql://elmslndfltdbo:$dbpw@127.0.0.1/default_$build --db-su=$dbsu $dbpwstring --account-mail="$admin" --site-mail="$site_email"
 done
 echo '3' > $steplog
 COUNTER=0
@@ -137,7 +143,7 @@ for tool in "${authoritylist[@]}"
   cd ${webdir}/${tool}
   sitedir=${webdir}/${tool}/sites
   elmslnecho "drush installing authority tool: $tool"
-  drush site-install ${dist} --v --y --db-url=mysql://${tool}_${host}:$dbpw@127.0.0.1/${tool}_${host} --db-su="$dbsu" --db-su-pw="$dbsupw" --account-mail="$admin" --site-mail="$site_email" --site-name="$tool"
+  drush site-install ${dist} --v --y --db-url=mysql://${tool}_${host}:$dbpw@127.0.0.1/${tool}_${host} --db-su=$dbsu $dbpwstring --account-mail="$admin" --site-mail="$site_email" --site-name="$tool"
   #move out of $tool site directory to host
   sudo mkdir -p $sitedir/$tool/$host
   sudo mkdir -p $sitedir/$tool/$host/files
