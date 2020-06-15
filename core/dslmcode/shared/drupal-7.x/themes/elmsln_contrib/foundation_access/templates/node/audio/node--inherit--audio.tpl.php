7705554ff078577e8be125ca10adb26a9d8be4b1
btopro <btopro@outlook.com>
2020-06-15 11:52:19 -0400

    https://github.com/elmsln/issues/issues/263

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/node/audio/node--inherit--audio.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/foundation_access/templates/node/audio/node--inherit--audio.tpl.php
@@ -1,13 +1,17 @@
-
-<a11y-media-player
+<video-player
 accent-color="indigo"
-audio-only
-<?php if (isset($node->field_poster['und'][0]['uri'])): ?> thumbnail-src="<?php print file_create_url($node->field_poster['und'][0]['uri']); ?>"<?php endif;?>
+source="<?php print file_create_url($node->field_audio['und'][0]['uri']); ?>"
+<?php if (isset($node->field_poster['und'][0]['uri'])): ?>
+  thumbnail-src="<?php print file_create_url($node->field_poster['und'][0]['uri']); ?>"
+<?php endif;?>
+<?php
+if (isset($node->field_caption['und'][0]['uri'])) {
+  $json = new stdClass();
+  $json->kind = "subtitles";
+  $json->srclang = "en";
+  $json->label = "English";
+  $json->src = file_create_url($node->field_caption['und'][0]['uri']);
+  print "tracks='[" . json_encode($json) . "]'";
+}?>
 >
-  <source src="<?php print file_create_url($node->field_audio['und'][0]['uri']); ?>" type="audio/mp3">
-  <?php if (isset($node->field_caption['und'][0]['uri'])): ?>
-    <track label="English" kind="subtitles" srclang="en" src="<?php print file_create_url($node->field_caption['und'][0]['uri']); ?>" default>
-  <?php endif;?>
-</a11y-media-player>
-
-
+</video-player>
