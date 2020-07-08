39a6496e7793c049d484199178297cb9515c8cbb
btopro <btopro@outlook.com>
2020-07-08 17:25:25 -0400

    haxcms

--- core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/haxcms_drupal_theme/node.tpl.php
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/haxcms_drupal_theme/node.tpl.php
@@ -80,8 +80,8 @@
  * @ingroup themeable
  */
 ?>
-hello
 <?php
+  print_r($node);
   // We hide the comments and links now so that we can render them later.
   hide($content['comments']);
   hide($content['links']);
