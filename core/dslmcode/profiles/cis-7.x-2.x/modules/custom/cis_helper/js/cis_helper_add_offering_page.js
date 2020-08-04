4c2987785ce4c418e99c7ced0c52aadf0d08fed1
btopro <btopro@outlook.com>
2020-08-04 11:22:53 -0400

    js

--- core/dslmcode/profiles/cis-7.x-2.x/modules/custom/cis_helper/js/cis_helper_add_offering_page.js
+++ core/dslmcode/profiles/cis-7.x-2.x/modules/custom/cis_helper/js/cis_helper_add_offering_page.js
@@ -4,18 +4,17 @@
  */
 (function ($) {
     $(document).ready(function(){
-        setTimeout(function() {
+        fullUrlBuilder();
+        document.getElementById('edit-courses').addEventListener('change', function(e){
             fullUrlBuilder();
-            document.getElementById('edit-courses').addEventListener('change', function(e){
-                fullUrlBuilder();
-            });
-            document.getElementById('edit-access-string').addEventListener('input', function(e){
-                fullUrlBuilder();
-            });
-        }, 5000);
+        });
+        document.getElementById('edit-access-string').addEventListener('input', function(e){
+            fullUrlBuilder();
+        });
     });
-})
-
+})(jQuery);
+// build and visualize the URL as someone types so they know
+// what they'll be able to copy and paste
 function fullUrlBuilder() {
     var fullUrl = Drupal.settings.elmslnCore.url.replace('cis-add-offering', 'course-redirect/');
     fullUrl += document.getElementById('edit-courses').value + '/';
@@ -25,6 +24,5 @@ function fullUrlBuilder() {
         div.setAttribute('id', 'custom-div');
         document.getElementById('cis-helper-add-offering-page').appendChild(div);
     }
-    console.log(fullUrl);
     document.getElementById('custom-div').innerHTML = '<strong>Access URL:</strong> ' + fullUrl;
 }
