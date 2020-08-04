9a4155953f15ca4be3e1c0d927eb237e631575bf
btopro <btopro@outlook.com>
2020-08-04 11:08:05 -0400

    API to add support for on-demand access

---
+++ core/dslmcode/profiles/cis-7.x-2.x/modules/custom/cis_helper/js/cis_helper_add_offering_page.js
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * Simple URL builder based on inputs
+ */
+(function ($) {
+    $(document).ready(function(){
+        fullUrlBuilder();
+        document.getElementById('edit-courses').addEventListener('change', function(e){
+            fullUrlBuilder();
+        });
+        document.getElementById('edit-access-string').addEventListener('input', function(e){
+            fullUrlBuilder();
+        });
+    });
+})
+
+function fullUrlBuilder() {
+    var fullUrl = Drupal.settings.elmslnCore.url.replace('cis-add-offering', 'course-redirect/');
+    fullUrl += document.getElementById('edit-courses').value + '/';
+    fullUrl += document.getElementById('edit-access-string').value;
+    if (!document.getElementById('custom-div')) {
+        var div = document.createElement('div');
+        div.setAttribute('id', 'custom-div');
+        document.getElementById('cis-helper-add-offering-page').appendChild(div);
+    }
+    document.getElementById('custom-div').innerHTML = '<strong>Access URL:</strong> ' + fullUrl;
+}
