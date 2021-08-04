c7a98e999fa2b926c886bdc3605aa19cf5735765
btopro <btopro@outlook.com>
2021-08-04 15:47:43 -0400

    https://github.com/elmsln/issues/issues/499 just about there..

---
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/haxcms_drupal_theme/cis-lmsless-network.tpl.php
@@ -0,0 +1,44 @@
+<?php
+  /**
+   * CIS LMS-less Network block template.
+   */
+?>
+  <ul>
+  <!-- End Menu Item Dropdowns -->
+  <?php foreach ($network['services'] as $title => $items) : ?>
+    <?php if ($title == t('Network')) { continue; } ?>
+    <li>
+      <div class="subheader"><?php print t('@title', array('@title' => token_replace($title))); ?></div>
+      <div class="divider grey"></div>
+    </li>
+    <?php
+      foreach ($items as $service) :
+        if (!isset($service['icon'])) {
+          $service['icon'] = $service['machine_name'];
+        }
+        // apply default system color if we get misses
+        if (!isset($service['distro'])) {
+          $service['distro'] = '_default_';
+        }
+        $activetool = '';
+        $iconcolor = $lmsless_classes[$service['distro']]['color'] . '-text text-darken-4';
+        if (isset($active['machine_name']) && $service['machine_name'] == $active['machine_name']) {
+          $activetool = $lmsless_classes[$service['distro']]['color'] . ' active-system white-text ';
+          $iconcolor = $lmsless_classes[$service['distro']]['color'] . ' white-text ';
+        }
+        $stitle = token_replace($service['title']);
+    ?>
+    <li>
+    <?php if (isset($service['icon_library']) && $service['icon_library'] == 'material'): ?>
+      <lrnsys-button href="<?php print token_replace($service['url']); ?>" button-class="black-text" hover-class="<?php print $lmsless_classes[$service['distro']]['color'];?> <?php print $lmsless_classes[$service['distro']]['dark'];?> white-text" data-prefetch-hover="true" data-jwerty-key="ctrl+<?php print drupal_strtolower(substr($stitle, 0, 1)); ?>" data-voicecommand="<?php print t('go to ') . drupal_strtolower($stitle); ?>" icon="<?php print $service['icon'];?>">
+        <span class="elmsln-network-label"><?php print $stitle; ?></span>
+      </lrnsys-button>
+    <?php else: ?>
+      <lrnsys-button icon="lrn:<?php print $service['icon'];?>" href="<?php print token_replace($service['url']); ?>" button-class="black-text" hover-class="<?php print $lmsless_classes[$service['distro']]['color'];?> <?php print $lmsless_classes[$service['distro']]['dark'];?> white-text" data-prefetch-hover="true" data-jwerty-key="ctrl+<?php print drupal_strtolower(substr($stitle, 0, 1)); ?>" data-voicecommand="<?php print t('go to ') . drupal_strtolower($stitle); ?>">
+        <span class="elmsln-network-label"><?php print $stitle; ?></span>
+      </lrnsys-button>
+    <?php endif; ?>
+    </li>
+    <?php endforeach ?>
+  <?php endforeach ?>
+  </ul>
---
+++ core/dslmcode/shared/drupal-7.x/themes/elmsln_contrib/haxcms_drupal_theme/cis-lmsless-user.tpl.php
@@ -0,0 +1,60 @@
+<ul style="margin:-24px">
+  <li class="ferpa-protect">
+    <div class="user-drawer-block">
+      <?php print $userbackground; ?>
+      <div class="details white">
+        <div class="user-details-image">
+          <?php if (isset($userpicturebig)) { print $userpicturebig; } ?>
+        </div>
+        <div class="user-details-text">
+          <?php if (!empty($username)) : ?>
+            <div class="name"><?php print "$username"; ?></div>
+          <?php endif; ?>
+          <div class="section-title"><?php if (isset($section_title)) { print $section_title; } ?></div>
+          <div class="user-roles"><?php if (!empty($user_roles)) { print $user_roles; } ?></div>
+        </div>
+      </div>
+    </div>
+  </li>
+  <?php if (!empty($username) && $username !== t("Anonymous")) : ?>
+  <li>
+    <lrnsys-button label="<?php print $userprofile['label']; ?>" href="<?php print $userprofile['href']; ?>" class="<?php if (is_array($userprofile['class'])) { print implode(' ', $userprofile['class']); } ?>" icon="<?php print $userprofile['icon']; ?>" hover-class="<?php if(is_array($userprofile['hover-class'])) {print implode(' ', $userprofile['hover-class']);} ?>"></lrnsys-button>
+  </li>
+  <?php endif; ?>
+  <?php if (isset($user_section) || !empty($masquerade)) : ?>
+  <li><div class="divider"></div></li>
+  <?php endif; ?>
+  <?php if (isset($user_section)) : ?>
+  <li>
+    <lrnsys-dialog header="<?php print t('Change section');?>">
+      <span slot="button">
+        <simple-icon icon="perm-identity"></simple-icon>
+        <span><?php print t('Change section');?></span>
+      </span>
+      <span slot="content">
+        <?php print $user_section; ?>
+      </span>
+    </lrnsys-dialog>
+  </li>
+  <?php endif; ?>
+  <?php if (!empty($masquerade)) : ?>
+  <li>
+    <lrnsys-dialog header="<?php print t('Impersonate account');?>">
+      <span slot="button">
+        <simple-icon icon="supervisor-account"></simple-icon>
+        <span><?php print t('Impersonate account');?></span>
+      </span>
+      <span slot="content">
+        <?php print $masquerade; ?>
+      </span>
+    </lrnsys-dialog>
+  </li>
+  <?php endif; ?>
+  <?php if (!empty($masquerade_logout)) : ?>
+  <li><?php print $masquerade_logout; ?></li>
+  <?php endif; ?>
+  <li><div class="divider"></div></li>
+  <li>
+    <lrnsys-button label="<?php print $userlink['label']; ?>" href="<?php print $userlink['href']; ?>" class="<?php print implode(' ', $userlink['class']); ?>" icon="<?php print $userlink['icon']; ?>" hover-class="<?php print implode(' ', $userlink['hover-class']); ?>"></lrnsys-button>
+  </li>
+</ul>
