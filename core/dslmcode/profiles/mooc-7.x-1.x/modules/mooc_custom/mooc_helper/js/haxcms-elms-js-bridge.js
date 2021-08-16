ffa35d5e94ea7dcd450363a36e13b21af530dcf2
btopro <btopro@outlook.com>
2021-08-16 15:11:03 -0400

    lightbox

---
+++ core/dslmcode/profiles/mooc-7.x-1.x/modules/mooc_custom/mooc_helper/js/haxcms-elms-js-bridge.js
@@ -0,0 +1,40 @@
+/**
+ * behavior to make sure select lists are applied every time we do an ajax reload.
+ */
+Drupal.behaviors.haxCmsMoocBridgeOldContent = {
+  attach: function (context, settings) {
+    document.querySelectorAll("a[data-imagelightbox]").forEach((item) => {
+      item.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        e.stopPropagation();
+        let target = e.target;
+        // move up to a tag if we are on an image tag
+        if (target.tagName === "IMG") {
+          target = target.parentNode;
+        }
+        const content = document.createElement("img");
+        content.src = target.getAttribute("href");
+        content.style.margin = "0 auto";
+        content.style.display = "block";
+        const evt = new CustomEvent("simple-modal-show", {
+          bubbles: true,
+          cancelable: true,
+          detail: {
+            title: e.target.getAttribute('alt'),
+            elements: {
+              content: content,
+            },
+            styles: {
+              "--simple-modal-z-index": "100000000",
+            },
+            invokedBy: target,
+            clone: false,
+          },
+        });
+        window.dispatchEvent(evt);
+      });
+    });
+  }
+};
+ 
