8c7e86623f305131f072ff64e996130e1a3b9c43
btopro <btopro@outlook.com>
2022-08-15 15:46:12 -0400

    https://github.com/elmsln/issues/issues/981 clean up of some options

--- core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/core/cis_lmsless/js/cis_lmsless_haxcms.js
+++ core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/core/cis_lmsless/js/cis_lmsless_haxcms.js
@@ -5,3 +5,6 @@ window.addEventListener('hax-store-ready', function(e) {
         tray.shadowRoot.querySelector(".collapsed").style.margin = "160px 0px 0px";
     }, 1000);
   });
+// forcibly disable sounds and darkmode
+localStorage.setItem('app-hax-soundStatus', "false");
+localStorage.setItem('app-hax-darkMode', "false");
